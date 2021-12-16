a0f25c30a9d6131af52bbebcb16dda5869450c15
brandoncimino <brandon.cimino@gmail.com>
2021-12-15 22:27:59 -0600

    Cleanup, added javadocs, and added annotations

--- BSharp/Clerical/BPath.cs
+++ BSharp/Clerical/BPath.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -11,6 +12,8 @@ using FowlFever.Conjugal.Affixing;
 
 using JetBrains.Annotations;
 
+using Pure = System.Diagnostics.Contracts.PureAttribute;
+
 namespace FowlFever.BSharp.Clerical {
     /// <summary>
     /// TODO: There is almost certainly a cross-platform library to use for this that I can get from Nuget
@@ -19,7 +22,7 @@ namespace FowlFever.BSharp.Clerical {
     public static class BPath {
         internal static readonly RegexGroup ExtensionGroup            = new RegexGroup(nameof(ExtensionGroup), @"(\.[^.]+?)+$");
         internal static readonly char[]     Separators                = Enum.GetValues(typeof(DirectorySeparator)).Cast<DirectorySeparator>().Select(DirectorySeparatorExtensions.ToChar).ToArray();
-        internal static readonly Regex      DirectorySeparatorPattern = new Regex(@"[\\\/]");
+        public static readonly   Regex      DirectorySeparatorPattern = new Regex(@"[\\\/]");
         internal static readonly string     OpenFolderIcon            = "📂";
         internal static readonly string     ClosedFolderIcon          = "📁";
         internal static readonly string     FileIcon                  = "📄";
@@ -78,45 +81,37 @@ namespace FowlFever.BSharp.Clerical {
                    .ToArray();
         }
 
-        [ContractAnnotation("path:null => null")]
-        public static string? GetFullExtension(string? path) {
-            if (path == null) {
-                return null;
-            }
-
+        [Pure]
+        public static string GetFullExtension(string path) {
             return Path.GetFileName(path)
                        .Match(ExtensionGroup.Regex)
                        .Groups[ExtensionGroup.GroupName]
                        .Value;
         }
 
-        [ContractAnnotation("path:null => null")]
-        public static string? GetFileNameWithoutExtensions(string? path) {
-            if (path == null) {
-                return null;
-            }
-
+        [Pure]
+        public static string GetFileNameWithoutExtensions(string path) {
             var fileName    = Path.GetFileName(path);
             var firstPeriod = fileName.IndexOf(".", StringComparison.Ordinal);
             return firstPeriod < 0 ? fileName : fileName.Substring(0, firstPeriod);
         }
 
-        [ContractAnnotation("null => false")]
-        public static bool EndsWithSeparator(string? path) {
+        /// <param name="path">a <see cref="Path"/> <see cref="string"/></param>
+        /// <returns>true if the <see cref="string"/> <b>ends</b> with <b>any</b> <see cref="DirectorySeparator"/></returns>
+        [Pure]
+        public static bool EndsWithSeparator(string path) {
             return path.EndsWith(DirectorySeparatorPattern);
         }
 
-        [ContractAnnotation("null => false")]
-        public static bool StartsWithSeparator(string? path) {
+        /// <param name="path">a <see cref="Path"/> <see cref="string"/></param>
+        /// <returns>true if the <see cref="string"/> <b>starts</b> with <b>any</b> <see cref="DirectorySeparator"/></returns>
+        [Pure]
+        public static bool StartsWithSeparator(string path) {
             return path.StartsWith(DirectorySeparatorPattern);
         }
 
-
+        [Pure]
         public static string EnsureTrailingSeparator(string? path, DirectorySeparator separator = DirectorySeparator.Universal) {
-            if (path == null) {
-                return separator.ToCharString();
-            }
-
             return NormalizeSeparators(
                 path?.Trim()
                     .TrimEnd(DirectorySeparatorPattern)
@@ -125,33 +120,42 @@ namespace FowlFever.BSharp.Clerical {
             );
         }
 
-
-        public static string StripLeadingSeparator(string? path, DirectorySeparator separator = DirectorySeparator.Universal) {
-            if (path == null) {
-                return "";
-            }
-
-            return NormalizeSeparators(
-                path?.Trim()
-                    .TrimStart(separator.ToCharString()),
-                separator
-            );
+        /// <summary>
+        /// Removes any <see cref="DirectorySeparator"/>s from the <b>beginning</b> of <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">the original <see cref="Path"/> <see cref="string"/></param>
+        /// <returns>the original <see cref="string"/> with all of the leading <see cref="DirectorySeparator"/>s removed</returns>
+        [Pure]
+        public static string StripLeadingSeparator(string? path) {
+            return path.TrimStart(DirectorySeparatorPattern) ?? "";
         }
 
-
+        /// <summary>
+        /// Replaces all <see cref="DirectorySeparator"/>s in <paramref name="path"/> with the desired <paramref name="separator"/>.
+        /// </summary>
+        /// <param name="path">the original <see cref="string"/></param>
+        /// <param name="separator">the desired <see cref="DirectorySeparator"/></param>
+        /// <returns>the original <see cref="string"/> with all <see cref="DirectorySeparator"/>s replaced with the desired <paramref name="separator"/></returns>
+        [Pure]
         public static string NormalizeSeparators(string? path, DirectorySeparator separator = DirectorySeparator.Universal) {
-            return path.IsBlank() ? "" : DirectorySeparatorPattern.Replace(path.Trim(), separator.ToCharString());
+            return path.IsBlank() ? "" : DirectorySeparatorPattern.Replace(path!.Trim(), separator.ToCharString());
         }
 
 
-        public static string JoinPath(
-            string? parent,
-            string? child
-        ) {
-            return JoinPath(parent, child, default(DirectorySeparator));
-        }
-
-
+        /// <summary>
+        /// Combines multiple <see cref="string"/>s into a <see cref="Path"/> <see cref="string"/>, ensuring that <b>exactly 1</b>
+        /// <see cref="DirectorySeparator"/> exists between them.
+        /// <p/>
+        /// Also <see cref="NormalizeSeparators"/> using <paramref name="separator"/>.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="StringUtils.IsEmpty"/> strings <b>are included in the path</b> as <c>""</c>, i.e. <c>JoinPath("a",null) => "a/"</c>
+        /// </remarks>
+        /// <param name="parent">the first part of the path</param>
+        /// <param name="child">the second part of the path</param>
+        /// <param name="separator">the desired <see cref="DirectorySeparator"/></param>
+        /// <returns>a new <see cref="Path"/> <see cref="string"/></returns>
+        [Pure]
         public static string JoinPath(
             string?            parent,
             string?            child,
@@ -163,7 +167,25 @@ namespace FowlFever.BSharp.Clerical {
             return NormalizeSeparators(path, separator);
         }
 
+        /**
+         * <inheritdoc cref="JoinPath(string?,string?,FowlFever.BSharp.Clerical.DirectorySeparator)"/>
+         */
+        [Pure]
+        public static string JoinPath(
+            string? parent,
+            string? child
+        ) {
+            return JoinPath(parent, child, default(DirectorySeparator));
+        }
 
+        /// <summary>
+        /// <inheritdoc cref="JoinPath(string?,string?,FowlFever.BSharp.Clerical.DirectorySeparator)"/>
+        /// </summary>
+        /// <param name="parentDirectory">the first part of the <see cref="Path"/></param>
+        /// <param name="child">the second part of the <see cref="Path"/></param>
+        /// <param name="separator">the desired <see cref="DirectorySeparator"/></param>
+        /// <returns>a new <see cref="Path"/> <see cref="string"/></returns>
+        [Pure]
         public static string JoinPath(
             DirectoryInfo?     parentDirectory,
             string?            child,
@@ -172,12 +194,23 @@ namespace FowlFever.BSharp.Clerical {
             return JoinPath(parentDirectory?.FullName, child, separator);
         }
 
-
+        /// <summary>
+        /// <inheritdoc cref="JoinPath(string?,string?,FowlFever.BSharp.Clerical.DirectorySeparator)"/>
+        /// </summary>
+        /// <param name="parts">the <see cref="string"/>s used to build the <see cref="Path"/></param>
+        /// <returns>a new <see cref="Path"/> <see cref="string"/></returns>
+        [Pure]
         public static string JoinPath(params string?[]? parts) {
             return JoinPath(parts, default(DirectorySeparator));
         }
 
-
+        /// <summary>
+        /// <inheritdoc cref="JoinPath(string?,string?,FowlFever.BSharp.Clerical.DirectorySeparator)"/>
+        /// </summary>
+        /// <param name="parts">the <see cref="string"/>s used to build the <see cref="Path"/></param>
+        /// <param name="separator">the desired <see cref="DirectorySeparator"/></param>
+        /// <returns>a new <see cref="Path"/> <see cref="string"/></returns>
+        [Pure]
         public static string JoinPath(IEnumerable<string?>? parts, DirectorySeparator separator = DirectorySeparator.Universal) {
             return parts?.Aggregate((pathSoFar, nextPart) => JoinPath(pathSoFar, nextPart, separator)) ?? "";
         }
