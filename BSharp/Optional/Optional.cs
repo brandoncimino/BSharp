cae867f540c3c634cec72ef9047d6e62b0ac4584
Brandon Cimino <bcimino@cccis.com>
2022-01-19 20:58:22 -0600

    Added a `.Select()` method for nullable types

--- BSharp/Optional/Optional.cs
+++ BSharp/Optional/Optional.cs
@@ -159,6 +159,27 @@ namespace FowlFever.BSharp.Optional {
         }
 
         /// <summary>
+        /// If this <typeparamref name="TIn"/> is <b>not</b> <c>null</c>, <see cref="Func{T,TResult}.Invoke"/> <paramref name="selector"/>.
+        ///
+        /// Otherwise, return a null <typeparamref name="TOut"/>.
+        /// </summary>
+        /// <remarks>
+        /// Intended to let you treat a nullable type (<see cref="ValueType"/> OR reference) as an <see cref="IOptional{T}"/>.
+        /// </remarks>
+        /// <param name="nullable">a <see cref="ValueType"/> that might be null</param>
+        /// <param name="selector">a <see cref="Func{TResult}"/> to apply to a <b>non-null</b> <typeparamref name="TIn"/></param>
+        /// <typeparam name="TIn">the original <see cref="Type"/></typeparam>
+        /// <typeparam name="TOut">the output <see cref="Type"/></typeparam>
+        /// <returns>a nullable <typeparamref name="TOut"/></returns>
+        [Pure]
+        public static TOut? Select<TIn, TOut>(
+            this TIn?       nullable,
+            Func<TIn, TOut> selector
+        ) {
+            return nullable != null ? selector(nullable) : default;
+        }
+
+        /// <summary>
         /// Provides a "default" <see cref="object.ToString"/> method for <see cref="IOptional{T}"/> implementations to use.
         /// </summary>
         /// <param name="optional">an <see cref="IOptional{T}"/></param>
