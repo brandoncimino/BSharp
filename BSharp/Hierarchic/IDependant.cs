9637f7815657496874e3c11d612c825fcea8f51b
brandoncimino <brandon.cimino@gmail.com>
2022-01-25 20:13:36 -0600

    - Removed the deprecated `Hierarchic` package - Updated some formatting settings

--- BSharp/Hierarchic/IDependant.cs
+++
@@ -1,7 +0,0 @@
-namespace FowlFever.BSharp.Hierarchic {
-    public interface IDependant<out TGuardian, TDependant>
-        where TGuardian : IGuardian<TGuardian, TDependant>
-        where TDependant : IDependant<TGuardian, TDependant> {
-        TGuardian Guardian { get; }
-    }
-}
