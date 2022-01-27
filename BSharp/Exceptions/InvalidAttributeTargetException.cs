fd936f0fb6114e3f0c0daf574d64a5e7ef60bb29
Brandon Cimino <bcimino@cccis.com>
2022-01-26 20:39:57 -0600

    Fixed InvalidAttributeTargetException.cs

--- BSharp/Exceptions/InvalidAttributeTargetException.cs
+++ BSharp/Exceptions/InvalidAttributeTargetException.cs
@@ -6,11 +6,9 @@ using FowlFever.BSharp.Strings;
 namespace FowlFever.BSharp.Exceptions {
     public class InvalidAttributeTargetException<T> : BrandonException {
         private readonly MemberInfo BadTarget;
-        public override  string     BaseMessage => $"The custom attribute {typeof(T).Name} was set to an invalid target, {StringUtils.FormatMember(BadTarget)}!";
+        public override  string     BaseMessage => $"The custom attribute {typeof(T).Name} was set to an invalid target, {BadTarget}!";
 
-        public InvalidAttributeTargetException() { }
-
-        public InvalidAttributeTargetException(string message, MemberInfo badTarget, Exception innerException = null) : base(message, innerException) {
+        public InvalidAttributeTargetException(string message, MemberInfo badTarget, Exception? innerException = null) : base(message, innerException!) {
             BadTarget = badTarget;
         }
     }
