10135bd069e25ff19ee970867b74a8f383d29adc
Brandon Cimino <bcimino@cccis.com>
2022-01-26 20:40:37 -0600

    Marked ValidationExtensions.cs as `Obsolete`

--- BSharp/ValidationExtensions.cs
+++ BSharp/ValidationExtensions.cs
@@ -9,16 +9,11 @@ using FowlFever.BSharp.Strings;
 using JetBrains.Annotations;
 
 namespace FowlFever.BSharp {
-    [PublicAPI]
+    [Obsolete("Use Must instead")]
     public static class ValidationExtensions {
         [ContractAnnotation("null => stop")]
         public static T MustNotBeNull<T>(this T? obj) {
             return obj ?? throw new ArgumentNullException(nameof(obj), $"Parameter of type {typeof(T).Prettify()} must not be null!");
-        }
-
-
-        public static T MustBeNumeric<T>(this T? obj) {
-            return obj?.IsNumber() == true ? obj : throw new ArgumentException($"Parameter of type {obj?.GetType() ?? typeof(T)} must be a numeric type!");
         }
 
         public static void ValidateMultiple<T>(T toValidate, IEnumerable<Action<T>> checks) {
