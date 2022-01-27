4ec404de9113411f07b947ced9e29325a759efad
Brandon Cimino <bcimino@cccis.com>
2022-01-26 20:17:06 -0600

    Added pseudo "dynamic" `Mathb` overrides

--- BSharp/Mathb.Signage.cs
+++ BSharp/Mathb.Signage.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Diagnostics.Contracts;
 
+using FowlFever.BSharp.Exceptions;
+
+using Pure = System.Diagnostics.Contracts.PureAttribute;
 using JetBrains.Annotations;
 
 namespace FowlFever.BSharp {
@@ -11,48 +15,41 @@ namespace FowlFever.BSharp {
         /**
          * <seealso cref="Math.Sign(short)"/>
          */
-        [Pure]
-        public static int Sign(this short value) {
+        [Pure] public static int Sign(this short value) {
             return Math.Sign(value);
         }
 
         /**
          * <seealso cref="Math.Sign(int)"/>
          */
-        [Pure]
-        public static int Sign(this int integer) {
+        [Pure] public static int Sign(this int integer) {
             return Math.Sign(integer);
         }
 
         /**
          * <seealso cref="Math.Sign(long)"/>
          */
-        [Pure]
-        public static int Sign(this long value) => Math.Sign(value);
+        [Pure] public static int Sign(this long value) => Math.Sign(value);
 
         /**
          * <seealso cref="Math.Sign(float)"/>
          */
-        [Pure]
-        public static int Sign(this float value) => Math.Sign(value);
+        [Pure] public static int Sign(this float value) => Math.Sign(value);
 
         /**
          * <seealso cref="Math.Sign(double)"/>
          */
-        [Pure]
-        public static int Sign(this double value) => Math.Sign(value);
+        [Pure] public static int Sign(this double value) => Math.Sign(value);
 
         /**
          * <seealso cref="Math.Sign(decimal)"/>
          */
-        [Pure]
-        public static int Sign(this decimal value) {
+        [Pure] public static int Sign(this decimal value) {
             return Math.Sign(value);
         }
 
         /// <returns><paramref name="value"/>.<see cref="TimeSpan.CompareTo(object)"/>(<see cref="TimeSpan.Zero"/>)</returns>
-        [Pure]
-        public static int Sign(this TimeSpan value) {
+        [Pure] public static int Sign(this TimeSpan value) {
             return value.CompareTo(TimeSpan.Zero);
         }
 
@@ -60,126 +57,97 @@ namespace FowlFever.BSharp {
 
         #region IsPositive
 
-        [Pure]
-        public static bool IsPositive(this short value) => value >= 0;
+        [Pure] public static bool IsPositive(this short    value) => value >= 0;
+        [Pure] public static bool IsPositive(this int      value) => value >= 0;
+        [Pure] public static bool IsPositive(this long     value) => value >= 0;
+        [Pure] public static bool IsPositive(this float    value) => value >= 0;
+        [Pure] public static bool IsPositive(this double   value) => value >= 0;
+        [Pure] public static bool IsPositive(this decimal  value) => value >= 0;
+        [Pure] public static bool IsPositive(this TimeSpan value) => value >= TimeSpan.Zero;
 
+        /// <summary>
+        /// Attempts to call the appropriate <see cref="IsPositive(short)"/> overload.
+        /// </summary>
+        /// <param name="value">a value supported by <see cref="IsPositive(short)"/>, etc.</param>
+        /// <returns>true if <paramref name="value"/> ≥ 0</returns>
+        /// <exception cref="ArgumentException">if <paramref name="value"/> isn't a type with an appropriate <see cref="IsPositive(short)"/> method</exception>
         [Pure]
-        public static bool IsPositive(this int value) => value >= 0;
-
-        [Pure]
-        public static bool IsPositive(this long value) => value >= 0;
-
-        [Pure]
-        public static bool IsPositive(this float value) => value >= 0;
-
-        [Pure]
-        public static bool IsPositive(this double value) => value >= 0;
-
-        [Pure]
-        public static bool IsPositive(this decimal value) => value >= 0;
-
-        [Pure]
-        public static bool IsPositive(this TimeSpan value) => value >= TimeSpan.Zero;
+        public static bool IsPositive<T>(T value) {
+            return value switch {
+                short s    => s.IsPositive(),
+                int i      => i.IsPositive(),
+                long l     => l.IsPositive(),
+                float f    => f.IsPositive(),
+                double d   => d.IsPositive(),
+                decimal d  => d.IsPositive(),
+                TimeSpan t => t.IsPositive(),
+                _ => throw RejectArgument.UnhandledSwitchType(value, nameof(value), nameof(IsPositive)),
+            };
+        }
 
         #endregion
 
         #region IsStrictlyPositive
 
-        [Pure]
-        public static bool IsStrictlyPositive(this int value) => value > 0;
+        [Pure] public static bool IsStrictlyPositive(this short    value) => value > 0;
+        [Pure] public static bool IsStrictlyPositive(this int      value) => value > 0;
+        [Pure] public static bool IsStrictlyPositive(this long     value) => value > 0;
+        [Pure] public static bool IsStrictlyPositive(this float    value) => value > 0;
+        [Pure] public static bool IsStrictlyPositive(this double   value) => value > 0;
+        [Pure] public static bool IsStrictlyPositive(this decimal  value) => value > 0;
+        [Pure] public static bool IsStrictlyPositive(this TimeSpan value) => value > TimeSpan.Zero;
 
+        /// <summary>
+        /// Attempts to call the appropriate <see cref="IsStrictlyPositive(short)"/> overload.
+        /// </summary>
+        /// <param name="value">a value supported by <see cref="IsStrictlyPositive(short)"/>, etc.</param>
+        /// <returns>true if <paramref name="value"/> > 0</returns>
+        /// <exception cref="ArgumentException">if <paramref name="value"/> isn't a type with an appropriate <see cref="IsStrictlyPositive(short)"/> method</exception>
         [Pure]
-        public static bool IsStrictlyPositive(this short value) => value > 0;
-
-        [Pure]
-        public static bool IsStrictlyPositive(this long value) => value > 0;
-
-        [Pure]
-        public static bool IsStrictlyPositive(this float value) => value > 0;
-
-        [Pure]
-        public static bool IsStrictlyPositive(this double value) => value > 0;
-
-        [Pure]
-        public static bool IsStrictlyPositive(this decimal value) => value > 0;
-
-        [Pure]
-        public static bool IsStrictlyPositive(this TimeSpan value) => value > TimeSpan.Zero;
+        public static bool IsStrictlyPositive(object value) {
+            return value switch {
+                short s    => s.IsStrictlyPositive(),
+                int i      => i.IsStrictlyPositive(),
+                long l     => l.IsStrictlyPositive(),
+                float f    => f.IsStrictlyPositive(),
+                double d   => d.IsStrictlyPositive(),
+                decimal d  => d.IsStrictlyPositive(),
+                TimeSpan t => t.IsStrictlyPositive(),
+                _          => throw RejectArgument.UnhandledSwitchType(value, nameof(value), nameof(IsStrictlyPositive)),
+            };
+        }
 
         #endregion
 
         #region IsNegative
 
+        [Pure] public static bool IsNegative(this short    value) => value < 0;
+        [Pure] public static bool IsNegative(this int      value) => value < 0;
+        [Pure] public static bool IsNegative(this long     value) => value < 0;
+        [Pure] public static bool IsNegative(this float    value) => value < 0;
+        [Pure] public static bool IsNegative(this double   value) => value < 0;
+        [Pure] public static bool IsNegative(this decimal  value) => value < 0;
+        [Pure] public static bool IsNegative(this TimeSpan value) => value < TimeSpan.Zero;
+
+        /// <summary>
+        /// Attempts to call the appropriate <see cref="IsNegative(short)"/> overload.
+        /// </summary>
+        /// <param name="value">a value supported by <see cref="IsNegative(short)"/>, etc.</param>
+        /// <returns>true if <paramref name="value"/> <![CDATA[<]]> 0</returns>
+        /// <exception cref="ArgumentException">if <paramref name="value"/> isn't a type with an appropriate <see cref="IsNegative(short)"/> method</exception>
         [Pure]
-        public static bool IsNegative(this short value) => value < 0;
-
-        [Pure]
-        public static bool IsNegative(this int value) => value < 0;
-
-        [Pure]
-        public static bool IsNegative(this long value) => value < 0;
-
-        [Pure]
-        public static bool IsNegative(this float value) => value < 0;
-
-        [Pure]
-        public static bool IsNegative(this double value) => value < 0;
-
-        [Pure]
-        public static bool IsNegative(this decimal value) => value < 0;
-
-        [Pure]
-        public static bool IsNegative(this TimeSpan value) => value < TimeSpan.Zero;
-
-        #endregion
-
-        #region MustBePositive
-
-        [Pure, NonNegativeValue]
-        public static short MustBePositive(this short value) => value.IsPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive (x ⩾ 0)!");
-
-        [Pure, NonNegativeValue]
-        public static int MustBePositive(this int value) => value.IsPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive (x ⩾ 0)!");
-
-        [Pure, NonNegativeValue]
-        public static long MustBePositive(this long value) => value.IsPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive (x ⩾ 0)!");
-
-        [Pure, NonNegativeValue]
-        public static float MustBePositive(this float value) => value.IsPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive (x ⩾ 0)!");
-
-        [Pure, NonNegativeValue]
-        public static double MustBePositive(this double value) => value.IsPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive (x ⩾ 0)!");
-
-        [Pure, NonNegativeValue]
-        public static decimal MustBePositive(this decimal value) => value.IsPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive (x ⩾ 0)!");
-
-        [Pure, NonNegativeValue]
-        public static TimeSpan MustBePositive(this TimeSpan value) => value.IsPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive (x ⩾ 0)!");
-
-        #endregion
-
-        #region MustBeStrictlyPositive
-
-        [Pure, NonNegativeValue]
-        public static short MustBeStrictlyPositive(this short value) => value.IsStrictlyPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be strictly positive (x > 0)!");
-
-        [Pure, NonNegativeValue]
-        public static int MustBeStrictlyPositive(this int value) => value.IsStrictlyPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be strictly positive (x > 0)!");
-
-        [Pure, NonNegativeValue]
-        public static long MustBeStrictlyPositive(this long value) => value.IsStrictlyPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be strictly positive (x > 0)!");
-
-        [Pure, NonNegativeValue]
-        public static float MustBeStrictlyPositive(this float value) => value.IsStrictlyPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be strictly positive (x > 0)!");
-
-        [Pure, NonNegativeValue]
-        public static double MustBeStrictlyPositive(this double value) => value.IsStrictlyPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be strictly positive (x > 0)!");
-
-        [Pure, NonNegativeValue]
-        public static decimal MustBeStrictlyPositive(this decimal value) => value.IsStrictlyPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be strictly positive (x > 0)!");
-
-        [Pure, NonNegativeValue]
-        public static TimeSpan MustBeStrictlyPositive(this TimeSpan value) => value.IsStrictlyPositive() ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be strictly positive (x > 0)!");
+        public static bool IsNegative(object value) {
+            return value switch {
+                short s    => s.IsNegative(),
+                int i      => i.IsNegative(),
+                long l     => l.IsNegative(),
+                float f    => f.IsNegative(),
+                double d   => d.IsNegative(),
+                decimal d  => d.IsNegative(),
+                TimeSpan t => t.IsNegative(),
+                _          => throw RejectArgument.UnhandledSwitchType(value, nameof(value), nameof(IsNegative)),
+            };
+        }
 
         #endregion
 
