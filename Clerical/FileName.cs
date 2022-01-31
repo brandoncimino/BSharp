38a6f862ac26a37b384aaa55df8c5c7c2ef289f3
Brandon Cimino <bcimino@cccis.com>
2022-01-31 00:20:31 -0600

    Improved `FileName`

--- Clerical/FileName.cs
+++ Clerical/FileName.cs
@@ -7,14 +7,21 @@ namespace FowlFever.Clerical;
 
 /// <summary>
 /// Represents a <see cref="FileSystemInfo"/>-safe name.
+///
+/// TODO: Add validation when things like <see cref="BaseName"/> or <see cref="Extensions"/> are set
+/// TODO: Always strip periods from <see cref="Extensions"/>
 /// </summary>
 public record FileName {
     public string   BaseName   { get; init; }
     public string[] Extensions { get; init; } = { };
-    public string   FullName   => Extensions.Prepend(BaseName).JoinString(".");
+    public string   NameWithExtensions   => Extensions.Prepend(BaseName).JoinString(".");
 
     public FileName(string fileName) {
         BaseName   = BPath.GetFileNameWithoutExtensions(fileName);
         Extensions = BPath.GetExtensions(fileName);
+    }
+
+    public static FileName Random() {
+        return new FileName(Path.GetRandomFileName());
     }
 }
