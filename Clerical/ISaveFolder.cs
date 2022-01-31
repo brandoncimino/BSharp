89f027e8c3c00221d491f3ac7506ebfb093f6831
Brandon Cimino <bcimino@cccis.com>
2022-01-30 23:41:54 -0600

    Added `ISaveFolder.FolderName` and `ISaveFolder.DirectoryInfo`

--- Clerical/ISaveFolder.cs
+++ Clerical/ISaveFolder.cs
@@ -4,8 +4,9 @@ namespace FowlFever.Clerical;
 /// Represents the <see cref="DirectoryInfo"/> in which <see cref="ISaveFile{TData}"/>s are stored.
 /// </summary>
 public interface ISaveFolder {
-    public string PersistentDataPath             { get; }
-    public string RelativePathFromPersistentData { get; }
+    public string   PersistentDataPath             { get; }
+    public FileName FolderName                     { get; }
+    public string   RelativePathFromPersistentData { get; }
 
-    public IEnumerable<ISaveFile<TData>> EnumerateSaveFiles<TData>(string searchPattern) where TData : ISaveData;
+    public DirectoryInfo DirectoryInfo { get; }
 }
