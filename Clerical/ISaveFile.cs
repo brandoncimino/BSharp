b5658e07a2f22a5e7bd955e8815a2c79bf9af18b
Brandon Cimino <bcimino@cccis.com>
2022-01-30 23:40:55 -0600

    - Simplified `ISaveFile` interface - Updated `DateTime` -> `DateTimeOffset`

--- Clerical/ISaveFile.cs
+++ Clerical/ISaveFile.cs
@@ -16,14 +16,9 @@ public interface ISaveFile<out TData>
     public TData? Data { get; }
 
     /// <summary>
-    /// Data about the <b>act of saving itself</b>, such as <see cref="ISaveMetaData.LastSaveTime"/>.
-    /// </summary>
-    public ISaveMetaData MetaData { get; }
-
-    /// <summary>
     /// The <see cref="DateTime"/> at which the <b><see cref="Data"/> refers to</b>.
     /// </summary>
-    public DateTime TimeStamp { get; }
+    public DateTimeOffset TimeStamp { get; }
 
     /// <summary>
     /// The <see cref="FileInfo"/> that this <see cref="ISaveFile{TData}"/> serializes to.
@@ -32,25 +27,4 @@ public interface ISaveFile<out TData>
     /// This property <b>must be immutable!</b>
     /// </remarks>
     public FileInfo File { get; }
-
-    /// <summary>
-    /// Serializes <see cref="Data"/> to <see cref="File"/>.
-    /// </summary>
-    /// <param name="duplicateFileResolution">determines what we should do when a file already exists</param>
-    /// <param name="jsonSettings">optional <see cref="JsonSerializerOptions"/></param>
-    /// <returns></returns>
-    public ISaveFile<TData> Save(DuplicateFileResolution duplicateFileResolution, JsonSerializerOptions? jsonSettings = default);
-
-    /// <inheritdoc cref="Save(FowlFever.Clerical.DuplicateFileResolution,System.Text.Json.JsonSerializerOptions?)"/>
-    /// <summary>
-    /// Serializes <see cref="Data"/> to <see cref="File"/>.
-    /// </summary>
-    public ISaveFile<TData> Save(SaveManagerSettings? saveSettings = default);
-
-    /// <summary>
-    /// Deserializes the contents of <see cref="File"/> and stores it in <see cref="Data"/>.
-    /// </summary>
-    /// <param name="saveSettings"></param>
-    /// <returns></returns>
-    public ISaveFile<TData> Load(SaveManagerSettings? saveSettings = default);
 }
