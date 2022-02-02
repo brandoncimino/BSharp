1581fb7ff13a74c8344c516271a9b96dd5857da7
Brandon Cimino <bcimino@cccis.com>
2022-02-01 23:54:43 -0600

    Added `FileNamePart`

---
+++ Clerical/Validated/FileNamePart.cs
@@ -0,0 +1,29 @@
+using System.Text.RegularExpressions;
+
+using FluentValidation;
+
+using FowlFever.BSharp.Clerical;
+using FowlFever.BSharp.Exceptions;
+
+namespace FowlFever.Clerical.Validated;
+
+/// <summary>
+/// Guarantees that a string:
+/// <ul>
+/// <li>Doesn't contain <see cref="DirectorySeparator"/>s</li>
+/// <li>Doesn't contain whitespace</li>
+/// <li>Doesn't contain periods</li>
+/// </ul>
+/// </summary>
+public class FileNamePart {
+    public readonly string Value;
+
+    public FileNamePart(string value) {
+        var whitespacePattern = new Regex(@"\s");
+        var arg = new ArgInfo<string?>(value, nameof(Value), $"new {GetType().Name}");
+        Must.NotMatch(arg, whitespacePattern);
+        Must.NotMatch(arg, BPath.DirectorySeparatorPattern);
+
+        Value = value;
+    }
+}
