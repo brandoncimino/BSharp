8ec63b00ce1f48fd2ffafa6ec62166ca96716544
Brandon Cimino <bcimino@cccis.com>
2022-02-03 21:57:14 -0600

    Added `RoundingDirection.Complement()`

--- BSharp/Enums/RoundingDirection.cs
+++ BSharp/Enums/RoundingDirection.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+
+using FowlFever.BSharp.Exceptions;
 
 using Humanizer;
 
@@ -14,7 +17,7 @@ public enum RoundingDirection {
     /// <summary>
     /// AKA <see cref="Math.Floor(decimal)"/>
     /// </summary>
-    Floor,
+    Floor = default,
     /// <summary>
     /// AKA <see cref="Math.Ceiling(decimal)"/>
     /// </summary>
@@ -22,15 +25,15 @@ public enum RoundingDirection {
 }
 
 public static class RoundingDirectionExtensions {
-    public static Rounder Rounder(this RoundingDirection roundingDirection) => roundingDirection switch {
-        RoundingDirection.Ceiling => Enums.Rounder.Ceiling,
-        RoundingDirection.Floor   => Enums.Rounder.Floor,
-        _                         => throw BEnum.UnhandledSwitch(roundingDirection, nameof(roundingDirection), nameof(Rounder)),
-    };
+    public static double  Round(this RoundingDirection roundingDirection, double  value) => Rounder.Get(roundingDirection).Round(value);
+    public static float   Round(this RoundingDirection roundingDirection, float   value) => Rounder.Get(roundingDirection).Round(value);
+    public static decimal Round(this RoundingDirection roundingDirection, decimal value) => Rounder.Get(roundingDirection).Round(value);
 
-    public static double  ApplyTo(this RoundingDirection roundingDirection, double  value) => roundingDirection.Rounder().Round(value);
-    public static float   ApplyTo(this RoundingDirection roundingDirection, float   value) => roundingDirection.Rounder().Round(value);
-    public static decimal ApplyTo(this RoundingDirection roundingDirection, decimal value) => roundingDirection.Rounder().Round(value);
+    public static RoundingDirection Complement(this RoundingDirection roundingDirection) => roundingDirection switch {
+        RoundingDirection.Floor   => RoundingDirection.Ceiling,
+        RoundingDirection.Ceiling => RoundingDirection.Floor,
+        _                         => throw BEnum.UnhandledSwitch(roundingDirection, nameof(roundingDirection), nameof(Complement)),
+    };
 }
 
 public abstract class Rounder {
@@ -41,31 +44,40 @@ public abstract class Rounder {
     public virtual  int     RoundToInt(float   value) => (int)Round(value);
     public virtual  int     RoundToInt(decimal value) => (int)Round(value);
 
+    public static Rounder Get(object roundingStyle) {
+        Must.NotBeNull(roundingStyle, nameof(roundingStyle), nameof(Get));
+
+        if (Rounders.TryGetValue(roundingStyle, out var rounder)) {
+            return rounder;
+        }
+
+        throw new ArgumentException($"[{roundingStyle.GetType()}]{roundingStyle} was not a valid rounding style!");
+    }
+
+    private static readonly Dictionary<object, Rounder> Rounders = new() {
+        [RoundingDirection.Ceiling]     = new CeilingRounder(),
+        [RoundingDirection.Floor]       = new FloorRounder(),
+        [MidpointRounding.ToEven]       = new HalfEvenRounder(),
+        [MidpointRounding.AwayFromZero] = new AwayFromZeroRounder(),
+    };
+
     private class FloorRounder : Rounder {
         public override double  Round(double  value) => Math.Floor(value);
         public override decimal Round(decimal value) => Math.Floor(value);
     }
-
-    public static readonly Rounder Floor = new FloorRounder();
 
     private class CeilingRounder : Rounder {
         public override double  Round(double  value) => Math.Ceiling(value);
         public override decimal Round(decimal value) => Math.Ceiling(value);
     }
 
-    public static readonly Rounder Ceiling = new CeilingRounder();
-
     private class HalfEvenRounder : Rounder {
         public override double  Round(double  value) => Math.Round(value, MidpointRounding.ToEven);
         public override decimal Round(decimal value) => Math.Round(value, MidpointRounding.ToEven);
     }
 
-    public static readonly Rounder HalfEven = new HalfEvenRounder();
-
     private class AwayFromZeroRounder : Rounder {
         public override double  Round(double  value) => Math.Round(value, MidpointRounding.AwayFromZero);
         public override decimal Round(decimal value) => Math.Round(value, MidpointRounding.AwayFromZero);
     }
-
-    public static readonly Rounder AwayFromZero = new AwayFromZeroRounder();
 }
