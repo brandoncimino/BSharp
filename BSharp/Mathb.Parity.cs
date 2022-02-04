e7cb4766385aeb7e8bd092e6bd0e2df3c8d3deb7
Brandon Cimino <bcimino@cccis.com>
2022-02-03 21:58:46 -0600

    Updated formatting

--- BSharp/Mathb.Parity.cs
+++ BSharp/Mathb.Parity.cs
@@ -10,29 +10,14 @@ namespace FowlFever.BSharp {
 
         #region Integral types
 
-        [Pure]
-        public static Parity Parity(this byte value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
-
-        [Pure]
-        public static Parity Parity(this sbyte value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
-
-        [Pure]
-        public static Parity Parity(this int value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
-
-        [Pure]
-        public static Parity Parity(this uint value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
-
-        [Pure]
-        public static Parity Parity(this long value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
-
-        [Pure]
-        public static Parity Parity(this ulong value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
-
-        [Pure]
-        public static Parity Parity(this short value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
-
-        [Pure]
-        public static Parity Parity(this ushort value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
+        [Pure] public static Parity Parity(this byte   value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
+        [Pure] public static Parity Parity(this sbyte  value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
+        [Pure] public static Parity Parity(this int    value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
+        [Pure] public static Parity Parity(this uint   value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
+        [Pure] public static Parity Parity(this long   value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
+        [Pure] public static Parity Parity(this ulong  value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
+        [Pure] public static Parity Parity(this short  value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
+        [Pure] public static Parity Parity(this ushort value) => value % 2 == 0 ? Enums.Parity.Even : Enums.Parity.Odd;
 
         #endregion
 
@@ -65,63 +50,29 @@ namespace FowlFever.BSharp {
 
         #region IsEven
 
-        [Pure]
-        public static bool IsEven(this int value) => value.Parity() == Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsEven(this uint value) => value.Parity() == Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsEven(this long value) => value.Parity() == Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsEven(this ulong value) => value.Parity() == Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsEven(this short value) => value.Parity() == Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsEven(this ushort value) => value.Parity() == Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsEven(this float value) => value.Parity() == Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsEven(this double value) => value.Parity() == Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsEven(this decimal value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this int     value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this uint    value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this long    value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this ulong   value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this short   value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this ushort  value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this float   value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this double  value) => value.Parity() == Enums.Parity.Even;
+        [Pure] public static bool IsEven(this decimal value) => value.Parity() == Enums.Parity.Even;
 
         #endregion
 
         #region IsNotEven
 
-        [Pure]
-        public static bool IsNotEven(this int value) => value.Parity() != Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsNotEven(this uint value) => value.Parity() != Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsNotEven(this long value) => value.Parity() != Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsNotEven(this ulong value) => value.Parity() != Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsNotEven(this short value) => value.Parity() != Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsNotEven(this ushort value) => value.Parity() != Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsNotEven(this float value) => value.Parity() != Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsNotEven(this double value) => value.Parity() != Enums.Parity.Even;
-
-        [Pure]
-        public static bool IsNotEven(this decimal value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this int     value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this uint    value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this long    value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this ulong   value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this short   value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this ushort  value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this float   value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this double  value) => value.Parity() != Enums.Parity.Even;
+        [Pure] public static bool IsNotEven(this decimal value) => value.Parity() != Enums.Parity.Even;
 
         #endregion
 
