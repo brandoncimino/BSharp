c4090f8404149984fb595bbac2a15ea894be65fa
Brandon Cimino <bcimino@cccis.com>
2022-02-04 20:46:04 -0600

    added javadocs

--- Clerical/ImmutableFileData.cs
+++ Clerical/ImmutableFileData.cs
@@ -2,6 +2,11 @@ using FluentValidation;
 
 namespace FowlFever.Clerical;
 
+/// <summary>
+/// An <see cref="ValidatedFileData{T}"/> that, once <see cref="ValidatedFileData{T}.Serialize"/>d or <see cref="ValidatedFileData{T}.Deserialize"/>d,
+/// cannot be modified.
+/// </summary>
+/// <inheritdoc/>
 public abstract class ImmutableFileData<T> : ValidatedFileData<T> {
     protected ImmutableFileData(FileInfo fileInfo) : base(fileInfo) { }
     protected ImmutableFileData(T        data, FileInfo fileInfo) : base(data, fileInfo) { }
