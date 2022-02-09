8ada0f04486f814c615244406f8f0f77c39864e3
Brandon Cimino <bcimino@cccis.com>
2022-02-08 20:50:54 -0600

    Added `ValidatedString`

---
+++ Clerical/Validated/ValidatedString.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace FowlFever.Clerical.Validated;
+
+/// <summary>
+/// A base class for records that are wrappers around a single <see cref="string"/> value.
+/// </summary>
+/// <remarks>
+/// The goal of this class is that working with a <see cref="ValidatedString"/> should be as close to working with a normal <see cref="string"/>
+/// as possible.
+/// <p/>
+/// To this end, <see cref="ValidatedString"/> provides a simple <see cref="IEquatable{T}"/> implementation
+/// and an <c>implicit operator</c> cast to <see cref="string"/>.
+/// </remarks>
+/// <param name="Value">the wrapped <see cref="string"/></param>
+public abstract record ValidatedString(string Value) : IEquatable<string> {
+    public bool Equals(string other) => Value == other;
+
+    public static implicit operator string(ValidatedString self) {
+        return self.Value;
+    }
+}
