aa05b7de25b927c12491cfaca7894e8dc1158e75
Brandon Cimino <bcimino@cccis.com>
2022-02-10 11:23:20 -0600

    - Added  javadocs to Fix-LineEndings - Properly utilizied the `$Path` parameter in `Fix-LineEndings` - Added the ability to opt-out of `-Recurse` in `Fix-LineEndings`

--- Fix-LineEndings.ps1
+++ Fix-LineEndings.ps1
@@ -1,7 +1,21 @@
+<#
+.SYNOPSIS
+    Re-writes every file in a directory so that:
+      - All of lines end in `\n` (aka [https://en.wikipedia.org/wiki/Newline#Representation](LF))
+      - There is no trailing newline character at the end of the file
+      - The encoding is [https://en.wikipedia.org/wiki/Byte_order_mark](UTF8NoBOM)
+#>
 function Fix-LineEndings(
-    $Path = "."
+    # The path or paths to the files or directories that will have their line endings fixed.
+    #   - Passed through as the `Get-ChildItem -Path` parameter.
+    [Parameter(ValueFromPipeline)]
+    [string[]]$Path = ".",
+
+    # Allows you to _opt-out_ of the `Get-ChildItem`'s `-Recurse` option.
+    [switch]$NoRecurse
 ){
-    $files = Get-ChildItem . -Recurse -File
+    $shouldRecur = !$NoRecurse
+    $files = Get-ChildItem -Path $Path -Recurse:$shouldRecur -File
     foreach($f in $files){
         $lines = $f | Get-Content
         $joined = $lines -join "`n"
