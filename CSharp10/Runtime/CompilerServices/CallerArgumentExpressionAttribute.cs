8b6c42b8daa19694c2a26155150eacd3d84c3330
Brandon Cimino <bcimino@cccis.com>
2022-05-02 12:30:06 -0500

    Added `CSharp10` project with backwards compatibility for NullableAttributes.cs and CallerArgumentExpressionAttribute.cs

---
+++ CSharp10/Runtime/CompilerServices/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Runtime.CompilerServices; 
+
+[AttributeUsage(AttributeTargets.Parameter, Inherited = default, AllowMultiple = false)]
+internal sealed class CallerArgumentExpressionAttribute : Attribute
+{
+#if !NET6_0_OR_GREATER
+    public CallerArgumentExpressionAttribute(string parameterName)
+    {
+        ParameterName = parameterName;
+    }
+
+    public string ParameterName { get; }
+#endif
+}
