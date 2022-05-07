0d92f9f232b98998715da83fc2553db145bee9e5
Brandon Cimino <bcimino@cccis.com>
2022-05-07 00:10:53 -0500

    Exposed `CSharp10` to `FowlFever.Clerical`

--- CSharp10/AssemblyInfo.cs
+++ CSharp10/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Runtime.CompilerServices;
-using System.Diagnostics.CodeAnalysis;
 
 [assembly: InternalsVisibleTo("FowlFever.BSharp")]
+[assembly: InternalsVisibleTo("FowlFever.Clerical")]
