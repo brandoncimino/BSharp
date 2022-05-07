84090ee34d649a4d2f5964392762b29a1d170cf6
Brandon Cimino <bcimino@cccis.com>
2022-05-07 00:36:27 -0500

    - Added additional interfaces to `ValidatedString` - Removed `==` and `!=` operators vs `string` because we already have an implicit conversion to `string`

--- Clerical/Validated/ValidatedString.cs
+++ Clerical/Validated/ValidatedString.cs
@@ -1,7 +1,7 @@
 namespace FowlFever.Clerical.Validated;
 
 /// <summary>
-/// A base class for records that are wrappers around a single <see cref="string"/> value.
+/// A base class for wrappers around a single <see cref="string"/> value.
 /// </summary>
 /// <remarks>
 /// The goal of this class is that working with a <see cref="ValidatedString"/> should be as close to working with a normal <see cref="string"/>
@@ -9,26 +9,47 @@ namespace FowlFever.Clerical.Validated;
 /// <p/>
 /// To this end, <see cref="ValidatedString"/> provides a simple <see cref="IEquatable{T}"/> implementation
 /// and an <c>implicit operator</c> cast to <see cref="string"/>.
+///
+/// 📎 We don't need to be exhaustive with our operator overloads like <c>==</c> and <c>!=</c> because of the implicit conversion to <see cref="string"/>.
+/// However, we do have to specify <see cref="Equals(string)"/>, <see cref="CompareTo(string)"/>, etc. methods in order to satisfy the <see cref="IEquatable{T}"/> interfaces, etc.
 /// </remarks>
-/// <param name="Value">the wrapped <see cref="string"/></param>
-public abstract record ValidatedString(string Value) : IEquatable<string>, IComparable<string>, IComparable {
-    public bool Equals(string other) => Value == other;
+public abstract class ValidatedString : IEquatable<string>,
+                                        IComparable<string>,
+                                        IComparable,
+                                        IEquatable<ValidatedString>,
+                                        IComparable<ValidatedString> {
+    /// <summary>
+    /// The wrapped <see cref="string"/>
+    /// </summary>
+    public readonly string Value;
 
-    public static implicit operator string(ValidatedString self) {
-        return self.Value;
+    protected ValidatedString(string value) {
+        Value = value;
     }
 
-    public static bool operator ==(ValidatedString a, string b) => a.Equals(b);
-    public static bool operator !=(ValidatedString a, string b) => !(a == b);
+    #region Operators
 
-    public static bool operator ==(string a, ValidatedString b) => a.Equals(b);
-    public static bool operator !=(string a, ValidatedString b) => !(a == b);
+    public static implicit operator string(ValidatedString           self)                        => self.Value;
+    public static                   bool operator ==(ValidatedString self, ValidatedString other) => self.Equals(other);
+    public static                   bool operator !=(ValidatedString self, ValidatedString other) => self.Equals(other);
 
-    public int CompareTo(string other) {
-        return string.Compare(Value, other, StringComparison.Ordinal);
-    }
+    #endregion
 
-    public int CompareTo(object obj) {
-        return Value.CompareTo(obj);
-    }
+    #region CompareTo
+
+    public int CompareTo(string          other) => string.Compare(Value, other,       StringComparison.Ordinal);
+    public int CompareTo(ValidatedString other) => string.Compare(Value, other.Value, StringComparison.Ordinal);
+    public int CompareTo(object          obj)   => Value.CompareTo(obj);
+
+    #endregion
+
+    #region Equals
+
+    public          bool Equals(string           other) => Value == other;
+    public          bool Equals(ValidatedString? other) => Value.Equals(other?.Value);
+    public override bool Equals(object?          obj)   => Value.Equals(obj);
+
+    #endregion
+
+    public override int GetHashCode() => Value.GetHashCode();
 }
