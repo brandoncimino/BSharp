59f8a3f739c9b470f65f76cbfc7bc4d4433f8663
Brandon Cimino <bcimino@cccis.com>
2022-05-07 00:40:30 -0500

    `record` -> `class`

--- Clerical/Validated/FileNamePart.cs
+++ Clerical/Validated/FileNamePart.cs
@@ -6,16 +6,14 @@ using FowlFever.BSharp.Exceptions;
 namespace FowlFever.Clerical.Validated;
 
 /// <summary>
-/// Guarantees that a string:
+/// Ensures that a string:
 /// <ul>
-/// <li>Doesn't contain <see cref="DirectorySeparator"/>s</li>
+/// <li>Is a valid <see cref="PathPart"/></li>
 /// <li>Doesn't contain whitespace</li>
 /// <li>Doesn't contain periods</li>
-/// <li>TODO: Doesn't contain any <see cref="Path.GetInvalidPathChars"/></li>
-/// <li>TODO: Doesn't contain any <see cref="Path.GetInvalidFileNameChars"/></li>
 /// </ul>
 /// </summary>
-public record FileNamePart : PathPart {
+public class FileNamePart : PathPart {
     public FileNamePart(string value) : base(value) {
         var whitespacePattern = new Regex(@"\s");
         var methodName        = $"new {GetType().Name}";
