bf994e1e8b83dafe2d43c578c82c30eacf46344b
Brandon Cimino <bcimino@cccis.com>
2022-05-25 15:25:41 -0500

    Added the ability to specify an alternate type for the property in a `BackerForAttribute`

--- BSharp/Attributes/BackerForAttribute.cs
+++ BSharp/Attributes/BackerForAttribute.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Reflection;
+
+using FowlFever.BSharp.Reflection;
 
 using JetBrains.Annotations;
 
@@ -6,10 +9,19 @@ namespace FowlFever.BSharp.Attributes {
     [PublicAPI]
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
     public class BackerForAttribute : BrandonAttribute {
-        public string BackedPropertyName { get; }
+        public string BackedPropertyName       { get; }
+        public Type?  BackedPropertyOwningType { get; }
 
-        public BackerForAttribute(string backedPropertyName) {
-            BackedPropertyName = backedPropertyName;
+        public BackerForAttribute(string backedPropertyName) => BackedPropertyName = backedPropertyName;
+
+        public BackerForAttribute(Type backedPropertyOwningType, string backedPropertyName) {
+            BackedPropertyName       = backedPropertyName;
+            BackedPropertyOwningType = backedPropertyOwningType;
+        }
+
+        public PropertyInfo GetBackedProperty(VariableInfo annotatedVariable) {
+            var owningType = BackedPropertyOwningType ?? annotatedVariable.MustGetReflectedType();
+            return owningType.GetRuntimeProperty(BackedPropertyName);
         }
     }
 }
