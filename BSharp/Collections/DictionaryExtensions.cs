f89cb58037ed8a8eaf53ff53c7d3519364f948fd
Brandon Cimino <bcimino@cccis.com>
2022-05-25 14:30:03 -0500

    Cleaned `DictionaryExtensions` and moved `GetOrAddLazily` to it

--- BSharp/Collections/DictionaryExtensions.cs
+++ BSharp/Collections/DictionaryExtensions.cs
@@ -1,42 +1,70 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
-namespace FowlFever.BSharp.Collections {
-    public static class DictionaryExtensions {
-        public static TValue? GetOrDefault<TKey, TValue>(
-            this IDictionary<TKey, TValue> dic,
-            TKey                           key,
-            TValue?                        fallback = default
-        ) {
-            if (dic == null) {
-                throw new ArgumentNullException(nameof(dic));
-            }
+using FowlFever.BSharp.Exceptions;
 
-            if (key == null) {
-                throw new ArgumentNullException(nameof(key));
-            }
+namespace FowlFever.BSharp.Collections;
 
-            return dic.ContainsKey(key) ? dic[key] : fallback;
+/// <summary>
+/// TODO: Move extensions from <see cref="CollectionUtils"/> into here
+/// </summary>
+public static class DictionaryExtensions {
+    public static TValue? GetOrDefault<TKey, TValue>(
+        this IDictionary<TKey, TValue> dic,
+        TKey                           key,
+        TValue?                        fallback = default
+    ) {
+        if (dic == null) {
+            throw new ArgumentNullException(nameof(dic));
         }
 
-        public static TValue? GetOrDefault<TKey, TValue>(
-            this IDictionary<TKey, TValue> dic,
-            TKey                           key,
-            Func<TValue>                   fallbackSupplier
-        ) {
-            if (dic == null) {
-                throw new ArgumentNullException(nameof(dic));
-            }
-
-            if (key == null) {
-                throw new ArgumentNullException(nameof(key));
-            }
-
-            if (fallbackSupplier == null) {
-                throw new ArgumentNullException(nameof(fallbackSupplier));
-            }
-
-            return dic.ContainsKey(key) ? dic[key] : fallbackSupplier.Invoke();
+        if (key == null) {
+            throw new ArgumentNullException(nameof(key));
         }
+
+        return dic.ContainsKey(key) ? dic[key] : fallback;
+    }
+
+    public static TValue? GetOrDefault<TKey, TValue>(
+        this IDictionary<TKey, TValue> dic,
+        TKey                           key,
+        Func<TValue>                   fallbackSupplier
+    ) {
+        if (dic == null) {
+            throw new ArgumentNullException(nameof(dic));
+        }
+
+        if (key == null) {
+            throw new ArgumentNullException(nameof(key));
+        }
+
+        if (fallbackSupplier == null) {
+            throw new ArgumentNullException(nameof(fallbackSupplier));
+        }
+
+        return dic.ContainsKey(key) ? dic[key] : fallbackSupplier.Invoke();
+    }
+
+    /// <summary>
+    /// Simplifies the implementation of <see cref="ConcurrentDictionary{TKey,TValue}"/>-based caching by "hiding" the <see cref="Lazy{T}"/>-ness of the <see cref="ConcurrentDictionary{TKey,TValue}.Values"/>.
+    /// </summary>
+    /// <remarks>
+    /// The <typeparamref name="TIn"/> and <typeparamref name="TOut"/> type parameters are used to overcome the lack of co- / contra-variance in <see cref="Lazy{T}"/>.
+    /// </remarks>
+    /// <param name="concurrentDictionary">the actual holder for all of the cached values</param>
+    /// <param name="key">the <see cref="KeyValuePair{TKey,TValue}.Key"/> of the entry</param>
+    /// <param name="valueFactory">the <see cref="Func{TResult}"/> that will generate a new <see cref="KeyValuePair{TKey,TValue}.Value"/> if <paramref name="key"/> isn't found</param>
+    /// <typeparam name="TKey">the actual <see cref="ConcurrentDictionary{TKey,TValue}"/> key type</typeparam>
+    /// <typeparam name="TVal">the actual <see cref="ConcurrentDictionary{TKey,TValue}"/> value type</typeparam>
+    /// <typeparam name="TIn">the type of the desired <paramref name="key"/></typeparam>
+    /// <typeparam name="TOut">the type of the <paramref name="valueFactory"/> output</typeparam>
+    /// <returns>the existing <typeparamref name="TVal"/> or the result of <paramref name="valueFactory"/></returns>
+    public static TVal GetOrAddLazily<TKey, TVal, TIn, TOut>(this ConcurrentDictionary<TKey, Lazy<TVal>> concurrentDictionary, [NotNull] TIn key, Func<TIn, TOut> valueFactory)
+        where TIn : TKey
+        where TOut : TVal {
+        Must.NotBeNull(key);
+        return concurrentDictionary.GetOrAdd(key, k => new Lazy<TVal>(() => valueFactory((TIn)k!))).Value;
     }
 }
