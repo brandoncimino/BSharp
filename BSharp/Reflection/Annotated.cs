0d50cb8ee57b6571bf3fe1c6824bd39fe2694366
Brandon Cimino <bcimino@cccis.com>
2022-05-25 11:05:06 -0500

    Added `Annotated`

---
+++ BSharp/Reflection/Annotated.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace FowlFever.BSharp.Reflection;
+
+/// <summary>
+/// Joins together <see cref="Attribute"/>s with their target <see cref="MemberInfo"/>.
+/// </summary>
+/// <typeparam name="TMember">the annotated <see cref="MemberInfo"/> type</typeparam>
+/// <typeparam name="TAttribute">the <see cref="Attribute"/> type</typeparam>
+public class Annotated<TMember, TAttribute>
+    where TMember : MemberInfo
+    where TAttribute : Attribute {
+    public TMember                 Member     { get; }
+    public IEnumerable<TAttribute> Attributes { get; }
+
+    public Annotated(TMember member, IEnumerable<TAttribute>? attributes = null) {
+        Member     = member;
+        Attributes = attributes ?? Member.GetCustomAttributes<TAttribute>();
+    }
+}
