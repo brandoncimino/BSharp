65376879ee7092019cb9366397074603e987bcbb
Brandon Cimino <bcimino@cccis.com>
2022-05-27 15:06:21 -0500

    Fleshed out constructors

--- BSharp/Collections/ConcurrentSet.cs
+++ BSharp/Collections/ConcurrentSet.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace FowlFever.BSharp.Collections;
@@ -15,12 +16,45 @@ namespace FowlFever.BSharp.Collections;
 /// </remarks>
 /// <typeparam name="T">the type of the entries in the <see cref="ISet{T}"/></typeparam>
 public class ConcurrentSet<T> : ISet<T> {
-    private readonly ConcurrentDictionary<T, byte> _myDic = new ConcurrentDictionary<T, byte>();
+    private readonly ConcurrentDictionary<T, byte> _myDic;
     /// <summary>
     /// TODO: see if there's a more efficient way to retrieve the <see cref="ConcurrentDictionary{TKey,TValue}.Keys"/> as a <see cref="ISet{T}"/>
     /// </summary>
     private ISet<T> KeySet => _myDic.Keys.ToImmutableHashSet();
-    private NotSupportedException NotSupported([CallerMemberName] string? unsupportedMethod = default) => new($"✋ {unsupportedMethod} isn't supported by {GetType().Name}!");
+    private static IEnumerable<KeyValuePair<T, byte>> AsEntries(IEnumerable<T>                stuff)                       => stuff.Select(it => new KeyValuePair<T, byte>(it, default));
+    private        NotSupportedException              NotSupported([CallerMemberName] string? unsupportedMethod = default) => new($"✋ {unsupportedMethod} isn't supported by {GetType().Name}!");
+
+    #region Constructors
+
+    public ConcurrentSet() => _myDic = new ConcurrentDictionary<T, byte>();
+
+    public ConcurrentSet(IEnumerable<T> stuff) {
+        _myDic = new ConcurrentDictionary<T, byte>(AsEntries(stuff));
+    }
+
+    public ConcurrentSet(IEnumerable<T> stuff, IEqualityComparer<T> equalityComparer) {
+        _myDic = new ConcurrentDictionary<T, byte>(AsEntries(stuff), equalityComparer);
+    }
+
+    public ConcurrentSet(IEqualityComparer<T> equalityComparer) {
+        _myDic = new ConcurrentDictionary<T, byte>(equalityComparer);
+    }
+
+    public ConcurrentSet(int concurrencyLevel, IEnumerable<T> stuff, IEqualityComparer<T> equalityComparer) {
+        _myDic = new ConcurrentDictionary<T, byte>(concurrencyLevel, AsEntries(stuff), equalityComparer);
+    }
+
+    public ConcurrentSet(int concurrencyLevel, int capacity) {
+        _myDic = new ConcurrentDictionary<T, byte>(concurrencyLevel, capacity);
+    }
+
+    public ConcurrentSet(int concurrencyLevel, int capacity, IEqualityComparer<T> equalityComparer) {
+        _myDic = new ConcurrentDictionary<T, byte>(concurrencyLevel, capacity, equalityComparer);
+    }
+
+    #endregion
+
+    #region ISet<T> Implementation
 
     public IEnumerator<T>   GetEnumerator()                          => _myDic.Keys.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator()                          => _myDic.Keys.GetEnumerator();
@@ -37,6 +71,8 @@ public class ConcurrentSet<T> : ISet<T> {
     public bool             Remove(T   item)                         => _myDic.TryRemove(item, out _);
     public int              Count                                    => _myDic.Count;
     bool ICollection<T>.    IsReadOnly                               => false;
+
+    #endregion
 
     #region Unsupported ISet<T> Methods
 
