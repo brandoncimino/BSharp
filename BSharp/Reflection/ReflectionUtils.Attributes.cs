ab73bcedabafa06b73879cbd82b14b6941f47a63
Brandon Cimino <bcimino@cccis.com>
2022-05-27 19:13:44 -0500

    convenience overloads for `GetAnnotated` that use specific `MemberInfo` types

--- BSharp/Reflection/ReflectionUtils.Attributes.cs
+++ BSharp/Reflection/ReflectionUtils.Attributes.cs
@@ -6,20 +6,18 @@ using System.Reflection;
 namespace FowlFever.BSharp.Reflection;
 
 public static partial class ReflectionUtils {
-    private static bool IsAnnotated(MemberInfo member, object annotationType) {
-        return member.IsDefined((annotationType as Type)!);
-    }
-
-    private static readonly Lazy<MethodInfo> IsAnnotatedMember = new Lazy<MethodInfo>(() => typeof(ReflectionUtils).GetMethod(nameof(IsAnnotated)));
+    #region Attributes
 
     /// <summary>
     /// A <see cref="MemberFilter"/> for members where a certain <see cref="Attribute"/> <see cref="MemberInfo.IsDefined"/>.
     /// </summary>
-    /// <remarks>
-    /// This roundabout way of constructing a <see cref="MemberFilter"/> is, apparently, much more efficient: see <a href="https://www.youtube.com/watch?v=er9nD-usM1A">Achieving compile-time performance with Reflection in C#</a>
-    /// </remarks>
-    internal static readonly MemberFilter AnnotatedMemberFilter = (MemberFilter)Delegate.CreateDelegate(typeof(MemberFilter), IsAnnotatedMember.Value);
+    internal static readonly MemberFilter AnnotatedMemberFilter = (member, annotationType) => member.IsDefined((annotationType as Type)!);
 
+    /// <summary>
+    /// Compares <see cref="MemberInfo"/>s by their <see cref="MemberInfo.MetadataToken"/>.
+    ///
+    /// TODO: Would an extension method of "MetadataCompare" be useful? or is that silly when we could just write <c>a.MetadataToken.Equals(b.MetadataToken)</c>?
+    /// </summary>
     internal class MetadataTokenComparer : Comparer<MemberInfo>, IEqualityComparer<MemberInfo> {
         public bool Equals(MemberInfo x, MemberInfo y) {
             return x.MetadataToken == y.MetadataToken;
@@ -44,19 +42,23 @@ public static partial class ReflectionUtils {
     /// but the performance of <see cref="Type.FindMembers"/> is <b>considerably</b> better:
     /// 
     /// <code><![CDATA[
+    /// 
     /// |               Method |        Mean |     Error |    StdDev |
     /// |--------------------- |------------:|----------:|----------:|
     /// |         ViaIsDefined | 2,237.31 ns | 33.473 ns | 53.092 ns |
     /// | Runtime_ViaIsDefined | 2,224.79 ns | 25.931 ns | 20.245 ns |
     /// |            ViaFilter |    78.97 ns |  0.813 ns |  0.761 ns |
+    /// 
     /// ]]></code>
+    ///
+    /// This makes it worthwhile to construct the silly <see cref="AnnotatedMemberFilter"/>.
     /// 
     /// </remarks>
     /// <param name="type">the original <see cref="Type"/></param>
     /// <param name="bindingAttr">determines how the search is performed</param>
     /// <typeparam name="TMember">the <see cref="MemberTypes"/> you're searching for</typeparam>
     /// <typeparam name="TAttribute">the desired <see cref="Attribute"/></typeparam>
-    /// <returns></returns>
+    /// <returns>the members with the specified <see cref="Attribute"/></returns>
     public static IEnumerable<TMember> FindMembersWithAttribute<TMember, TAttribute>(this Type type, BindingFlags bindingAttr = default)
         where TMember : MemberInfo
         where TAttribute : Attribute {
@@ -79,8 +81,35 @@ public static partial class ReflectionUtils {
     }
 
     /// <inheritdoc cref="CustomAttributeExtensions.GetCustomAttributes{T}(MemberInfo, bool)"/>
+    /// <returns>An <see cref="Annotated{TMember,TAttribute}"/> instance containing the results.</returns>
     public static Annotated<TMember, TAttribute> GetAnnotated<TMember, TAttribute>(this TMember member, bool inherit = true)
         where TMember : MemberInfo
         where TAttribute : Attribute
         => new(member);
+
+    /// <inheritdoc cref="GetAnnotated{TMember,TAttribute}"/>
+    public static Annotated<PropertyInfo, T> GetAnnotated<T>(this PropertyInfo property, bool inherit = true)
+        where T : Attribute => new(property);
+
+    /// <inheritdoc cref="GetAnnotated{TMember,TAttribute}"/>
+    public static Annotated<FieldInfo, T> GetAnnotated<T>(this FieldInfo field, bool inherit = true)
+        where T : Attribute => new(field);
+
+    /// <inheritdoc cref="GetAnnotated{TMember,TAttribute}"/>
+    public static Annotated<MethodInfo, T> GetAnnotated<T>(this MethodInfo method, bool inherit = true)
+        where T : Attribute => new(method);
+
+    /// <inheritdoc cref="GetAnnotated{TMember,TAttribute}"/>
+    public static Annotated<VariableInfo, T> GetAnnotated<T>(this VariableInfo variable, bool inherit = true)
+        where T : Attribute => new(variable);
+
+    /// <inheritdoc cref="GetAnnotated{TMember,TAttribute}"/>
+    public static Annotated<ConstructorInfo, T> GetAnnotated<T>(this ConstructorInfo constructor, bool inherit = true)
+        where T : Attribute => new(constructor);
+
+    /// <inheritdoc cref="GetAnnotated{TMember,TAttribute}"/>
+    public static Annotated<EventInfo, T> GetAnnotated<T>(this EventInfo eventInfo, bool inherit = true)
+        where T : Attribute => new(eventInfo);
+
+    #endregion
 }
