481ffbda432dcc9e64fe0545c7e6167d218cad4b
Brandon Cimino <bcimino@cccis.com>
2022-05-29 22:28:57 -0500

    Added more `Lazily` extensions

--- BSharp/Sugar/Lazily.cs
+++ BSharp/Sugar/Lazily.cs
@@ -4,4 +4,10 @@ namespace FowlFever.BSharp.Sugar;
 
 public static class Lazily {
     public static Lazy<T> Get<T>(Func<T> supplier) => new(supplier);
+
+    public static Lazy<T> Get<T>(T value) => new(() => value);
+
+    public static Lazy<TLazy> Get<TFIn, TLazy, TFOut>(Func<TFIn, TFOut> supplier, TFIn input)
+        where TFOut : TLazy
+        => new(() => supplier(input));
 }
