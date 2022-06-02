3ea4037f1acaf44f948350e00bb31c7ef1b0040f
Brandon Cimino <bcimino@cccis.com>
2022-06-01 21:20:51 -0500

    Added ability to align header cells

--- BSharp/Strings/Tabler/Cell.cs
+++ BSharp/Strings/Tabler/Cell.cs
@@ -6,14 +6,19 @@ namespace FowlFever.BSharp.Strings.Tabler;
 
 [PublicAPI]
 public readonly record struct Cell : IPrettifiable {
-    public Optional<object?> Value { get; }
+    public Optional<object?> Value    { get; }
+    public bool              IsHeader { get; }
 
-    public Cell(object? value) {
+    public Cell(object? value, bool isHeader = false) {
         // this prevents nesting of cells, i.e. Cell(Cell(5)) => Cell(5)
-        Value = value is Cell cell ? cell.Value : value;
+        Value    = value is Cell cell ? cell.Value : value;
+        IsHeader = isHeader;
     }
 
     public string Prettify(PrettificationSettings? settings = default) {
-        return Value.Prettify(settings.Resolve() with { PreferredLineStyle = LineStyle.Single });
+        settings = settings.Resolve();
+        settings = settings with { PreferredLineStyle = LineStyle.Single };
+        return Value.Prettify(settings)
+                    .Align(IsHeader ? settings.TableHeaderAlignment : default);
     }
 }
