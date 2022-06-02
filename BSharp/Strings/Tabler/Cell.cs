1b1c0744e5dd36f1ea7c0447c68b6c0783461b18
Brandon Cimino <bcimino@cccis.com>
2022-06-01 21:12:02 -0500

    Simplified `Cell`

--- BSharp/Strings/Tabler/Cell.cs
+++ BSharp/Strings/Tabler/Cell.cs
@@ -1,24 +1,19 @@
-using System;
+using FowlFever.BSharp.Optional;
 
 using JetBrains.Annotations;
 
 namespace FowlFever.BSharp.Strings.Tabler;
 
 [PublicAPI]
-public record Cell : IPrettifiable {
-    private          PrettificationSettings? Settings { get; }
-    public           object?                 Value    { get; }
-    private readonly Lazy<string>            PrettyString;
+public readonly record struct Cell : IPrettifiable {
+    public Optional<object?> Value { get; }
 
-    public Cell(object value, PrettificationSettings? settings = default) {
+    public Cell(object? value) {
         // this prevents nesting of cells, i.e. Cell(Cell(5)) => Cell(5)
         Value = value is Cell cell ? cell.Value : value;
-        //TODO: Support multiline cells...hoo boy
-        Settings     = (settings ?? PrettificationSettings.Default) with { PreferredLineStyle = LineStyle.Single };
-        PrettyString = new Lazy<string>(() => Value.Prettify(Settings));
     }
 
     public string Prettify(PrettificationSettings? settings = default) {
-        return settings == null ? PrettyString.Value : Value.Prettify(settings with { PreferredLineStyle = LineStyle.Single });
+        return Value.Prettify(settings.Resolve() with { PreferredLineStyle = LineStyle.Single });
     }
 }
