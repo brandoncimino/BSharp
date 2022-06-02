641afac7923a2ca54bc9c4778547d712c8907117
Brandon Cimino <bcimino@cccis.com>
2022-06-01 20:26:25 -0500

    Added an optional `Details` field

--- BSharp/Exceptions/RejectionException.cs
+++ BSharp/Exceptions/RejectionException.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace FowlFever.BSharp.Exceptions;
@@ -9,13 +10,15 @@ namespace FowlFever.BSharp.Exceptions;
 /// A more expressive <see cref = "ArgumentException" /> used by <see cref = "Must" />.
 /// </summary>
 public class RejectionException : ArgumentException {
-    private const string Shrug                         = "🤷";
-    private const string ReasonIcon                    = "🙅";
-    private const string RejectIcon                    = "🚮";
-    private const string NullIcon                      = "⛔";
-    private const string NoReason                      = $"<reason not specified {Shrug}>";
-    private const string NoRejector                    = $"<somebody {Shrug}>";
-    private const int    DefaultValueStringLengthLimit = 30;
+    private const           string Shrug      = "🤷";
+    private const           string ReasonIcon = "🙅";
+    private const           string RejectIcon = "🚮";
+    private static readonly string Indent     = new(' ', RejectIcon.Length);
+    private const           string NullIcon   = "⛔";
+    private const           string NoReason   = $"<reason not specified {Shrug}>";
+    private const           string NoRejector = $"<somebody {Shrug}>";
+
+    private const int DefaultValueStringLengthLimit = 30;
 
     public object? ActualValue { get; }
 
@@ -34,14 +37,37 @@ public class RejectionException : ArgumentException {
         init => Data[nameof(Reason)] = value;
     }
 
-    public int ValueStringLengthLimit { get; init; } = DefaultValueStringLengthLimit;
+    public string? Details {
+        get => Data[nameof(Details)]?.ToString();
+        init => Data[nameof(Details)] = value;
+    }
 
-    public string Preamble => $"{RejectIcon} `{RejectedBy ?? NoRejector}` rejected `{ParamName}`";
+    private int ValueStringLengthLimit { get; init; } = DefaultValueStringLengthLimit;
 
-    public override string Message => $"{Preamble}: {Reason ?? NoReason}\nActual: {ActualValueString}";
+    private string Preamble => $"`{RejectedBy ?? NoRejector}` rejected `{ParamName}`";
+
+    public override string Message {
+        get {
+            var lines = new[] {
+                            Preamble,
+                            Details,
+                            $"Reason: {Reason ?? NoReason}",
+                            $"Actual: {ActualValueString}",
+                        }
+                        .Where(it => string.IsNullOrEmpty(it) == false)
+                        .Select(
+                            (ln, i) => {
+                                var indent = i == 0 ? RejectIcon : Indent;
+                                return $"{indent} {ln}";
+                            }
+                        );
+            return string.Join("\n", lines);
+        }
+    }
 
     public RejectionException(
         object? actualValue,
+        string? details = default,
         [CallerArgumentExpression("actualValue")]
         string? parameterName = default,
         [CallerMemberName]
@@ -50,6 +76,7 @@ public class RejectionException : ArgumentException {
         Exception? innerException = default
     ) : base(null, parameterName, innerException) {
         ActualValue       = actualValue;
+        Details           = details;
         ActualValueString = actualValue?.ToString();
         RejectedBy        = rejectedBy;
         Reason            = reason;
