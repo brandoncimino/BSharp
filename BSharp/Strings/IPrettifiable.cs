083cdb6d9bf85e4d0db968b11895b11f44608bdc
Brandon Cimino <bcimino@cccis.com>
2022-06-09 22:40:14 -0500

    added javadocs

--- BSharp/Strings/IPrettifiable.cs
+++ BSharp/Strings/IPrettifiable.cs
@@ -5,6 +5,11 @@ namespace FowlFever.BSharp.Strings {
     /// Allows an object to specifically designate a method for use by <see cref="Prettification"/>.
     /// </summary>
     public interface IPrettifiable {
+        /// <summary>
+        /// Generates a fancy <see cref="string"/> representation of this object, taking <see cref="PrettificationSettings"/> into account.
+        /// </summary>
+        /// <param name="settings">a set of <see cref="PrettificationSettings"/></param>
+        /// <returns>a pretty <see cref="string"/></returns>
         [System.Diagnostics.Contracts.Pure]
         public string Prettify(PrettificationSettings? settings = default);
     }
