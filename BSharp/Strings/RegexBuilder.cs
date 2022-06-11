6ade38db5063aca09808bd73ebb3d07e05fc8415
Brandon Cimino <bcimino@cccis.com>
2022-06-11 16:03:25 -0500

    Added a base `RegexBuilder` class

---
+++ BSharp/Strings/RegexBuilder.cs
@@ -0,0 +1,30 @@
+using System.Text.RegularExpressions;
+
+namespace FowlFever.BSharp.Strings;
+
+/// <summary>
+/// A base class to construct builder-style <c>record</c>s for <see cref="System.Text.RegularExpressions.Regex"/> patterns. 
+/// </summary>
+public abstract record RegexBuilder : IHas<Regex> {
+    private string? _pattern;
+    /// <summary>
+    /// The <see cref="string"/> representation of the final <see cref="Regex"/>.
+    /// </summary>
+    public string Pattern => _pattern ??= BuildPattern();
+
+    private Regex?    _value;
+    Regex IHas<Regex>.Value => Regex;
+
+    /// <summary>
+    /// The final <see cref="System.Text.RegularExpressions.Regex"/> for this <see cref="RegexBuilder"/>.
+    /// </summary>
+    public Regex Regex => _value ??= Options.HasValue
+                                         ? new Regex(Pattern, Options.Value)
+                                         : new Regex(Pattern);
+
+    public RegexOptions? Options { get; init; }
+
+    protected abstract string BuildPattern();
+
+    public sealed override string ToString() => Pattern;
+}
