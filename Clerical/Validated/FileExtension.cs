42d92bca57abbeb649b5080f92461f27400dbb8f
Brandon Cimino <bcimino@cccis.com>
2022-06-11 13:49:08 -0500

    Updated to with an `AfterValidation` method

--- Clerical/Validated/FileExtension.cs
+++ Clerical/Validated/FileExtension.cs
@@ -1,23 +1,19 @@
 using System.Text.RegularExpressions;
 
-using FowlFever.BSharp.Exceptions;
+using FowlFever.BSharp;
 using FowlFever.BSharp.Strings;
 
 namespace FowlFever.Clerical.Validated;
 
 /// <summary>
-/// Ensures that <see cref="ValidatedString.Value"/>:
+/// Ensures that <see cref="Wrapped{}.Value"/>:
 /// <ul>
 /// <li>Doesn't contain whitespace</li>
 /// <li>Contains at most 1 leading period</li>
 /// </ul>
 /// </summary>
-public class FileExtension : PathPart {
+public record FileExtension(string Value) : FileNamePart(Value) {
     private static readonly Regex Pattern = new Regex(@"^\.[^\s\.]$");
 
-    public FileExtension(string value) : base(Fluff(value)) {
-        Must.Match(value, Pattern);
-    }
-
-    private static string Fluff(string value) => value.PrependIfMissing(".");
+    internal override string AfterValidation(string value) => value.PrependIfMissing(".");
 }
