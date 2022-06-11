32e8632da6135e0e0303d2535aeea043e48c81f2
Brandon Cimino <bcimino@cccis.com>
2022-06-11 16:09:28 -0500

    Added a builder for `RegexCharacterSet`

---
+++ BSharp/Strings/RegexCharacterSet.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using FowlFever.BSharp.Enums;
+
+namespace FowlFever.BSharp.Strings;
+
+internal sealed record RegexCharacterSet(IEnumerable<char> Inclusions, IEnumerable<char> Exclusions) : RegexBuilder {
+    private ImmutableArray<string> _inclusions = EscapeChars(Inclusions).ToImmutableArray();
+    private ImmutableArray<string> _exclusions = EscapeChars(Exclusions).ToImmutableArray();
+
+    private enum RegexCharacterSetStyle { Inclusive, Exclusive, Subtractive }
+
+    private bool HasInclusions => _inclusions.IsDefaultOrEmpty == false;
+    private bool HasExclusions => _exclusions.IsDefaultOrEmpty == false;
+
+    private RegexCharacterSetStyle Style => this switch {
+        { HasInclusions: true, HasExclusions: true }  => RegexCharacterSetStyle.Subtractive,
+        { HasInclusions: true, HasExclusions: false } => RegexCharacterSetStyle.Inclusive,
+        { HasInclusions: false, HasExclusions: true } => RegexCharacterSetStyle.Exclusive,
+        _                                             => throw new ArgumentException($"Must include 1+ {nameof(Inclusions)} or {nameof(Exclusions)}!"),
+    };
+
+    private static IEnumerable<string> EscapeChars(IEnumerable<char> chars) {
+        return chars.Select(it => it.ToString())
+                    .Select(Regex.Escape);
+    }
+
+    protected override string BuildPattern() {
+        return Style switch {
+            RegexCharacterSetStyle.Inclusive   => $"[{EscapeChars(Inclusions)}]",
+            RegexCharacterSetStyle.Exclusive   => $"[^{EscapeChars(Exclusions)}]",
+            RegexCharacterSetStyle.Subtractive => $"[{EscapeChars(Inclusions)}-[{EscapeChars(Exclusions)}]]",
+            _                                  => throw BEnum.UnhandledSwitch(Style),
+        };
+    }
+}
