8449e23b6d8e3ae9980f743165aa3ffd51a4e7e6
Brandon Cimino <bcimino@cccis.com>
2022-06-11 13:40:59 -0500

    Added `[Validator]` stuff to `FileNamePart`

--- Clerical/Validated/FileNamePart.cs
+++ Clerical/Validated/FileNamePart.cs
@@ -1,23 +1,24 @@
-using System.Text.RegularExpressions;
-
-using FowlFever.BSharp.Clerical;
 using FowlFever.BSharp.Exceptions;
+using FowlFever.BSharp.Strings;
+using FowlFever.Clerical.Fluffy;
 
 namespace FowlFever.Clerical.Validated;
 
 /// <summary>
-/// Ensures that a string:
-/// <ul>
-/// <li>Is a valid <see cref="PathPart"/></li>
-/// <li>Doesn't contain whitespace</li>
-/// <li>Doesn't contain periods</li>
-/// </ul>
+/// Represents one part of a <see cref="FileInfo"/>.<see cref="FileInfo.Name"/> - either the base name or a <see cref="FileExtension"/>.
+///
+/// Specifically, <see cref="FileNamePart"/>s subdivide <see cref="PathPart"/>s by periods. 
 /// </summary>
-public class FileNamePart : PathPart {
-    public FileNamePart(string value) : base(value) {
-        var whitespacePattern = new Regex(@"\s");
-        var methodName        = $"new {GetType().Name}";
-        Must.NotMatch(value, whitespacePattern,               methodName: methodName);
-        Must.NotMatch(value, BPath.DirectorySeparatorPattern, methodName: methodName);
-    }
+/// <example>
+/// <code><![CDATA[
+///                            [         FileName          ]
+/// C:\Users\bcimino\dev\BSharp\BSharp.sln.DotSettings.user
+///                            [BSharp]   [DotSettings]                 
+///                                   [sln]           [user]
+/// ]]></code>
+/// </example>
+public record FileNamePart(string Value) : PathPart(Value) {
+    internal override string Fluff(string value) => base.Fluff(value).TrimStart(".", 1);
+
+    [Validator] private void NoPeriods() => Must.NotContain(Value, ".");
 }
