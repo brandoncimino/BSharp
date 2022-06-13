c127e9315d4cd460a0052c0de1c55acc4ca2b59e
Brandon Cimino <bcimino@cccis.com>
2022-06-13 00:55:02 -0500

    replaced `Stack` with `ConcurrentStack` so that we can use the safe `TryPop`

--- BSharp/Strings/Indenter/JaggedIndenter.cs
+++ BSharp/Strings/Indenter/JaggedIndenter.cs
@@ -1,20 +1,20 @@
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 
 namespace FowlFever.BSharp.Strings.Indenter;
 
 public class JaggedIndenter : IStackIndenter {
-    public           string     IndentString { get; init; } = " ";
-    private readonly Stack<int> IndentStack = new();
-    public           int        CurrentIndent => IndentStack.Count;
-    public           int        IndentSize    { get; init; } = 2;
-    public           string     Render()      => IndentString.RepeatToLength(IndentStack.Sum());
+    public           string               IndentString { get; init; } = " ";
+    private readonly ConcurrentStack<int> IndentStack = new();
+    public           int                  CurrentIndent => IndentStack.Count;
+    public           int                  IndentSize    { get; init; } = 2;
+    public           string               Render()      => IndentString.RepeatToLength(IndentStack.Sum());
 
     public void Indent(int relativeIndent = 1) {
         var abs = relativeIndent.Abs();
         for (int i = 0; i < abs; i++) {
             if (relativeIndent < 0) {
-                IndentStack.Pop();
+                IndentStack.TryPop(out _);
             }
             else {
                 IndentStack.Push(IndentSize);
