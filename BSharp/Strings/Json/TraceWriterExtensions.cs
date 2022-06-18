31c354402d1e3a3bed0ca11b3ab242c67bf6c6ca
Brandon Cimino <bcimino@cccis.com>
2022-06-18 00:43:11 -0500

    moved `TraceLevel` parameters to the end 'cus they're annoying

--- BSharp/Strings/Json/TraceWriterExtensions.cs
+++ BSharp/Strings/Json/TraceWriterExtensions.cs
@@ -12,28 +12,28 @@ namespace FowlFever.BSharp.Strings.Json {
             Func<string>       message,
             int                indent    = 0,
             Exception?         exception = default
-        ) => traceWriter?.Trace(TraceLevel.Info, message, indent, exception);
+        ) => traceWriter?.Trace(message, indent, exception, TraceLevel.Info);
 
         public static void Error(
             this ITraceWriter? traceWriter,
             Func<string>       message,
             int                indent    = 0,
             Exception?         exception = default
-        ) => traceWriter?.Trace(TraceLevel.Error, message, indent, exception);
+        ) => traceWriter?.Trace(message, indent, exception, TraceLevel.Error);
 
         public static void Warning(
             this ITraceWriter? traceWriter,
             Func<string>       message,
             int                indent    = 0,
             Exception?         exception = default
-        ) => traceWriter?.Trace(TraceLevel.Warning, message, indent, exception);
+        ) => traceWriter?.Trace(message, indent, exception, TraceLevel.Warning);
 
         public static void Verbose(
             this ITraceWriter? traceWriter,
             Func<string>       message,
             int                indent    = 0,
             Exception?         exception = default
-        ) => traceWriter?.Trace(TraceLevel.Verbose, message, indent, exception);
+        ) => traceWriter?.Trace(message, indent, exception, TraceLevel.Verbose);
 
         /// <summary>
         /// <see cref="ITraceWriter.Trace"/>s a message, lazily evaluating the <paramref name="message"/> <see cref="Func{TResult}"/> only if the <see cref="ITraceWriter.LevelFilter"/> includes the desired <see cref="TraceLevel"/>.
@@ -42,33 +42,26 @@ namespace FowlFever.BSharp.Strings.Json {
         /// Also has other niceties like handling null <see cref="ITraceWriter"/>s, allowing the <paramref name="exception"/> to be omitted, etc.
         /// </remarks>
         /// <param name="traceWriter">the <see cref="ITraceWriter"/> that will be doing the logging</param>
-        /// <param name="level">the desired <see cref="TraceLevel"/> of the message</param>
         /// <param name="message">a <see cref="Func{TResult}"/> that will supply the string that gets logged</param>
         /// <param name="indent"></param>
         /// <param name="exception">an optional <see cref="Exception"/> to include in the logged message</param>
+        /// <param name="level">the desired <see cref="TraceLevel"/> of the message</param>
         public static void Trace(
             this ITraceWriter? traceWriter,
-            TraceLevel         level,
-            Func<string>       message,
+            Supplied<string>   message,
             int                indent    = 0,
-            Exception?         exception = default
+            Exception?         exception = default,
+            TraceLevel?        level     = default
         ) {
             if (traceWriter == null) {
                 return;
             }
 
-            if (traceWriter.LevelFilter >= level) {
-                traceWriter.Trace(level, message.Invoke().Indent(indent).JoinLines(), exception);
-            }
-        }
+            var lvl = level ?? traceWriter.GetDefaultLevel();
 
-        public static void Trace(
-            this ITraceWriter? traceWriter,
-            string             message,
-            int                indent    = 0,
-            Exception?         exception = default
-        ) {
-            traceWriter?.Trace(traceWriter.GetDefaultLevel(), message.Indent(indent).JoinLines(), exception);
+            if (traceWriter.LevelFilter >= lvl) {
+                traceWriter.Trace(lvl, message.Value?.Indent(indent).JoinLines()!, exception);
+            }
         }
 
         private static TraceLevel GetDefaultLevel(this ITraceWriter? traceWriter) {
