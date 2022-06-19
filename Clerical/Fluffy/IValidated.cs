ca4197220520ccf217030eaf8ad490a06b151c09
Brandon Cimino <bcimino@cccis.com>
2022-06-18 22:33:58 -0500

    added `IValidated` interface

---
+++ Clerical/Fluffy/IValidated.cs
@@ -0,0 +1,12 @@
+namespace FowlFever.Clerical.Fluffy;
+
+/// <summary>
+/// Defines a <see cref="ValidationTarget"/> that <see cref="IValidatorMethod{T}"/>s can be applied to.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IValidated<out T> {
+    /// <summary>
+    /// The actual value that <see cref="ValidatorMethod{T}"/>s are applied to.
+    /// </summary>
+    T ValidationTarget { get; }
+}
