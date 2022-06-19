8d2f7d411de12df9bd7380c4b89f22340cbff826
Brandon Cimino <bcimino@cccis.com>
2022-06-18 22:58:05 -0500

    small cleanup

--- Clerical/Fluffy/Validated.cs
+++ Clerical/Fluffy/Validated.cs
@@ -8,16 +8,37 @@ namespace FowlFever.Clerical.Fluffy;
 /// A <see cref="Wrapped{T}"/> value that will always call <see cref="Validator.Validate{T}"/> when it is constructed.
 /// </summary>
 /// <typeparam name="T">the type of the underlying <see cref="Wrapped{T}.Value"/></typeparam>
-public abstract record Validated<T> : Wrapped<T> {
-    public override T Value { get; }
+public abstract record Validated<T> : Wrapped<T>, IValidated<T> {
+    public sealed override T Value { get; }
 
     [SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
     protected Validated(T value) {
-        Value = BeforeValidation(value);
-        Validator.Validate(this);
-        Value = AfterValidation(Value);
+        Value = value;
+        Validate();
+
+        var (changed, fluffed) = TryFluff(value);
+
+        if (!changed) {
+            return;
+        }
+
+        Value = fluffed;
+        Validate();
     }
 
-    internal virtual T BeforeValidation(T value) => value;
-    internal virtual T AfterValidation(T  value) => value;
+    private void Validate() {
+        Validator.Validate(this);
+    }
+
+    internal virtual T Fluff(T value) {
+        return value;
+    }
+
+    private (bool, T) TryFluff(T value) {
+        var fluffed = Fluff(value);
+        var changed = value?.GetHashCode() != fluffed?.GetHashCode() && !Equals(value, fluffed);
+        return (changed, fluffed);
+    }
+
+    T IValidated<T>.ValidationTarget => Value;
 }
