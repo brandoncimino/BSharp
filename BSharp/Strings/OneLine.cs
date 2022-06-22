ed3605d3e55e53539ed2043d91285d4aa48a8450
Brandon Cimino <bcimino@cccis.com>
2022-06-22 02:19:09 -0500

    Added a `Length` property and some indexers

--- BSharp/Strings/OneLine.cs
+++ BSharp/Strings/OneLine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 using FowlFever.BSharp.Exceptions;
@@ -17,4 +18,15 @@ public sealed record OneLine : Wrapped<string> {
     public override string Value => InitValue;
 
     public OneLine(string value) => InitValue = value;
+
+    /// <summary>
+    /// <inheritdoc cref="string.Length"/>
+    /// </summary>
+    public int Length => Value.Length;
+
+    /// <inheritdoc cref="string.this[int]"/>
+    public char this[int index] => Value[index];
+
+    /// <inheritdoc cref="string.Substring(int,int)"/>
+    public OneLine this[Range range] => new(Value[range]);
 }
