9c60cba82d4bd4be4a9f9cc69f5d24f14ee9f335
Brandon Cimino <bcimino@cccis.com>
2022-06-22 01:06:44 -0500

    Added `CharExtensions`

---
+++ BSharp/Strings/CharExtensions.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FowlFever.BSharp.Strings;
+
+public static class CharExtensions {
+    private const           string LineBreakCharacters = "\n\r";
+    public static           bool   IsLineBreak(this char character) => LineBreakCharacters.Contains(character);
+    private static readonly Regex  WordCharacter = new Regex(@"^\w$");
+    public static           bool   IsWordCharacter(this char character) => WordCharacter.IsMatch(character.ToString());
+    private static readonly Regex  NonWordCharacter = new Regex(@"^\W$");
+    public static           bool   IsNonWordCharacter(this char character) => NonWordCharacter.IsMatch(character.ToString());
+}
