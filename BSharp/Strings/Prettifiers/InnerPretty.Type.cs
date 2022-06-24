f7cb47531e0e05e1c9975ce4edaad2f84c54964b
Brandon Cimino <bcimino@cccis.com>
2022-06-23 22:19:46 -0500

    cleanup

--- BSharp/Strings/Prettifiers/InnerPretty.Type.cs
+++ BSharp/Strings/Prettifiers/InnerPretty.Type.cs
@@ -7,35 +7,35 @@ using FowlFever.BSharp.Collections;
 using FowlFever.BSharp.Enums;
 using FowlFever.BSharp.Reflection;
 
-using JetBrains.Annotations;
-
 [assembly: InternalsVisibleTo("BrandonUtils.BSharp.Strings")]
 
-namespace FowlFever.BSharp.Strings.Prettifiers {
-    internal static partial class InnerPretty {
-        /// <summary>
-        /// Prettifies a type, which may have different rules depending on whether it's a <see cref="ValueTuple{T1}"/>, <see cref="Type.IsGenericType"/>, etc.
-        /// </summary>
-        /// <param name="type">the <see cref="Type"/> to make pretty</param>
-        /// <param name="settings">optional <see cref="PrettificationSettings"/></param>
-        /// <returns>a pretty <see cref="string"/></returns>
-        [Pure]
-        public static string PrettifyType(this Type? type, PrettificationSettings? settings) {
-            settings ??= PrettificationSettings.Default;
+namespace FowlFever.BSharp.Strings.Prettifiers;
 
-            if (type == null) {
-                return settings.NullPlaceholder;
-            }
+internal static partial class InnerPretty {
+    /// <summary>
+    /// Prettifies a type, which may have different rules depending on whether it's a <see cref="ValueTuple{T1}"/>, <see cref="Type.IsGenericType"/>, etc.
+    /// </summary>
+    /// <param name="type">the <see cref="Type"/> to make pretty</param>
+    /// <param name="settings">optional <see cref="PrettificationSettings"/></param>
+    /// <returns>a pretty <see cref="string"/></returns>
+    [Pure]
+    public static string PrettifyType(this Type? type, PrettificationSettings? settings) {
+        settings = settings.Resolve();
 
-            if (type.IsTupleType()) {
-                return PrettifyTupleType(type, settings);
-            }
-
-            // if the type is generic, we need to trim the `n and replace it with the generic type arguments
-            return type.IsGenericType ? PrettifyGenericType(type, settings) : type.NameOrKeyword();
+        if (type == null) {
+            return settings.NullPlaceholder;
         }
 
-        private static string PrettifyGenericType(Type? genericType, PrettificationSettings settings) {
+        if (type.IsTupleType()) {
+            return PrettifyTupleType(type, settings);
+        }
+
+        // if the type is generic, we need to trim the `n and replace it with the generic type arguments
+        return type.IsGenericType ? PrettifyGenericType(type, settings) : type.NameOrKeyword();
+
+        #region Local methods
+
+        static string PrettifyGenericType(Type? genericType, PrettificationSettings settings) {
             if (genericType?.IsGenericType != true) {
                 throw new ArgumentException($"{genericType} is not a generic type!", nameof(genericType));
             }
@@ -52,21 +52,21 @@ namespace FowlFever.BSharp.Strings.Prettifiers {
             return genericType.Name.Replace($"`{genArgs.Length}", PrettifyGenericTypeArguments(genArgs, settings));
         }
 
-        private static string PrettifyTupleType(Type tupleType, PrettificationSettings settings) {
+        static string PrettifyTupleType(Type tupleType, PrettificationSettings settings) {
             var genArgs = tupleType.GetGenericArguments().Select(it => it.PrettifyType(settings));
             return $"({genArgs.JoinString(", ")})";
         }
 
-        private static string PrettifyNullableType(Type type, PrettificationSettings settings) {
-            return $"{type.Prettify(settings)}?";
+        static string PrettifyNullableType(Type type, PrettificationSettings settings) {
+            return $"{type.PrettifyType(settings)}?";
         }
 
-        private static string PrettifyGenericTypeArguments(IEnumerable<Type> genericTypeArguments, PrettificationSettings settings) {
+        static string PrettifyGenericTypeArguments(IEnumerable<Type> genericTypeArguments, PrettificationSettings settings) {
             var stylizedArgs = StylizeGenericTypeArguments(genericTypeArguments, settings);
             return $"<{stylizedArgs}>";
         }
 
-        private static string StylizeGenericTypeArguments(IEnumerable<Type?> genericTypeArguments, PrettificationSettings settings) {
+        static string StylizeGenericTypeArguments(IEnumerable<Type?> genericTypeArguments, PrettificationSettings settings) {
             return settings.TypeLabelStyle switch {
                 TypeNameStyle.None  => "",
                 TypeNameStyle.Full  => genericTypeArguments.Select(it => it.PrettifyType(settings)).JoinString(", "),
@@ -75,13 +75,15 @@ namespace FowlFever.BSharp.Strings.Prettifiers {
             };
         }
 
-        internal static string WithTypeLabel(
-            this string?           thing,
-            Type                   labelType,
-            PrettificationSettings settings,
-            string                 joiner = " "
-        ) {
-            return new[] { labelType.GetTypeLabel(settings), thing }.NonEmpty().JoinString(joiner);
-        }
+        #endregion
+    }
+
+    private static string WithTypeLabel(
+        this string?           thing,
+        Type                   labelType,
+        PrettificationSettings settings,
+        string                 joiner = " "
+    ) {
+        return new[] { labelType.GetTypeLabel(settings), thing }.NonEmpty().JoinString(joiner);
     }
 }
