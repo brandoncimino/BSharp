575e13ae38e517ff9e155d35452b086d461cf6a4
Brandon Cimino <bcimino@cccis.com>
2022-06-28 21:20:25 -0500

    Added a constructor that takes in `GraphemeCluster`s

--- BSharp/Strings/TextElementString.cs
+++ BSharp/Strings/TextElementString.cs
@@ -11,13 +11,20 @@ namespace FowlFever.BSharp.Strings;
 /// An immutable version of <see cref="StringInfo"/> with nutty <see cref="GraphemeCluster"/>s.
 /// </summary>
 public record TextElementString : WrappedImmutableCollection<GraphemeCluster, ImmutableList<GraphemeCluster>> {
-    public             string                               StringSource { get; }
+    private            string?                              _stringSource;
+    public             string                               StringSource => _stringSource ??= string.Join("", _graphemeClusters.Value);
     private readonly   Lazy<ImmutableList<GraphemeCluster>> _graphemeClusters;
     public override    ImmutableList<GraphemeCluster>       Value                                              => _graphemeClusters.Value;
     protected override ImmutableList<GraphemeCluster>       CreateSlice(IEnumerable<GraphemeCluster> elements) => elements.ToImmutableList();
 
     public TextElementString(string stringSource) {
-        StringSource      = stringSource;
+        _stringSource     = stringSource;
         _graphemeClusters = new Lazy<ImmutableList<GraphemeCluster>>(() => stringSource.EnumerateTextElements().ToImmutableList());
     }
+
+    public TextElementString(IEnumerable<GraphemeCluster> graphemeClusters) {
+        _graphemeClusters = new Lazy<ImmutableList<GraphemeCluster>>(graphemeClusters.ToImmutableList());
+    }
+
+    public static TextElementString Empty => new("");
 }
