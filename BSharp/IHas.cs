92bbaeeadbd58cc05ca6e5ff6a655899c688a10b
Brandon Cimino <bcimino@cccis.com>
2022-07-01 10:25:53 -0500

    added a `ValueEquals` extension

--- BSharp/IHas.cs
+++ BSharp/IHas.cs
@@ -14,9 +14,10 @@ public interface IHas<out T> {
 }
 
 public static class HasExtensions {
-    public static T? GetValueOrDefault<T>(this IHas<T>? self) {
-        return self == null ? default : self.Value;
-    }
+    [Obsolete($"Please use {nameof(OrDefault)} instead")]
+    public static T? GetValueOrDefault<T>(this IHas<T>? self) => self.OrDefault();
+
+    public static T? OrDefault<T>(this IHas<T>? self) => self == null ? default : self.Value;
 
     /// <param name="self">this <see cref="IHas{T}"/></param>
     /// <typeparam name="T">the type of the wrapped <see cref="IHas{T}.Value"/></typeparam>
@@ -24,4 +25,12 @@ public static class HasExtensions {
     public static T Get<T>(this IHas<T> self) => self.Value;
 
     public static Supplied<T> Supplied<T>(this IHas<T> self) => new(self.Get);
+
+    public static bool ValueEquals<T>(this IHas<T?>? self, IEquatable<T?>? other) {
+        return other switch {
+            T t         => self.OrDefault()?.Equals(t),
+            IHas<T> has => self.OrDefault()?.Equals(has.OrDefault()),
+            _           => other?.Equals(self.OrDefault())
+        } == true;
+    }
 }
