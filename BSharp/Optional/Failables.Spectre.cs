1f1071129ecb1e24d7ac3f044836a7f7a34781e5
Brandon Cimino <bcimino@cccis.com>
2022-07-06 23:17:39 -0500

    Added `Spectre.Console` support for `Failables`

---
+++ BSharp/Optional/Failables.Spectre.cs
@@ -0,0 +1,38 @@
+using System;
+
+using JetBrains.Annotations;
+
+using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace FowlFever.BSharp.Optional;
+
+public static partial class Failables {
+    #region Spectre.Console Support
+
+    public static IRenderable GetRenderable(this IFailable failable) {
+        var panel = new Panel(GetRenderableExcuse(failable) ?? Text.Empty) {
+            Header = new PanelHeader(failable.GetIcon().EscapeMarkup())
+        };
+        return panel;
+    }
+
+    private static Func<ExceptionSettings> ImmutableExceptionSettings() {
+        return () => new ExceptionSettings() {
+            Format = ExceptionFormats.ShortenEverything,
+            Style = {
+                Dimmed = new Style(foreground: Color.DarkSlateGray1, decoration: Decoration.Italic)
+            }
+        };
+    }
+
+    [UsedImplicitly]
+    public static ExceptionSettings DefaultExceptionSettings = ImmutableExceptionSettings().Invoke();
+
+    public static IRenderable? GetRenderableExcuse(this IFailable failable, ExceptionSettings? settings = default) {
+        settings ??= DefaultExceptionSettings;
+        return failable.Excuse?.GetRenderable(settings);
+    }
+
+    #endregion
+}
