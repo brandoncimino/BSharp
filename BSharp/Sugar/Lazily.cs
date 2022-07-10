d984352b73d3ad1f13a030580b1f1c5683f999c2
Brandon Cimino <bcimino@cccis.com>
2022-07-10 05:29:04 -0500

    added `LazyHas`

--- BSharp/Sugar/Lazily.cs
+++ BSharp/Sugar/Lazily.cs
@@ -1,16 +1,16 @@
 using System;
 using System.Linq;
+using System.Threading;
 
 namespace FowlFever.BSharp.Sugar;
 
 public static class Lazily {
-    public static Lazy<T> Get<T>(Func<T> supplier) => new(supplier);
+    public static LazyHas<T> Get<T>(Func<T> supplier) => new(supplier);
 
-    public static Lazy<T> Get<T>(T value) => new(() => value);
+    public static LazyHas<T> Get<T>(T value) => new(value);
 
-    public static Lazy<TLazy> Get<TFIn, TLazy, TFOut>(Func<TFIn, TFOut> supplier, TFIn input)
-        where TFOut : TLazy
-        => new(() => supplier(input));
+    public static LazyHas<TLazy> Get<TFIn, TLazy, TFOut>(Func<TFIn, TFOut> supplier, TFIn input)
+        where TFOut : TLazy => new(() => supplier(input));
 
     /// <summary>
     /// A method to return <see cref="Lazy{T}.Value"/> to help play nice with <see cref="Func{TResult}"/>s, such as inside of <see cref="Enumerable.Select{TSource,TResult}(System.Collections.Generic.IEnumerable{TSource},System.Func{TSource,int,TResult})"/>
@@ -19,4 +19,26 @@ public static class Lazily {
     /// <typeparam name="T">the type of the underlying <see cref="Lazy{T}.Value"/></typeparam>
     /// <returns>the <see cref="Lazy{T}.Value"/></returns>
     public static T Get<T>(this Lazy<T> lazy) => lazy.Value;
+
+    public static LazyHas<T> AsHas<T>(this Lazy<T> lazy) => lazy.IsValueCreated ? Get(lazy.Value) : Get(lazy.Get);
+}
+
+/// <inheritdoc cref="Lazy{T}"/>
+/// <remarks>
+/// Applies the <see cref="IHas{T}"/> interface to the <see cref="Lazy{T}"/> object.
+/// </remarks>
+public class LazyHas<T> : Lazy<T>, IHas<T> {
+    public LazyHas() { }
+
+    public LazyHas(bool isThreadSafe) : base(isThreadSafe) { }
+
+    public LazyHas(Func<T> valueFactory) : base(valueFactory) { }
+
+    public LazyHas(Func<T> valueFactory, bool isThreadSafe) : base(valueFactory, isThreadSafe) { }
+
+    public LazyHas(Func<T> valueFactory, LazyThreadSafetyMode mode) : base(valueFactory, mode) { }
+
+    public LazyHas(LazyThreadSafetyMode mode) : base(mode) { }
+
+    public LazyHas(T value) : base(value) { }
 }
