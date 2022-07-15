a8d884a2acde0e03221b2efbd6327140bacb022e
Brandon Cimino <bcimino@cccis.com>
2022-07-15 12:09:57 -0500

    added default implementations to `IEquivalent`

--- BSharp/IEquivalent.cs
+++ BSharp/IEquivalent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace FowlFever.BSharp;
 
@@ -12,4 +13,21 @@ public interface IEquivalent<T> :
     IEquatable<IEquatable<T?>?>,
     IComparable<T?>,
     IComparable<IHas<T?>?>,
-    IComparable<IComparable<T?>?> { }
+    IComparable<IComparable<T?>?> {
+    protected IEqualityComparer<T?> CanonEquality => EqualityComparer<T?>.Default;
+    protected IComparer<T?>         CanonComparer => Comparer<T?>.Default;
+
+    T                          Equivalent              { get; }
+    bool IEquatable<T?>.       Equals(T?        other) => CanonEquality.Equals(Equivalent, other);
+    bool IEquatable<IHas<T?>?>.Equals(IHas<T?>? other) => CanonEquality.Equals(Equivalent, other.OrDefault());
+
+    bool IEquatable<IEquatable<T?>?>.Equals(IEquatable<T?>? other) => other switch {
+        T t         => Equivalent?.Equals(t),
+        IHas<T> has => Equivalent?.Equals(has.OrDefault()),
+        _           => other?.Equals(Equivalent)
+    } == true;
+
+    int IComparable<T?>.              CompareTo(T?               other) => CanonComparer.Compare(Equivalent, other);
+    int IComparable<IHas<T?>?>.       CompareTo(IHas<T?>?        other) => CanonComparer.Compare(Equivalent, other.OrDefault());
+    int IComparable<IComparable<T?>?>.CompareTo(IComparable<T?>? other) => other == null ? 1 : other.CompareTo(Equivalent) * -1;
+}
