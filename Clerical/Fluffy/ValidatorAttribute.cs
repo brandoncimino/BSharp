5897339b5e7f87f5cd0dd336c8a32640bf9270b6
Brandon Cimino <bcimino@cccis.com>
2022-07-17 22:51:54 -0500

    marked `[Experimental]`

--- Clerical/Fluffy/ValidatorAttribute.cs
+++ Clerical/Fluffy/ValidatorAttribute.cs
@@ -12,6 +12,7 @@ namespace FowlFever.Clerical.Fluffy;
 /// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 [MeansImplicitUse]
+[Experimental("Should probably be reworked to play nice with the System.Component.DataModel validation attributes")]
 public class ValidatorAttribute : BrandonAttribute {
     /// <summary>
     /// A detailed <see cref="IValidatorMethod.Description"/> of this validation.
