000f0c59b2b753668b766ae101b8b10750d86138
Brandon Cimino <bcimino@cccis.com>
2022-08-01 14:00:47 -0500

    renamed `JoinPath` -> `JoinPathString`

--- Clerical/Validated/ClericalExtensions.cs
+++ Clerical/Validated/ClericalExtensions.cs
@@ -4,7 +4,8 @@ using FowlFever.Clerical.Validated.Atomic;
 namespace FowlFever.Clerical.Validated;
 
 public static class ClericalExtensions {
-    public static string JoinPath(this IEnumerable<PathPart> parts, DirectorySeparator separator = DirectorySeparator.Universal) {
+    public static string JoinPathString<T>(this IEnumerable<T> parts, DirectorySeparator separator = DirectorySeparator.Universal)
+        where T : IPathPart {
         return string.Join(separator.ToChar(), parts);
     }
 }
