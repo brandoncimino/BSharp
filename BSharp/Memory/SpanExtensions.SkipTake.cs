16da8eb9f15a1a7f03f44e7f53ef9591fcf84b1a
Brandon Cimino <bcimino@cccis.com>
2022-08-07 18:48:18 -0500

    reduced overload count by defaulting the limit parameters to `int.MaxValue`

--- BSharp/Memory/SpanExtensions.SkipTake.cs
+++ BSharp/Memory/SpanExtensions.SkipTake.cs
@@ -17,63 +17,59 @@ public static partial class SpanExtensions {
     [Pure]
     public static ReadOnlySpan<T> SafeSlice<T>(this ReadOnlySpan<T> span, Range range) => span[range.Clamp(span.Length)];
 
-    #region Skip
-
     [Pure]
     public static ReadOnlySpan<T> Skip<T>(this ReadOnlySpan<T> span, int toSkip) => toSkip switch {
-        < 0 => span,
-        _   => span.SafeSlice(toSkip..)
+        <= 0                         => span,
+        _ when toSkip >= span.Length => default,
+        _                            => span[toSkip..]
     };
-
-    [Pure] public static ReadOnlySpan<T> SkipWhile<T>(this ReadOnlySpan<T> span, [RequireStaticDelegate] Func<T, bool> predicate)                => span.Skip(span.IndexWhere(predicate, false));
-    [Pure] public static ReadOnlySpan<T> SkipWhile<T>(this ReadOnlySpan<T> span, [RequireStaticDelegate] Func<T, bool> predicate, int skipLimit) => span.Skip(span.IndexWhere(predicate, false).Limit(skipLimit));
-
-    [Pure]
-    public static ReadOnlySpan<T> SkipLast<T>(this ReadOnlySpan<T> span, int toSkip) {
-        return toSkip switch {
-            < 0 => span,
-            _   => span.SafeSlice(..^toSkip)
-        };
-    }
-
-    [Pure] public static ReadOnlySpan<T> SkipLastWhile<T>(this ReadOnlySpan<T> span, Func<T, bool> predicate)                => span.SkipLast(span.LastIndexWhere(predicate, false));
-    [Pure] public static ReadOnlySpan<T> SkipLastWhile<T>(this ReadOnlySpan<T> span, Func<T, bool> predicate, int skipLimit) => span.SkipLast(span.LastIndexWhere(predicate, false).Limit(skipLimit));
-
-    #endregion
-
-    #region Skip
 
     [Pure]
     public static ReadOnlySpan<T> Take<T>(this ReadOnlySpan<T> span, int toTake) => toTake switch {
-        < 0 => default,
-        _   => span.SafeSlice(..toTake)
+        <= 0                         => default,
+        _ when toTake >= span.Length => span,
+        _                            => span[..^toTake]
     };
 
-    [Pure] public static ReadOnlySpan<T> TakeWhile<T>(this ReadOnlySpan<T> span, Func<T, bool> predicate)                => span.Take(span.IndexWhere(predicate, false));
-    [Pure] public static ReadOnlySpan<T> TakeWhile<T>(this ReadOnlySpan<T> span, Func<T, bool> predicate, int skipLimit) => span.Take(span.IndexWhere(predicate, false).Limit(skipLimit));
+    #region {x}Last
 
-    [Pure]
-    public static ReadOnlySpan<T> TakeLast<T>(this ReadOnlySpan<T> span, int toTake) => toTake switch {
-        < 0 => default,
-        _   => span.SafeSlice(^toTake..)
-    };
-
-    [Pure] public static ReadOnlySpan<T> TakeLastWhile<T>(this ReadOnlySpan<T> span, Func<T, bool> predicate)                => span.Take(span.LastIndexWhere(predicate, false));
-    [Pure] public static ReadOnlySpan<T> TakeLastWhile<T>(this ReadOnlySpan<T> span, Func<T, bool> predicate, int skipLimit) => span.Take(span.LastIndexWhere(predicate, false).Limit(skipLimit));
+    [Pure] public static ReadOnlySpan<T> SkipLast<T>(this ReadOnlySpan<T> span, int toSkip) => span.Take(span.Length - toSkip);
+    [Pure] public static ReadOnlySpan<T> TakeLast<T>(this ReadOnlySpan<T> span, int toTake) => span.Skip(span.Length - toTake);
 
     #endregion
 
-    [Pure]
-    public static ReadOnlySpan<T> TrimStart<T>(this ReadOnlySpan<T> span, T toTrim, int trimLimit)
-        where T : IEquatable<T> {
-        return span.Skip(span.IndexNot(toTrim).Limit(trimLimit));
-    }
+    #region SkipWhile
 
     [Pure]
-    public static ReadOnlySpan<T> TrimEnd<T>(this ReadOnlySpan<T> span, T toTrim, int numberToTrim)
-        where T : IEquatable<T> {
-        return span.SkipLast(span.LastIndexNot(toTrim).Limit(numberToTrim));
-    }
+    public static ReadOnlySpan<T> SkipWhile<T, T2>(this ReadOnlySpan<T> span, [RequireStaticDelegate] Func<T, T2> selector, T2 expected, int skipLimit = int.MaxValue)
+        where T2 : IEquatable<T2> => span.Skip(span.CountWhile(selector, expected));
+
+    [Pure] public static ReadOnlySpan<T> SkipWhile<T>(this ReadOnlySpan<T> span, [RequireStaticDelegate] Func<T, bool> predicate, int skipLimit = int.MaxValue) => span.SkipWhile(predicate, true, skipLimit);
+
+    [Pure]
+    public static ReadOnlySpan<T> SkipLastWhile<T, T2>(this ReadOnlySpan<T> span, [RequireStaticDelegate] Func<T, T2> selector, T2 equals, int skipLimit = int.MaxValue)
+        where T2 : IEquatable<T2> =>
+        span.SkipLast(span.CountLastWhile(selector, equals, skipLimit));
+
+    [Pure] public static ReadOnlySpan<T> SkipLastWhile<T>(this ReadOnlySpan<T> span, [RequireStaticDelegate] Func<T, bool> predicate, int skipLimit = int.MaxValue) => span.SkipLastWhile(predicate, true, skipLimit);
+
+    #endregion
+
+    #region TakeWhile
+
+    [Pure]
+    public static ReadOnlySpan<T> TakeWhile<T, T2>(this ReadOnlySpan<T> span, Func<T, T2> selector, T2 expected, int takeLimit = int.MaxValue)
+        where T2 : IEquatable<T2> => span.Take(span.CountWhile(selector, expected).Min(takeLimit));
+
+    [Pure] public static ReadOnlySpan<T> TakeWhile<T>(this ReadOnlySpan<T> span, Func<T, bool> predicate, int takeLimit = int.MaxValue) => span.TakeWhile(predicate, true, takeLimit);
+
+    [Pure]
+    public static ReadOnlySpan<T> TakeLastWhile<T, T2>(this ReadOnlySpan<T> span, Func<T, T2> selector, T2 expected, int takeLimit = int.MaxValue)
+        where T2 : IEquatable<T2> => span.Take(span.CountLastWhile(selector, expected).Min(takeLimit));
+
+    [Pure] public static ReadOnlySpan<T> TakeLastWhile<T>(this ReadOnlySpan<T> span, Func<T, bool> predicate, int takeLimit = int.MaxValue) => span.TakeLastWhile(predicate, true);
+
+    #endregion
 
     #endregion
 }
