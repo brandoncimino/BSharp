810045d9f840fbf87285811642050d1999e61c2a
Brandon Cimino <bcimino@cccis.com>
2022-08-14 23:06:07 -0500

    - Added a single element indexer - Simplified `Slice` - Switched `_stringSource` from `string?` -> `Lazy<string>` - Implemented `ITextElements`

--- BSharp/Strings/TextElementString.cs
+++ BSharp/Strings/TextElementString.cs
@@ -1,42 +1,39 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Globalization;
 
 using FowlFever.BSharp.Collections;
 
 namespace FowlFever.BSharp.Strings;
 
-/// <summary>
-/// An immutable version of <see cref="StringInfo"/> with nutty <see cref="GraphemeCluster"/>s.
-/// </summary>
-public sealed record TextElementString : WrappedImmutableCollection<GraphemeCluster, ImmutableList<GraphemeCluster>, TextElementString>, IEquatable<string?> {
-    private          string?                              _stringSource;
-    public           string                               StringSource => _stringSource ??= string.Join("", _graphemeClusters.Value);
-    private readonly Lazy<ImmutableList<GraphemeCluster>> _graphemeClusters;
-    public override  ImmutableList<GraphemeCluster>       Value => _graphemeClusters.Value;
+/// <inheritdoc cref="ITextElements"/>
+public sealed record TextElementString : ITextElements {
+    private readonly Lazy<string>                          _stringSource;
+    public           string                                Source => _stringSource.Value;
+    private readonly Lazy<ImmutableArray<GraphemeCluster>> _graphemeClusters;
+    public           ImmutableArray<GraphemeCluster>       Clusters => _graphemeClusters.Value;
 
-    public override TextElementString Slice(int start, int length) => this switch {
-        {
-            _stringSource: not null,
-            _graphemeClusters.IsValueCreated: false
-        } => new TextElementString(_stringSource.Substring(start, length)), // avoid calculating the grapheme clusters and let the new slice deal with them
-        _ => new TextElementString(_graphemeClusters.Value.GetRange(start, start + length)),
-    };
+    public GraphemeCluster this[int index] => Clusters[index];
 
-    public TextElementString(string stringSource) {
-        _stringSource     = stringSource;
-        _graphemeClusters = new Lazy<ImmutableList<GraphemeCluster>>(() => stringSource.EnumerateTextElements().ToImmutableList());
+    public ITextElements Slice(int start, int length) {
+        return this switch {
+            { _graphemeClusters.IsValueCreated: true } => new TextElementString(Clusters.Slice(start, length)),
+            _                                          => new TextElementString(Source.Substring(start, length))
+        };
+    }
+
+    public TextElementString(string? stringSource) {
+        _stringSource     = new Lazy<string>(stringSource ?? "");
+        _graphemeClusters = new Lazy<ImmutableArray<GraphemeCluster>>(() => stringSource.EnumerateTextElements().ToImmutableArray());
     }
 
     public TextElementString(IEnumerable<GraphemeCluster> graphemeClusters) {
         // 📎 .ToImmutableList() already checks if the underlying instance is an ImmutableList() to prevent duplication, so we don't have to do it ourselves.
-        _graphemeClusters = new Lazy<ImmutableList<GraphemeCluster>>(graphemeClusters.ToImmutableList);
+        _graphemeClusters = new Lazy<ImmutableArray<GraphemeCluster>>(graphemeClusters.ToImmutableArray);
+        _stringSource     = new Lazy<string>(() => string.Join(null, Clusters));
     }
 
-    public static TextElementString Empty => new("");
-
-    public          bool Equals(string? other)            => StringSource == other;
-    public override int  GetHashCode()                    => StringSource.GetHashCode();
-    public          bool Equals(TextElementString? other) => StringSource == other?.StringSource;
+    public static   TextElementString Empty                            => new("");
+    public override int               GetHashCode()                    => Source.GetHashCode();
+    public          bool              Equals(TextElementString? other) => Source == other?.Source;
 }
