13de54e0720832e35d54386480246e176e4f5dde
Brandon Cimino <bcimino@cccis.com>
2022-08-14 23:12:56 -0500

    Added `Lazy.AndThen` and `Lazy.Or`

--- BSharp/Sugar/Lazily.cs
+++ BSharp/Sugar/Lazily.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Threading;
 
+using FowlFever.BSharp.Attributes;
 using FowlFever.Implementors;
 
 namespace FowlFever.BSharp.Sugar;
@@ -25,12 +26,37 @@ public static class Lazily {
     public static LazyHas<T> AsHas<T>(this Lazy<T> lazy) => lazy.IsValueCreated ? Get(lazy.Value) : Get(lazy.Get);
 
     public static T? OrDefault<T>(this Lazy<T>? lazy) => lazy == null ? default : lazy.Value;
+
+    /// <summary>
+    /// "Chains" operations together lazily by applying a <see cref="Func{T,TResult}"/> to the <see cref="Lazy{T}.Value"/> of <paramref name="lazy"/>.
+    /// </summary>
+    /// <param name="lazy">the original <see cref="Lazy{T}"/></param>
+    /// <param name="transformation">applied to the <see cref="Lazy{T}.Value"/></param>
+    /// <typeparam name="T">the original <see cref="Lazy{T}"/> type</typeparam>
+    /// <typeparam name="T2">the transformed <see cref="Lazy{T}"/> type</typeparam>
+    /// <returns>a new <see cref="Lazy{T}"/></returns>
+    public static Lazy<T2> AndThen<T, T2>(this Lazy<T> lazy, Func<T, T2> transformation) {
+        return new Lazy<T2>(() => transformation(lazy.Value));
+    }
+
+    /// <summary>
+    /// Returns the <see cref="Lazy{T}.Value"/> of the <paramref name="fallback"/> if this <see cref="Lazy{T}"/> returns <c>null</c>.
+    /// </summary>
+    /// <param name="lazy">this <see cref="Lazy{T}"/></param>
+    /// <param name="fallback">the result if this <see cref="Lazy{T}"/> returns <c>null</c></param>
+    /// <typeparam name="T">the <see cref="Lazy{T}.Value"/> type</typeparam>
+    /// <returns>a new <see cref="Lazy{T}"/></returns>
+    public static Lazy<T> Or<T>(this Lazy<T?> lazy, LazyShim<T> fallback) {
+        var lz = fallback.Lazy;
+        return new Lazy<T>(() => lazy.Value ?? lz.Value);
+    }
 }
 
 /// <inheritdoc cref="Lazy{T}"/>
 /// <remarks>
 /// Applies the <see cref="IHas{T}"/> interface to the <see cref="Lazy{T}"/> object.
 /// </remarks>
+[Experimental]
 public class LazyHas<T> : Lazy<T>, IHas<T> {
     public LazyHas() { }
 
