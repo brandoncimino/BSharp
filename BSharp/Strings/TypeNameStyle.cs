6acd9acac93802e656c2aabb3369393558121c8c
Brandon Cimino <bcimino@cccis.com>
2022-08-15 12:58:40 -0500

    added a `TypeName` extension

--- BSharp/Strings/TypeNameStyle.cs
+++ BSharp/Strings/TypeNameStyle.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
 
 using FowlFever.BSharp.Enums;
 using FowlFever.BSharp.Reflection;
@@ -7,36 +9,50 @@ using FowlFever.BSharp.Strings.Json;
 using FowlFever.BSharp.Strings.Prettifiers;
 using FowlFever.BSharp.Strings.Settings;
 
-namespace FowlFever.BSharp.Strings {
-    public enum TypeNameStyle {
-        None = 0, Short = 1, Full = 2,
+namespace FowlFever.BSharp.Strings;
+
+public enum TypeNameStyle {
+    None, Short, Full,
+}
+
+public static class TypeNameStyleExtensions {
+    /// <summary>
+    /// Returns the <see cref="InnerPretty.PrettifyType"/> form of <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="obj">an arbitrary <typeparamref name="T"/> instance</param>
+    /// <param name="style"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    [Pure]
+    [SuppressMessage("ReSharper", "EntityNameCapturedOnly.Global", Justification = "Flagrantly untrue")]
+    public static string TypeName<T>(this T obj, TypeNameStyle style = TypeNameStyle.Full) {
+        style.Rejecting(TypeNameStyle.None);
+        return MethodBase.GetCurrentMethod()!.GetNullability(nameof(obj)).PrettifyType();
     }
 
-    public static class TypeNameStyleExtensions {
-        public static string GetTypeLabel(this Type? type, PrettificationSettings? settings) {
-            settings = Prettification.ResolveSettings(settings);
+    public static string GetTypeLabel(this Type? type, PrettificationSettings? settings) {
+        settings = settings.Resolve();
 
-            var style = type?.IsEnum == true ? settings.EnumLabelStyle : settings.TypeLabelStyle;
+        var style = type?.IsEnum == true ? settings.EnumLabelStyle : settings.TypeLabelStyle;
 
-            settings?.TraceWriter.Verbose(() => $"Using style: {style} (type: {settings.TypeLabelStyle}, enum: {settings.EnumLabelStyle})");
+        settings?.TraceWriter.Verbose(() => $"Using style: {style} (type: {settings.TypeLabelStyle}, enum: {settings.EnumLabelStyle})");
 
-            if (type == null || style == TypeNameStyle.None) {
-                return "";
-            }
-
-            var str = type.PrettifyType(settings);
-
-            if (type.IsArray || type.IsEnum || (type != typeof(string) && type.IsEnumerable())) {
-                return str;
-            }
-
-            return $"[{str}]";
+        if (type == null || style == TypeNameStyle.None) {
+            return "";
         }
 
-        public static TypeNameStyle Reduce(this TypeNameStyle style, int steps = 1) {
-            var newStep = (int)style - steps;
-            newStep = newStep.Clamp(0, BEnum.GetValues<TypeNameStyle>().Cast<int>().Max());
-            return (TypeNameStyle)newStep;
+        var str = type.PrettifyType(settings);
+
+        if (type.IsArray || type.IsEnum || (type != typeof(string) && type.IsEnumerable())) {
+            return str;
         }
+
+        return $"[{str}]";
+    }
+
+    public static TypeNameStyle Reduce(this TypeNameStyle style, int steps = 1) {
+        var newStep = (int)style - steps;
+        newStep = newStep.Clamp(0, BEnum.GetValues<TypeNameStyle>().Cast<int>().Max());
+        return (TypeNameStyle)newStep;
     }
 }
