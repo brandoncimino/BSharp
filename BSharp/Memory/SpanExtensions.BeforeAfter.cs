569ed79c20cb5359e65a9350e0387109f8ebba1d
Brandon Cimino <bcimino@cccis.com>
2022-08-15 14:08:18 -0500

    Added stuff for sub-sequences

--- BSharp/Memory/SpanExtensions.BeforeAfter.cs
+++ BSharp/Memory/SpanExtensions.BeforeAfter.cs
@@ -13,6 +13,10 @@ public static partial class SpanExtensions {
     public static ReadOnlySpan<T> AfterFirstAny<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> splitters)
         where T : IEquatable<T> => span.Skip(span.IndexOfAny(splitters));
 
+    [Pure]
+    public static ReadOnlySpan<T> AfterFirst<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> subSequence)
+        where T : IEquatable<T> => span.Skip(span.IndexOf(subSequence)).Take(subSequence.Length);
+
     #endregion
 
     #region BeforeFirst
@@ -24,6 +28,10 @@ public static partial class SpanExtensions {
     [Pure]
     public static ReadOnlySpan<T> BeforeFirstAny<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> splitters)
         where T : IEquatable<T> => span.Take(span.IndexOfAny(splitters));
+
+    [Pure]
+    public static ReadOnlySpan<T> BeforeFirst<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> subSequence)
+        where T : IEquatable<T> => span.Take(span.IndexOf(subSequence));
 
     #endregion
 
@@ -37,6 +45,13 @@ public static partial class SpanExtensions {
     public static ReadOnlySpan<T> AfterLastAny<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> splitters)
         where T : IEquatable<T> => span.Skip(span.LastIndexOfAny(splitters));
 
+    [Pure]
+    public static ReadOnlySpan<T> AfterLast<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> subSequence)
+        where T : IEquatable<T> {
+        var index = span.LastIndexOf(subSequence);
+        return index >= 0 ? span[(index + subSequence.Length)..] : default;
+    }
+
     #endregion
 
     #region BeforeLast
@@ -48,6 +63,10 @@ public static partial class SpanExtensions {
     [Pure]
     public static ReadOnlySpan<T> BeforeLastAny<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> splitters)
         where T : IEquatable<T> => span.Take(span.LastIndexOfAny(splitters));
+
+    [Pure]
+    public static ReadOnlySpan<T> BeforeLast<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> subSequence)
+        where T : IEquatable<T> => span.SkipLast(span.IndexOf(subSequence) + subSequence.Length);
 
     #endregion
 }
