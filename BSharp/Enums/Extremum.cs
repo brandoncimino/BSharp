d68d60ab978e4345ad776b355fe4b46d5dbc7dd0
Brandon Cimino <bcimino@cccis.com>
2022-08-17 22:54:39 -0500

    added `Extremum`

---
+++ BSharp/Enums/Extremum.cs
@@ -0,0 +1,6 @@
+namespace FowlFever.BSharp.Enums;
+
+/// <summary>
+/// Represents <a href="https://en.wikipedia.org/wiki/Maxima_and_minima">minimum or maximum</a>.
+/// </summary>
+public enum Extremum { Min, Max }
