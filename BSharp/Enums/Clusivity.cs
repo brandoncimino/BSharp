e341aac859ede1fd9b7d990aa7f012e0fd03eff4
Brandon Cimino <bcimino@cccis.com>
2022-08-17 22:48:20 -0500

    - used file-scoped namespace (how I thought I long since converted everything?!) - added some javadocs

--- BSharp/Enums/Clusivity.cs
+++ BSharp/Enums/Clusivity.cs
@@ -1,9 +1,9 @@
 using JetBrains.Annotations;
 
-namespace FowlFever.BSharp.Enums {
-    [PublicAPI]
-    public enum Clusivity {
-        Inclusive,
-        Exclusive,
-    }
-}
+namespace FowlFever.BSharp.Enums;
+
+/// <summary>
+/// Represents <a href="https://en.wikipedia.org/wiki/Interval_(mathematics)#Including_or_excluding_endpoints">inclusion or exclusion</a>, aka <a href="https://en.wikipedia.org/wiki/Clusivity">clusivity</a>.
+/// </summary>
+[PublicAPI]
+public enum Clusivity { Inclusive, Exclusive, }
