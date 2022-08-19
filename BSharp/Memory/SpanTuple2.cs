64389e92bd7fa6921e21657fa4baa193c86ff0d0
Brandon Cimino <bcimino@cccis.com>
2022-08-18 20:48:35 -0500

    reduced nesting

--- BSharp/Memory/SpanTuple2.cs
+++ BSharp/Memory/SpanTuple2.cs
@@ -29,29 +29,57 @@ public readonly ref struct SpanTuple2<TA, TB> {
 
 /// <inheritdoc cref="SpanTuple2{TA,TB}"/>
 public readonly ref struct SpanTuple2<T> {
-    private readonly SpanTuple2<T, T> _spans;
-
-    public int  Length  => _spans.Length;
-    public bool IsEmpty => _spans.IsEmpty;
-
-    public static SpanTuple2<T> Empty => default;
+    public ReadOnlySpan<T> A       { get; init; }
+    public ReadOnlySpan<T> B       { get; init; }
+    public int             Length  => A.Length + B.Length;
+    public bool            IsEmpty => Length == 0;
 
     public SpanTuple2(ReadOnlySpan<T> a, ReadOnlySpan<T> b) {
-        _spans = new SpanTuple2<T, T>(a, b);
-    }
-
-    public ReadOnlySpan<T> A {
-        get => _spans.A;
-        init => _spans = _spans with { A = value };
-    }
-
-    public ReadOnlySpan<T> B {
-        get => _spans.B;
-        init => _spans = _spans with { B = value };
+        A = a;
+        B = b;
     }
 
     public void Deconstruct(out ReadOnlySpan<T> a, out ReadOnlySpan<T> b) {
         a = A;
         b = B;
+    }
+}
+
+public readonly ref struct Span2Extra<TA, TB, TExtra> {
+    public ReadOnlySpan<TA>   A     { get; init; }
+    public ReadOnlySpan<TB>   B     { get; init; }
+    public TExtra             Extra { get; init; }
+    public SpanTuple2<TA, TB> Spans => new(A, B);
+
+    public void Deconstruct(out ReadOnlySpan<TA> a, out ReadOnlySpan<TB> b, out TExtra extra) {
+        a     = A;
+        b     = B;
+        extra = Extra;
+    }
+
+    public void Deconstruct(out SpanTuple2<TA, TB> spans, out TExtra extra) {
+        spans = Spans;
+        extra = Extra;
+    }
+}
+
+public readonly ref struct Span2Extra<T, TExtra> {
+    public ReadOnlySpan<T> A     { get; init; }
+    public ReadOnlySpan<T> B     { get; init; }
+    public SpanTuple2<T>   Spans => new(A, B);
+    public TExtra          Extra { get; init; }
+
+    public Span2Extra(ReadOnlySpan<T> a, ReadOnlySpan<T> b, TExtra extra) {
+        A     = a;
+        B     = b;
+        Extra = extra;
+    }
+
+    public Span2Extra(TExtra extra, ReadOnlySpan<T> a, ReadOnlySpan<T> b) : this(a, b, extra) { }
+
+    public void Deconstruct(out ReadOnlySpan<T> a, out ReadOnlySpan<T> b, out TExtra extra) {
+        a     = A;
+        b     = B;
+        extra = Extra;
     }
 }
