de2536f72dbe875f6e94e26c8b756bffa0d0565d
Brandon Cimino <bcimino@cccis.com>
2022-08-29 17:57:25 -0500

    added `OneLineList`

---
+++ BSharp/Strings/Spectral/OneLineList.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+using FowlFever.Conjugal.Affixing;
+
+using Spectre.Console.Rendering;
+
+namespace FowlFever.BSharp.Strings.Spectral;
+
+/// <summary>
+/// Renders a bunch of <see cref="IRenderable"/>s into a list on one line, e.g. <c>[1, 2, 3]</c>
+/// </summary>
+public class OneLineList : IRenderable {
+    private readonly        IImmutableList<IRenderable> _stuff;
+    public                  ICircumfix                  Bookends      { get; init; } = BSharp.Enums.Bookends.SquareBrackets;
+    public                  Stylist                     BookendStyle  { get; init; }
+    private                 Segment                     PrefixSegment => Bookends.GetPrefix().EscapeSegment(BookendStyle);
+    private                 Segment                     SuffixSegment => Bookends.GetSuffix().EscapeSegment(BookendStyle);
+    private static readonly Segment                     JoinerSegment = new(", ");
+
+    public OneLineList(IEnumerable<IRenderable> stuff) {
+        _stuff = stuff.ToImmutableList();
+    }
+
+    private SegmentLine GetSegmentLine(RenderContext context, int maxWidth) {
+        var line = new SegmentLine { PrefixSegment };
+        FlatJoin(line, _stuff, it => it.Render(context, maxWidth), JoinerSegment);
+        line.Add(SuffixSegment);
+        return line;
+    }
+
+    private static TList FlatJoin<TList, T, T2>(TList list, IEnumerable<T2> stuff, Func<T2, IEnumerable<T>> selector, T joiner)
+        where TList : List<T> {
+        using var erator = stuff.GetEnumerator();
+
+        if (erator.MoveNext() == false) {
+            return list;
+        }
+
+        list.AddRange(selector(erator.Current));
+
+        while (erator.MoveNext()) {
+            list.Add(joiner);
+            list.AddRange(selector(erator.Current));
+        }
+
+        return list;
+    }
+
+    public Measurement Measure(RenderContext context, int maxWidth) {
+        return new Measurement(maxWidth, maxWidth);
+    }
+
+    public IEnumerable<Segment> Render(RenderContext context, int maxWidth) {
+        return GetSegmentLine(context, maxWidth);
+    }
+}
