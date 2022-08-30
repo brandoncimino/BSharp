c1d65ecfcd09b7f5cc412f80164ba5002faa5479
Brandon Cimino <bcimino@cccis.com>
2022-08-30 00:41:27 -0500

    added some tests for `Clerk` file extension methods

--- Tests/Clerical2/FileExtensionTests.cs
+++ Tests/Clerical2/FileExtensionTests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Linq;
 
+using FowlFever.BSharp.Memory;
 using FowlFever.BSharp.Optional;
+using FowlFever.Clerical.Validated;
 using FowlFever.Clerical.Validated.Atomic;
 using FowlFever.Testing;
 
@@ -42,4 +44,18 @@ public class FileExtensionTests {
 
     [Test] public void FileExtension_Negative([ValueSource(nameof(NegativeFileExtensions))] FileExtensionExpectation expectation) => New_FileExtension_Expectation(expectation);
     [Test] public void FileExtension_Positive([ValueSource(nameof(PositiveFileExtensions))] FileExtensionExpectation expectation) => New_FileExtension_Expectation(expectation);
+
+    [TestCase("a/b/c/d/.e.f", ".e.f")]
+    public void GetFullExtensionTest(string input, string expected) {
+        Assert.That(Clerk.GetFullExtension(input).ToString(), Is.EqualTo(expected));
+    }
+
+    [TestCase("a.b.c",        "b", "c")]
+    [TestCase(".a.b.c",       "a", "b", "c")]
+    [TestCase("a/b/c/d/.e.f", "e", "f")]
+    [TestCase("a/")]
+    public void EnumerateFileExtensionsTest(string input, params string[] expectedExtensions) {
+        var actual = Clerk.EnumerateExtensions(input).ToStringArray().ToArray();
+        Assert.That(actual, Is.EqualTo(expectedExtensions));
+    }
 }
