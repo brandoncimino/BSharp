024892a7d704b57bbf060f0e6d3e61b6cff37d62
Brandon Cimino <bcimino@cccis.com>
2022-09-04 01:43:55 -0500

    removed a redundant settings resolve

--- BSharp/Strings/Prettifiers/InnerPretty.Enum.cs
+++ BSharp/Strings/Prettifiers/InnerPretty.Enum.cs
@@ -5,7 +5,6 @@ using FowlFever.BSharp.Strings.Settings;
 namespace FowlFever.BSharp.Strings.Prettifiers {
     internal partial class InnerPretty {
         internal static string PrettifyEnum(Enum enm, PrettificationSettings settings) {
-            settings = Prettification.ResolveSettings(settings);
             return enm.ToString().WithTypeLabel(enm.GetType(), settings, ".");
         }
     }
