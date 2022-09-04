402da1ada9a7c8eb95d16abb408a6640c95046e9
Brandon Cimino <bcimino@cccis.com>
2022-09-04 01:57:22 -0500

    added `Chars.EmDash` and `Chars.ZeroWidthJoiner`

---
+++ BSharp/Strings/Chars.cs
@@ -0,0 +1,13 @@
+namespace FowlFever.BSharp.Strings;
+
+public static class Chars {
+    /// <summary>
+    /// See <a href="https://en.wikipedia.org/wiki/Dash#Em_dash">em dash</a>.
+    /// </summary>
+    public const char EmDash = '—';
+
+    /// <summary>
+    /// See <a href="https://en.wikipedia.org/wiki/Zero-width_joiner">zero-width joiner</a>.
+    /// </summary>
+    public const char ZeroWidthJoiner = '\u200D';
+}
