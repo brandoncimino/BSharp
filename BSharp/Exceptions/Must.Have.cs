dcef52096977a2362395f4a7cf8707d5d3b89d82
Brandon Cimino <bcimino@cccis.com>
2022-09-04 17:10:11 -0500

    added a single `bool` version of `Must.Have`

--- BSharp/Exceptions/Must.Have.cs
+++ BSharp/Exceptions/Must.Have.cs
@@ -1,12 +1,23 @@
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
-
-using FowlFever.BSharp.Attributes;
 
 namespace FowlFever.BSharp.Exceptions;
 
 public partial class Must {
-    [Experimental($"Need to clean up all the different {nameof(Must)}.{nameof(Be)} overloads")]
+    /// <summary>
+    /// Throws a <see cref="RejectionException"/> if <paramref name="actual"/> and <paramref name="expected"/> are not equal according to the <see cref="EqualityComparer{T}.Default"/> equality comparer.
+    /// </summary>
+    /// <param name="actual">the value that we <i>have</i></param>
+    /// <param name="expected">the value that we <i>want</i></param>
+    /// <param name="details">optional additional details</param>
+    /// <param name="_actual">see <see cref="CallerArgumentExpressionAttribute"/></param>
+    /// <param name="_expected">see <see cref="CallerArgumentExpressionAttribute"/></param>
+    /// <param name="_caller">see <see cref="CallerMemberNameAttribute"/></param>
+    /// <typeparam name="T">the type of <paramref name="actual"/></typeparam>
+    /// <returns><paramref name="actual"/>, <b><i>if</i></b> it was equal to <paramref name="expected"/></returns>
+    /// <exception cref="RejectionException">if <paramref name="actual"/> is <b><i>not</i></b> equal to <paramref name="expected"/></exception>
     [return: NotNullIfNotNull("actual")]
     public static T? Have<T>(
         T?                                             actual,
@@ -20,10 +31,33 @@ public partial class Must {
             return actual;
         }
 
-        if (actual?.Equals(expected) == true) {
+        if (EqualityComparer<T?>.Default.Equals(actual, expected)) {
             return actual;
         }
 
-        throw new RejectionException(actual, details, _actual, _caller, $"must == {expected}");
+        throw new RejectionException(actual, details, _actual, _caller, $"({_actual}: [{actual}]) must equal ({_expected}: [{expected}])");
+    }
+
+    /// <summary>
+    /// Throws a <see cref="RejectionException"/> if <paramref name="condition"/> is <c>false</c>.
+    /// </summary>
+    /// <param name="condition">the <b><i>result</i></b> of some validation</param>
+    /// <param name="details">optional additional details describing the <paramref name="condition"/></param>
+    /// <param name="_condition">see <see cref="CallerArgumentExpressionAttribute"/></param>
+    /// <param name="_caller">see <see cref="CallerMemberNameAttribute"/></param>
+    /// <exception cref="RejectionException">if <paramref name="condition"/> is <c>false</c></exception>
+    public static void Have(
+        bool    condition,
+        string? details = default,
+        [CallerArgumentExpression("condition")]
+        string? _condition = default,
+        [CallerMemberName] string? _caller = default
+    ) {
+        Debug.Assert(true);
+        if (condition) {
+            return;
+        }
+
+        throw new RejectionException(details: $"{_condition} was {condition}!", _caller: _caller);
     }
 }
