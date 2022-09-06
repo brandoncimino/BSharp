3912fd127bfce7978513a75499ea668877795fcb
Brandon Cimino <bcimino@cccis.com>
2022-09-06 18:10:17 -0500

    made an overload that should reduce boxing

--- BSharp/Strings/IPrettifier.cs
+++ BSharp/Strings/IPrettifier.cs
@@ -1,16 +1,20 @@
 using System;
 
 using FowlFever.BSharp.Collections;
+using FowlFever.BSharp.Exceptions;
 using FowlFever.BSharp.Strings.Settings;
 
 namespace FowlFever.BSharp.Strings {
     public interface IPrettifier : IPrimaryKeyed<Type> {
         /// <value>the <see cref="Type"/> that this <see cref="IPrettifier{T}"/> can <see cref="Prettify"/>.</value>
-
         Type PrettifierType { get; }
         Type IPrimaryKeyed<Type>.PrimaryKey => PrettifierType;
 
         bool CanPrettify(Type type) => PrettifierType.IsAssignableFrom(type);
+
+        /// <inheritdoc cref="Prettify{T}"/>
+        [Obsolete("Use the generic Prettify<T> instead")]
+        string Prettify(object? cinderella, PrettificationSettings? settings = default) => Prettify<object>(cinderella, settings);
 
         /// <summary>
         /// Returns a pretty <see cref="string"/> representation of <paramref name="cinderella"/> <b>IF</b> <paramref name="cinderella"/> is of the <see cref="PrettifierType"/>.
@@ -21,7 +25,7 @@ namespace FowlFever.BSharp.Strings {
         /// <param name="settings">an optional <see cref="PrettificationSettings"/> instance</param>
         /// <returns>a pretty <see cref="string"/> representation of <paramref name="cinderella"/></returns>
         /// <exception cref="InvalidCastException">if <paramref name="cinderella"/> is not of the <see cref="PrettifierType"/></exception>
-        string Prettify(object? cinderella, PrettificationSettings? settings = default);
+        string Prettify<T>(T? cinderella, PrettificationSettings? settings = default);
     }
 
     /**
@@ -34,5 +38,14 @@ namespace FowlFever.BSharp.Strings {
         string Prettify(T? cinderella, PrettificationSettings? settings = default);
 
         Type IPrettifier.PrettifierType => typeof(T);
+
+        string IPrettifier.Prettify<T1>(T1? cinderella, PrettificationSettings? settings)
+            where T1 : default {
+            return cinderella switch {
+                null => settings.Resolve().NullPlaceholder,
+                T t  => Prettify(t, settings),
+                _    => throw Reject.UnhandledSwitchType(cinderella),
+            };
+        }
     }
 }
