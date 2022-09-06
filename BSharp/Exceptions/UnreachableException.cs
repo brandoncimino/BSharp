b6f97ff7ed1d36fbc00a4ac2fad3afac96bc8605
Brandon Cimino <bcimino@cccis.com>
2022-09-05 23:10:04 -0500

    added javadocs

--- BSharp/Exceptions/UnreachableException.cs
+++ BSharp/Exceptions/UnreachableException.cs
@@ -12,6 +12,14 @@ public class UnreachableException : BrandonException {
     private readonly   int?    _lineNo;
     protected override string  BaseMessage => $"[{_caller}] {_filePath}:line {_lineNo} should have been unreachable!";
 
+    /// <summary>
+    /// <inheritdoc cref="UnreachableException"/>
+    /// </summary>
+    /// <param name="message">a <see cref="BrandonException.CustomMessage"/> to be included in the <see cref="Exception.Message"/></param>
+    /// <param name="innerException">the <see cref="Exception.InnerException"/></param>
+    /// <param name="_caller">see <see cref="CallerMemberNameAttribute"/></param>
+    /// <param name="_filePath">see <see cref="CallerFilePathAttribute"/></param>
+    /// <param name="_lineNo">see <see cref="CallerLineNumberAttribute"/></param>
     public UnreachableException(
         string?                    message        = default,
         Exception?                 innerException = default,
