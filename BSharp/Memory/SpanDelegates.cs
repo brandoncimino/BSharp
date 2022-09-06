a5419639927e2b7e74326c0693140d1bb9d1ee5e
Brandon Cimino <bcimino@cccis.com>
2022-09-06 12:43:04 -0500

    removed the `BSharp` version of `SpanDelegates`

--- BSharp/Memory/SpanDelegates.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace FowlFever.BSharp.Memory;
-
-public delegate TOut ReadOnlySpanFunc<TIn, in TArg, out TOut>(ReadOnlySpan<TIn> span, TArg arg);
-public delegate TOut ReadOnlySpanFunc<TIn, out TOut>(ReadOnlySpan<TIn>          span);
-public delegate TOut SpanFunc<TIn, in TArg, out TOut>(Span<TIn>                 span, TArg arg);
-public delegate TOut SpanFunc<TIn, out TOut>(Span<TIn>                          span);
