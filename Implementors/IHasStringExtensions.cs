2b28ecc85c751bc586001e8e2b8facf5bafe73e9
Brandon Cimino <bcimino@cccis.com>
2022-09-06 21:42:57 -0500

    added `IsEmpty` / `IsBlank` extensions

--- Implementors/IHasStringExtensions.cs
+++ Implementors/IHasStringExtensions.cs
@@ -1,6 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace FowlFever.Implementors;
 
 public static class HasStringExtensions {
-    public static string             OrEmpty<T>(this T? self) where T : IHas<string?>? => self?.Value ?? "";
-    public static ReadOnlySpan<char> AsSpan<T>(this  T? self) where T : IHas<string?>? => self?.Value;
+    public static string             OrEmpty<T>(this                         T? self) where T : IHas<string?>? => self?.Value ?? "";
+    public static ReadOnlySpan<char> AsSpan<T>(this                          T? self) where T : IHas<string?>? => self?.Value;
+    public static bool               IsEmpty<T>([NotNullWhen(   false)] this T? self) where T : IHas<string?>? => string.IsNullOrEmpty(self?.Value);
+    public static bool               IsNotEmpty<T>([NotNullWhen(true)] this  T? self) where T : IHas<string?>? => !self.IsEmpty();
+    public static bool               IsBlank<T>([NotNullWhen(   false)] this T? self) where T : IHas<string?>? => string.IsNullOrWhiteSpace(self?.Value);
+    public static bool               IsNotBlank<T>([NotNullWhen(true)] this  T? self) where T : IHas<string?>? => !self.IsBlank();
 }
