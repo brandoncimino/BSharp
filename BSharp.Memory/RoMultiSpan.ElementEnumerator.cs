f2ffa57368160e1cb506d6abafc464744dcc3846
Brandon Cimino <bcimino@cccis.com>
2022-09-12 09:56:33 -0500

    - exposed `Source`s of `SpanEnumerator` and `ElementEnumerator` - properly added a `GetEnumerator` method to `ElementEnumerator` - added lots of javadocs

--- BSharp.Memory/RoMultiSpan.ElementEnumerator.cs
+++ BSharp.Memory/RoMultiSpan.ElementEnumerator.cs
@@ -3,7 +3,20 @@ using System;
 namespace FowlFever.BSharp.Memory;
 
 public readonly ref partial struct RoMultiSpan<T> {
+    #region Element Enumerator
+
+    /// <inheritdoc cref="ElementEnumerator"/>
+    /// <returns>a new <see cref="ElementEnumerator"/></returns>
+    public ElementEnumerator EnumerateElements() => new(this);
+
+    /// <summary>
+    /// "Flattens" all of the elements from each consecutive <see cref="ReadOnlySpan{T}"/> in a <see cref="RoMultiSpan{T}"/>.
+    /// </summary>
     public ref struct ElementEnumerator {
+        /// <summary>
+        /// The original <see cref="RoMultiSpan{T}"/> that is being iterated over.
+        /// </summary>
+        public RoMultiSpan<T> Source => _spanerator.Source;
         private SpanEnumerator             _spanerator;
         private ReadOnlySpan<T>.Enumerator _elementEnumerator = default;
         public  T                          Current => _elementEnumerator.Current;
@@ -24,5 +37,9 @@ public readonly ref partial struct RoMultiSpan<T> {
 
             return false;
         }
+
+        public ElementEnumerator GetEnumerator() => this;
     }
+
+    #endregion
 }
--- BSharp.Memory/RoMultiSpan.SpanEnumerator.cs
+++ BSharp.Memory/RoMultiSpan.SpanEnumerator.cs
@@ -5,27 +5,28 @@ using JetBrains.Annotations;
 namespace FowlFever.BSharp.Memory;
 
 public readonly ref partial struct RoMultiSpan<T> {
+    #region Span Enumerator
+
     /// <inheritdoc cref="SpanEnumerator"/>
     /// <returns>a new <see cref="SpanEnumerator"/></returns>
+    /// <remarks>
+    /// The <see cref="GetEnumerator"/> method exists to enable <a href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-foreach-statement"><c>foreach</c> loops</a>.
+    /// If you find the need to call it explicitly, you should probably call <see cref="EnumerateSpans"/> instead, which is more explicit to prevent confusion with <see cref="EnumerateElements"/>. 
+    /// </remarks>
     public SpanEnumerator GetEnumerator() => new(this);
 
-    public RoMultiSpan<T> Where(ReadOnlySpanFunc<T, bool> predicate) => new SpanEnumerator(this, predicate).ToMultiSpan();
-
-    public TOut[] Select<TOut>(ReadOnlySpanFunc<T, TOut> selector) {
-        var results = new TOut[SpanCount];
-
-        for (int i = 0; i < SpanCount; i++) {
-            results[i] = selector(this[i]);
-        }
-
-        return results;
-    }
+    /// <inheritdoc cref="SpanEnumerator"/>
+    /// <returns>a new <see cref="SpanEnumerator"/></returns>
+    public SpanEnumerator EnumerateSpans() => GetEnumerator();
 
     /// <summary>
     /// Enumerates the individual <see cref="ReadOnlySpan{T}"/>s in a <see cref="RoMultiSpan{T}"/>.
     /// </summary>
     public ref struct SpanEnumerator {
-        private readonly RoMultiSpan<T>             _source;
+        /// <summary>
+        /// The original <see cref="RoMultiSpan{T}"/> that is being iterated over.
+        /// </summary>
+        public readonly RoMultiSpan<T> Source;
         private          int                        _current = -1;
         private readonly ReadOnlySpanFunc<T, bool>? _where;
 
@@ -33,20 +34,20 @@ public readonly ref partial struct RoMultiSpan<T> {
             this switch {
                 { _current: < 0 }    => throw NotStartedException(),
                 { IsFinished: true } => throw AlreadyFinishedException(),
-                _                    => _source[_current]
+                _                    => Source[_current]
             };
 
-        public bool IsFinished => _current >= _source.SpanCount;
+        public bool IsFinished => _current >= Source.SpanCount;
 
         public SpanEnumerator(RoMultiSpan<T> source, [RequireStaticDelegate] ReadOnlySpanFunc<T, bool>? where = default) {
-            _source = source;
-            _where  = where;
+            Source = source;
+            _where = where;
         }
 
         public bool MoveNext() {
             _current += 1;
 
-            if (_current >= _source.SpanCount) {
+            if (_current >= Source.SpanCount) {
                 return false;
             }
 
@@ -83,4 +84,46 @@ public readonly ref partial struct RoMultiSpan<T> {
 
         #endregion
     }
+
+    #endregion
+
+    #region Spanq
+
+    /// <param name="predicate">a function that takes determines which <see cref="ReadOnlySpan{T}"/>s we want to keep</param>
+    /// <returns>a new <see cref="RoMultiSpan{T}"/> containing the <see cref="ReadOnlySpan{T}"/>s for whom the given <paramref name="predicate"/> returns <c>true</c></returns>
+    public RoMultiSpan<T> Where(ReadOnlySpanFunc<T, bool> predicate) => new SpanEnumerator(this, predicate).ToMultiSpan();
+
+    /// <summary>
+    /// Transforms each <see cref="ReadOnlySpan{T}"/> in this <see cref="RoMultiSpan{T}"/> into a <typeparamref name="TOut"/>, returning the results as a new <see cref="Array"/>.
+    /// </summary>
+    /// <param name="selector">transforms a <see cref="ReadOnlySpan{T}"/> into a <typeparamref name="TOut"/></param>
+    /// <typeparam name="TOut">the output type</typeparam>
+    /// <returns>a new <see cref="Array"/> of <typeparamref name="TOut"/></returns>
+    public TOut[] Select<TOut>(ReadOnlySpanFunc<T, TOut> selector) {
+        var results = new TOut[SpanCount];
+
+        for (int i = 0; i < SpanCount; i++) {
+            results[i] = selector(this[i]);
+        }
+
+        return results;
+    }
+
+    /// <summary>
+    /// Transforms each <see cref="ReadOnlySpan{T}"/> <i>(along with its index)</i> in this <see cref="RoMultiSpan{T}"/> into a <typeparamref name="TOut"/>, returning the results as a new <see cref="Array"/>.
+    /// </summary>
+    /// <param name="selectorWithIndex">transforms a <see cref="ReadOnlySpan{T}"/> + its index into a <typeparamref name="TOut"/></param>
+    /// <typeparam name="TOut">the output type</typeparam>
+    /// <returns>a new <see cref="Array"/> of <typeparamref name="TOut"/></returns>
+    public TOut[] Select<TOut>(ReadOnlySpanFunc<T, int, TOut> selectorWithIndex) {
+        var results = new TOut[SpanCount];
+
+        for (int i = 0; i < SpanCount; i++) {
+            results[i] = selectorWithIndex(this[i], i);
+        }
+
+        return results;
+    }
+
+    #endregion
 }
