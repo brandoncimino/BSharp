cd8d3ed463212837a0849d72198e4af05c6500c9
Brandon Cimino <bcimino@cccis.com>
2022-09-17 21:09:16 -0500

    updated conditional compilation of `Contains` from 6+ to 5+

--- BSharp.Memory/Spanq.cs
+++ BSharp.Memory/Spanq.cs
@@ -9,19 +9,12 @@ namespace FowlFever.BSharp.Memory;
 public static partial class Spanq {
     #region Containment
 
-#if !NET6_0_OR_GREATER
-    [Pure]
-    public static bool Contains<T>(this ReadOnlySpan<T> span, T entry)
-        where T : IEquatable<T> => span.IndexOf(entry) >= 0;
+#if !NET5_0_OR_GREATER
+    [Pure] public static bool Contains<T>(this ReadOnlySpan<T> span, T entry) where T : IEquatable<T> => span.IndexOf(entry) >= 0;
 #endif
 
-    [Pure]
-    public static bool Contains<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> subSpan)
-        where T : IEquatable<T> => span.IndexOf(subSpan) >= 0;
-
-    [Pure]
-    public static bool ContainsAny<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> soughtAfter)
-        where T : IEquatable<T> => span.IndexOfAny(soughtAfter) >= 0;
+    [Pure] public static bool Contains<T>(this    ReadOnlySpan<T> span, ReadOnlySpan<T> subSpan) where T : IEquatable<T>     => span.IndexOf(subSpan)        >= 0;
+    [Pure] public static bool ContainsAny<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> soughtAfter) where T : IEquatable<T> => span.IndexOfAny(soughtAfter) >= 0;
 
     #endregion
 
