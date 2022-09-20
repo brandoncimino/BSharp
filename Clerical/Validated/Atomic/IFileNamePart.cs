8a07edfffa0b4bd8fd2cd8c53e780ef7ddb9f8b8
Brandon Cimino <bcimino@cccis.com>
2022-09-20 01:02:58 -0500

    updated `IFileNamePart.Ratify` to reject `SpecialPathPart`s

--- Clerical/Validated/Atomic/IFileNamePart.cs
+++ Clerical/Validated/Atomic/IFileNamePart.cs
@@ -1,9 +1,17 @@
+using FowlFever.BSharp.Exceptions;
+
 namespace FowlFever.Clerical.Validated.Atomic;
 
 public interface IFileNamePart : IPathPart {
     public FileNamePart ToFileNamePart();
 
     public new static ReadOnlySpan<char> Ratify(ReadOnlySpan<char> fileNamePart) {
+        var specialPathPart = PathPart.GetSpecialPathPart(fileNamePart);
+        if (specialPathPart != null) {
+            Reject.Parameter(fileNamePart, $"The {nameof(SpecialPathPart)} {specialPathPart} `{specialPathPart.Value.PathString()}` cannot be an {nameof(IFileNamePart)}!");
+        }
+
+        IPathPart.Ratify(fileNamePart);
         BadCharException.Assert(fileNamePart, Clerk.InvalidFileNamePartChars);
         return fileNamePart;
     }
