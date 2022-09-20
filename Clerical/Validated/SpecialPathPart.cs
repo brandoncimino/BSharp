985fd8b329606ae20ff6d56efbe764b1b605ec92
Brandon Cimino <bcimino@cccis.com>
2022-09-20 00:57:29 -0500

    separated out `SpecialPathPart`

---
+++ Clerical/Validated/SpecialPathPart.cs
@@ -0,0 +1,24 @@
+using FowlFever.BSharp.Enums;
+using FowlFever.Clerical.Validated.Atomic;
+
+namespace FowlFever.Clerical.Validated;
+
+public enum SpecialPathPart : byte { CurrentDirectory, ParentDirectory, HomeDirectory }
+
+public static class SpecialPathPartExtensions {
+    public static string PathString(this SpecialPathPart specialPathPart) => specialPathPart switch {
+        SpecialPathPart.CurrentDirectory => ".",
+        SpecialPathPart.ParentDirectory  => "..",
+        SpecialPathPart.HomeDirectory    => "~",
+        _                                => throw BEnum.UnhandledSwitch(specialPathPart),
+    };
+
+    public static PathPart GetPathPart(this SpecialPathPart specialPathPart) => specialPathPart switch {
+        SpecialPathPart.CurrentDirectory => PathPart.CurrentDirectory,
+        SpecialPathPart.ParentDirectory  => PathPart.ParentDirectory,
+        SpecialPathPart.HomeDirectory    => PathPart.HomeDirectory,
+        _                                => throw BEnum.UnhandledSwitch(specialPathPart),
+    };
+
+    public static SpecialPathPart? AsSpecialPathPart<T>(this T pathPart) where T : IPathPart => PathPart.GetSpecialPathPart(pathPart.ToPathPart().Value);
+}
