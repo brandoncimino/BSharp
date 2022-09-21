d5d0aa032ac5b68729d92e599a2953d422bfb0ba
Brandon Cimino <bcimino@cccis.com>
2022-09-20 21:28:22 -0500

    - added `Palette` to the `BlogList` constructor - fell back to Blog's `Palette` in `Post` methods - added static `Params` methods

--- BSharp/BlogList.cs
+++ BSharp/BlogList.cs
@@ -24,8 +24,9 @@ public sealed record BlogList : IDisposable, IRenderable {
         init => _palette = value;
     }
 
-    public BlogList([CallerMemberName] string title = "") {
-        _title = title;
+    public BlogList(string? title = default, Palette? palette = default, [CallerMemberName] string? _caller = default) {
+        _title   = title ?? _caller;
+        _palette = palette;
     }
 
     private IRenderable? GetTitle() {
@@ -62,25 +63,113 @@ public sealed record BlogList : IDisposable, IRenderable {
         };
     }
 
+    #region Post
+
     public BlogList Post<T, TLabel>(T? value, TLabel? label, Palette? palette = default, [CallerArgumentExpression("value")] string? _expression = default) {
-        _rows.Add(Renderwerks.GetLabelled(value, label, palette, _expression));
+        _rows.Add(Renderwerks.GetLabelled(value, label, palette ?? Palette, _expression));
         return this;
     }
 
     public BlogList Post<T>(T? value, Palette? palette = default, [CallerArgumentExpression("value")] string? _expression = default) {
-        _rows.Add(Renderwerks.GetLabelled(value, palette, _expression));
+        _rows.Add(Renderwerks.GetLabelled(value, palette ?? Palette, _expression));
         return this;
     }
 
     public BlogList Post<T, TLabel>(ReadOnlySpan<T> span, TLabel? label = default, Palette? palette = default, [CallerArgumentExpression("span")] string? _expression = default) {
-        _rows.Add(Renderwerks.GetLabelled(span, label, palette, _expression));
+        _rows.Add(Renderwerks.GetLabelled(span, label, palette ?? Palette, _expression));
         return this;
     }
 
     public BlogList Post<T>(ReadOnlySpan<T> span, Palette? palette = default, [CallerArgumentExpression("span")] string? _expression = default) {
-        _rows.Add(Renderwerks.GetLabelled(span, palette, _expression));
+        _rows.Add(Renderwerks.GetLabelled(span, palette ?? Palette, _expression));
         return this;
     }
+
+    #region static Post
+
+    public static void Params<A>(
+        A?                                      a,
+        Palette?                                palette = default,
+        [CallerMemberName]              string? _caller = default,
+        [CallerArgumentExpression("a")] string? _a      = default
+    ) {
+        using var blog = new BlogList(_caller, palette);
+        blog.Post(a, _a);
+    }
+
+    public static void Params<A, B>(
+        A?                                      a,
+        B?                                      b,
+        Palette?                                palette = default,
+        [CallerMemberName]              string? _caller = default,
+        [CallerArgumentExpression("a")] string? _a      = default,
+        [CallerArgumentExpression("b")] string? _b      = default
+    ) {
+        using BlogList blog = new(_caller, palette);
+        blog.Post(a, _a)
+            .Post(b, _b);
+    }
+
+    public static void Params<A, B, C>(
+        A?                                      a,
+        B?                                      b,
+        C?                                      c,
+        Palette?                                palette = default,
+        [CallerMemberName]              string? _caller = default,
+        [CallerArgumentExpression("a")] string? _a      = default,
+        [CallerArgumentExpression("b")] string? _b      = default,
+        [CallerArgumentExpression("c")] string? _c      = default
+    ) {
+        using BlogList blog = new(_caller, palette);
+        blog.Post(a, _a)
+            .Post(b, _b)
+            .Post(c, _c);
+    }
+
+    public static void Params<A, B, C, D>(
+        A?                                      a,
+        B?                                      b,
+        C?                                      c,
+        D?                                      d,
+        Palette?                                palette = default,
+        [CallerMemberName]              string? _caller = default,
+        [CallerArgumentExpression("a")] string? _a      = default,
+        [CallerArgumentExpression("b")] string? _b      = default,
+        [CallerArgumentExpression("c")] string? _c      = default,
+        [CallerArgumentExpression("d")] string? _d      = default
+    ) {
+        using BlogList blog = new(_caller, palette);
+        blog.Post(a, _a)
+            .Post(b, _b)
+            .Post(c, _c)
+            .Post(d, _d);
+    }
+
+    public static void Params<A, B, C, D, E>(
+        A?                                      a,
+        B?                                      b,
+        C?                                      c,
+        D?                                      d,
+        E?                                      e,
+        Palette?                                palette = default,
+        [CallerMemberName]              string? _caller = default,
+        [CallerArgumentExpression("a")] string? _a      = default,
+        [CallerArgumentExpression("b")] string? _b      = default,
+        [CallerArgumentExpression("c")] string? _c      = default,
+        [CallerArgumentExpression("d")] string? _d      = default,
+        [CallerArgumentExpression("e")] string? _e      = default
+    ) {
+        using BlogList blog = new(_caller, palette);
+        blog.Post(a, _a)
+            .Post(b, _b)
+            .Post(c, _c)
+            .Post(d, _d)
+            .Post(e, _e);
+    }
+
+    #endregion
+
+    #endregion
 
     public void Dispose() {
         Brandon.Render(this);
