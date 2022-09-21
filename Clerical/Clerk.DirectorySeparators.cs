741494b453cbe0882ee945fd767c17b2aad9b7b1
Brandon Cimino <bcimino@cccis.com>
2022-09-21 09:48:09 -0500

    added `EnsureEndingDirectorySeparator`

--- Clerical/Clerk.DirectorySeparators.cs
+++ Clerical/Clerk.DirectorySeparators.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics.Contracts;
 
 using FowlFever.BSharp.Clerical;
+using FowlFever.BSharp.Memory;
+using FowlFever.BSharp.Strings;
 
 namespace FowlFever.Clerical;
 
@@ -22,4 +24,44 @@ public static partial class Clerk {
     /// <inheritdoc cref="EndsInDirectorySeparator(System.ReadOnlySpan{char})"/>
     [Pure]
     public static bool EndsInDirectorySeparator(string? path) => EndsInDirectorySeparator(path.AsSpan());
+
+    /// <summary>
+    /// Appends a <see cref="DirectorySeparator"/> to a <paramref name="path"/> if it doesn't end in one.
+    /// </summary>
+    /// <remarks>
+    /// If the <paramref name="path"/> ends in a different <see cref="DirectorySeparator"/>, then that separator is <b>not modified</b>:
+    /// <code><![CDATA[
+    /// EnsureEndingDirectorySeparator("a",  DirectorySeparator.Universal); // => a/
+    /// EnsureEndingDirectorySeparator("a/", DirectorySeparator.Universal); // => a/
+    /// EnsureEndingDirectorySeparator("a\", DirectorySeparator.Universal); // => a\
+    /// ]]></code>
+    /// </remarks>
+    /// <param name="path">the <see cref="FileSystemInfo.OriginalPath"/></param>
+    /// <param name="separator">the <see cref="DirectorySeparator"/> that will be added if the <paramref name="path"/> doesn't already end in one</param>
+    /// <returns>the <paramref name="path"/>, ending with a <see cref="DirectorySeparator"/></returns>
+    [Pure]
+    public static ReadOnlySpan<char> EnsureEndingDirectorySeparator(ReadOnlySpan<char> path, DirectorySeparator separator = DirectorySeparator.Universal) {
+        if (path.Length == 0) {
+            return separator.ToCharString();
+        }
+
+        if (path[^1].IsDirectorySeparator()) {
+            return path;
+        }
+
+        return stackalloc char[path.Length + 1].Start(path, out var pos)
+                                               .Write(separator.ToChar(), ref pos)
+                                               .ToString();
+    }
+
+    /// <inheritdoc cref="EnsureEndingDirectorySeparator(System.ReadOnlySpan{char},FowlFever.BSharp.Clerical.DirectorySeparator)"/>
+    [Pure]
+    public static string EnsureEndingDirectorySeparator(string? path, DirectorySeparator separator = DirectorySeparator.Universal) {
+        if (path == null) {
+            return separator.ToCharString();
+        }
+
+        var str = EnsureEndingDirectorySeparator(path.AsSpan(), separator);
+        return str.Length == path.Length ? path : str.ToString();
+    }
 }
