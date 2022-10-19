bfed0dc6dc92ab36b610766fa78eec25301220a9
Brandon Cimino <bcimino@cccis.com>
2022-10-18 21:40:10 -0500

    to file-scoped namespace

--- BSharp/Clerical/DirectorySeparator.cs
+++ BSharp/Clerical/DirectorySeparator.cs
@@ -2,29 +2,28 @@ using System.IO;
 
 using FowlFever.BSharp.Enums;
 
-namespace FowlFever.BSharp.Clerical {
-    public enum DirectorySeparator {
-        /// <summary>
-        /// Aka "Unix".
-        /// </summary>
-        Universal,
-        Windows,
-        PlatformDependent
+namespace FowlFever.BSharp.Clerical;
+
+public enum DirectorySeparator {
+    /// <summary>
+    /// Aka "Unix".
+    /// </summary>
+    Universal,
+    Windows,
+    PlatformDependent
+}
+
+public static class DirectorySeparatorExtensions {
+    public static char ToChar(this DirectorySeparator separator) {
+        return separator switch {
+            DirectorySeparator.Universal         => '/',
+            DirectorySeparator.Windows           => '\\',
+            DirectorySeparator.PlatformDependent => Path.DirectorySeparatorChar,
+            _                                    => throw BEnum.InvalidEnumArgumentException(nameof(separator), separator)
+        };
     }
 
-    public static class DirectorySeparatorExtensions {
-        public static char ToChar(this DirectorySeparator separator) {
-            return separator switch {
-                DirectorySeparator.Universal         => '/',
-                DirectorySeparator.Windows           => '\\',
-                DirectorySeparator.PlatformDependent => Path.DirectorySeparatorChar,
-                _                                    => throw BEnum.InvalidEnumArgumentException(nameof(separator), separator)
-            };
-        }
-
-
-        public static string ToCharString(this DirectorySeparator separator) {
-            return separator.ToChar().ToString();
-        }
+    public static string ToCharString(this DirectorySeparator separator) {
+        return separator.ToChar().ToString();
     }
 }
