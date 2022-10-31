2ea0562e8c8aa51a4cabbb59379db8facb40d49c
Brandon Cimino <bcimino@cccis.com>
2022-10-31 12:43:58 -0500

    renamed `ToPathString` / `ToPathPart`

--- Clerical/SpecialPathPart.cs
+++ Clerical/SpecialPathPart.cs
@@ -1,24 +1,21 @@
 using FowlFever.BSharp.Enums;
-using FowlFever.Clerical.Validated.Atomic;
 
 namespace FowlFever.Clerical;
 
 public enum SpecialPathPart : byte { CurrentDirectory, ParentDirectory, HomeDirectory }
 
 public static class SpecialPathPartExtensions {
-    public static string PathString(this SpecialPathPart specialPathPart) => specialPathPart switch {
+    public static string ToPathString(this SpecialPathPart specialPathPart) => specialPathPart switch {
         SpecialPathPart.CurrentDirectory => ".",
         SpecialPathPart.ParentDirectory  => "..",
         SpecialPathPart.HomeDirectory    => "~",
         _                                => throw BEnum.UnhandledSwitch(specialPathPart),
     };
 
-    public static PathPart GetPathPart(this SpecialPathPart specialPathPart) => specialPathPart switch {
+    public static PathPart ToPathPart(this SpecialPathPart specialPathPart) => specialPathPart switch {
         SpecialPathPart.CurrentDirectory => PathPart.CurrentDirectory,
         SpecialPathPart.ParentDirectory  => PathPart.ParentDirectory,
         SpecialPathPart.HomeDirectory    => PathPart.HomeDirectory,
         _                                => throw BEnum.UnhandledSwitch(specialPathPart),
     };
-
-    public static SpecialPathPart? AsSpecialPathPart<T>(this T pathPart) where T : IPathPart => PathPart.GetSpecialPathPart(pathPart.ToPathPart().Value);
 }
