02fb377d842842b520d7ba7c038da7bb3a22ad01
Brandon Cimino <bcimino@cccis.com>
2022-10-31 12:42:58 -0500

    used the simplified types, `ValueArray<PathPart>` and `PathPart`

--- Clerical/Clerk.cs
+++ Clerical/Clerk.cs
@@ -1,58 +1,37 @@
 using System.Diagnostics.Contracts;
 
+using FowlFever.BSharp;
+using FowlFever.BSharp.Collections;
 using FowlFever.BSharp.Exceptions;
 using FowlFever.BSharp.Memory;
-using FowlFever.Clerical.Validated.Atomic;
-using FowlFever.Clerical.Validated.Composed;
 
 namespace FowlFever.Clerical;
 
 /// <summary>
-/// Contains factory methods for <see cref="Validated"/> objects like <see cref="PathPart"/> and <see cref="FileName"/>.
+/// Contains factory methods for <see cref="Validated"/> objects like <see cref="PathPart"/> and <see cref="FileExtension"/>.
 /// </summary>
 public static partial class Clerk {
+    /// <summary>
+    /// Splits apart each <see cref="PathPart"/> from the given <paramref name="path"/>.
+    /// </summary>
+    /// <param name="path">the original string</param>
+    /// <returns>a <see cref="ValueArray{T}"/> containing each <see cref="PathPart"/> in the input <paramref name="path"/></returns>
     [Pure]
-    public static IEnumerable<PathPart> SplitPath(string? path) {
-        if (path == null) {
-            yield break;
-        }
-
-        int pos = 0;
-
-        static int SeparatorIndex(ReadOnlySpan<char> str) {
-            return str.IndexOfAny(DirectorySeparatorChars.AsSpan());
-        }
-
-        while (pos < path.Length) {
-            var remaining = path.AsSpan(pos);
-            var sep       = SeparatorIndex(path.AsSpan(pos));
-
-            // no more separators; yield whatever's is left and then break
-            if (sep <= -1) {
-                yield return new PathPart(remaining);
-                yield break;
-            }
-
-            // at least 1 char before the next separator, so yield that part
-            if (sep >= 1) {
-                yield return new PathPart(remaining[..sep]);
-            }
-
-            // advance `pos` by the size of the part + 1 (to account for the separator itself)
-            pos += sep + 1;
-        }
+    public static ValueArray<PathPart> SplitPath(ReadOnlySpan<char> path) {
+        return path.IsEmpty ? default(ValueArray<PathPart>) : EnumeratePathParts(path).ToImmutableArray(static str => PathPart.Of(str));
     }
 
-    [Pure] public static SpanSpliterator<char> SplitPath(ReadOnlySpan<char> path) => EnumeratePathParts(path);
-
-    [Pure] private static SpanSpliterator<char> EnumeratePathParts(ReadOnlySpan<char> path) => new(path, DirectorySeparatorChars.AsSpan(), SplitterMatchStyle.AnyEntry, StringSplitOptions.RemoveEmptyEntries | (StringSplitOptions)2);
+    [Pure] internal static SpanSpliterator<char> EnumeratePathParts(ReadOnlySpan<char> path) => new(path, DirectorySeparatorChars.AsSpan(), SplitterMatchStyle.AnyEntry, StringSplitOptions.RemoveEmptyEntries | (StringSplitOptions)2);
 
     /// <summary>
     /// <inheritdoc cref="Path.GetTempPath"/>
     /// </summary>
-    /// <returns>a <see cref="DirectoryPath"/> equivalent of <see cref="Path.GetTempPath"/></returns>
+    /// <remarks>
+    /// 📎 The built-in method, <see cref="Path.GetTempPath"/>, is ambiguously named - it specifically always returns a folder <i>(and uses the term "folder" in its doc comments, not directory!)</i>.
+    /// </remarks>
+    /// <returns>a sequence of <see cref="PathPart"/>s equivalent of <see cref="Path.GetTempPath"/></returns>
     [Pure]
-    public static DirectoryPath GetTempFolder() => new(Path.GetTempPath());
+    public static ValueArray<PathPart> GetTempFolder() => SplitPath(Path.GetTempPath());
 
     /// <summary>
     /// <inheritdoc cref="Path.GetTempFileName"/>
@@ -60,16 +39,16 @@ public static partial class Clerk {
     /// <remarks>
     /// ⚠ The built-in method, <see cref="Path.GetTempFileName"/> is <b>incorrectly named</b>! It actually <b>creates a new file!</b>
     /// </remarks>
-    /// <returns>a <see cref="FileName"/> equivalent of <see cref="Path.GetTempFileName"/></returns>
+    /// <returns>a sequence of <see cref="PathPart"/>s equivalent of <see cref="Path.GetTempFileName"/></returns>
     [Pure]
-    public static FileName CreateTempFile() => GetFileName(Path.GetTempFileName());
+    public static ValueArray<PathPart> CreateTempFile() => SplitPath(Path.GetTempFileName());
 
     /// <summary>
     /// <inheritdoc cref="Path.GetRandomFileName"/>
     /// </summary>
-    /// <returns>a <see cref="FileName"/> equivalent of <see cref="Path.GetRandomFileName"/></returns>
+    /// <returns>a sequence of <see cref="PathPart"/>s equivalent to <see cref="Path.GetRandomFileName"/></returns>
     [Pure]
-    public static FileName GetRandomFileName() => FindFileName(Path.GetRandomFileName()).MustNotBeNull();
+    public static PathPart GetRandomFileName() => FindFileName(Path.GetRandomFileName()).MustNotBeNull();
 
     /// <param name="path">a file path</param>
     /// <returns><c>true</c> if the <paramref name="path"/> <see cref="EndsInDirectorySeparator(System.ReadOnlySpan{char})"/> or is a <see cref="SpecialPathPart"/></returns>
