0e26f0930f1e112000e139326714a51f87d8a39d
Brandon Cimino <bcimino@cccis.com>
2022-12-14 17:45:01 -0600

    simplified & improved `GetValueOrDefault` methods, renaming them to match the built-in `IReadOnlyDictionary.GetValueOrDefault`

--- BSharp/Collections/DictionaryExtensions.cs
+++ BSharp/Collections/DictionaryExtensions.cs
@@ -10,32 +10,82 @@ namespace FowlFever.BSharp.Collections;
 /// TODO: Move extensions from <see cref="CollectionUtils"/> into here
 /// </summary>
 public static class DictionaryExtensions {
-    public static TValue? GetOrDefault<TKey, TValue>(
+    /// <summary>
+    /// "Safely" retrieves an entry that might not be there.
+    /// </summary>
+    /// <param name="dic">this <see cref="IDictionary{TKey,TValue}"/></param>
+    /// <param name="key">the desired <see cref="KeyValuePair{TKey,TValue}.key"/></param>
+    /// <typeparam name="TKey">the <see cref="KeyValuePair{TKey,TValue}.key"/> type</typeparam>
+    /// <typeparam name="TValue">the <see cref="KeyValuePair{TKey,TValue}.value"/> type</typeparam>
+    /// <returns><see cref="IDictionary{TKey,TValue}.this[TKey]"/>, if present; otherwise, the <c>default</c>(<typeparamref name="TValue"/>)</returns>
+    public static TValue? GetValueOrDefault<TKey, TValue>(
+        this IDictionary<TKey, TValue> dic,
+        TKey                           key
+    ) {
+        return dic.TryGetValue(key, out var val) ? val : default;
+    }
+
+    /// <summary>
+    /// <inheritdoc cref="GetValueOrDefault{TKey,TValue}"/>
+    /// </summary>
+    /// <param name="dic">this <see cref="IDictionary{TKey,TValue}"/></param>
+    /// <param name="key">the desired <see cref="KeyValuePair{TKey,TValue}.key"/></param>
+    /// <param name="fallback">the <typeparamref name="TValue"/> returned if the <paramref name="key"/> isn't found</param>
+    /// <typeparam name="TKey">the <see cref="KeyValuePair{TKey,TValue}.key"/> type</typeparam>
+    /// <typeparam name="TValue">the <see cref="KeyValuePair{TKey,TValue}.value"/> type</typeparam>
+    /// <typeparam name="TFallback">the <paramref name="fallback"/> type</typeparam>
+    /// <returns><see cref="IDictionary{TKey,TValue}.this[TKey]"/>, if present; otherwise, <paramref name="fallback"/></returns>
+    /// <remarks>
+    /// Compared to the built-in <see cref="CollectionExtensions"/> method, <see cref="CollectionExtensions.GetValueOrDefault{TKey,TValue}(System.Collections.Generic.IReadOnlyDictionary{TKey,TValue},TKey)"/>.
+    /// <ul>
+    /// <li>This method works with <see cref="IDictionary{TKey,TValue}"/> instead of <see cref="IReadOnlyDictionary{TKey,TValue}"/>.
+    /// While concrete types like <see cref="Dictionary{TKey,TValue}"/> generally implement both <see cref="IDictionary{TKey,TValue}"/> and <see cref="IReadOnlyDictionary{TKey,TValue}"/>,
+    /// code often passes around the most generic interface, which is <see cref="IDictionary{TKey,TValue}"/>.
+    /// </li>
+    /// <li>This method introduces an extra type parameter, <typeparamref name="TFallback"/>, which allows the nullability of the return type to be determined by the given <paramref name="fallback"/>.</li>
+    /// </ul>
+    /// </remarks>
+    public static TFallback GetValueOrDefault<TKey, TValue, TFallback>(
         this IDictionary<TKey, TValue> dic,
         TKey                           key,
-        TValue?                        fallback = default
-    ) {
+        TFallback                      fallback
+    )
+        where TValue : TFallback {
         return dic.TryGetValue(key, out var val) ? val : fallback;
     }
 
-    public static TValue? GetOrDefault<TKey, TValue>(
+    /// <summary>
+    /// Invokes <see cref="IDictionary{TKey,TValue}.this[TKey]"/> if the <paramref name="key"/> is present; otherwise, invokes <paramref name="fallbackSupplier"/>.
+    /// </summary>
+    /// <param name="dic">this <see cref="IDictionary{TKey,TValue}"/></param>
+    /// <param name="key">the desired <see cref="KeyValuePair{TKey,TValue}.key"/></param>
+    /// <param name="fallbackSupplier">a <see cref="Func{TResult}"/> that generates the fallback value if the <paramref name="key"/> isn't found</param>
+    /// <typeparam name="TKey">the <see cref="KeyValuePair{TKey,TValue}.key"/> type</typeparam>
+    /// <typeparam name="TValue">the <see cref="KeyValuePair{TKey,TValue}.value"/> type</typeparam>
+    /// <returns><see cref="IDictionary{TKey,TValue}.this[TKey]"/>, if present; otherwise, the result of <paramref name="fallbackSupplier"/></returns>
+    public static TValue GetValueOrSupply<TKey, TValue>(
         this IDictionary<TKey, TValue> dic,
         TKey                           key,
         Func<TValue>                   fallbackSupplier
     ) {
-        if (dic == null) {
-            throw new ArgumentNullException(nameof(dic));
-        }
+        return dic.TryGetValue(key, out var val) ? val : fallbackSupplier();
+    }
 
-        if (key == null) {
-            throw new ArgumentNullException(nameof(key));
-        }
-
-        if (fallbackSupplier == null) {
-            throw new ArgumentNullException(nameof(fallbackSupplier));
-        }
-
-        return dic.ContainsKey(key) ? dic[key] : fallbackSupplier.Invoke();
+    /// <summary>
+    /// Invokes <see cref="IDictionary{TKey,TValue}.this[TKey]"/> if the <paramref name="key"/> is present; otherwise, invokes <see cref="fallbackSupplier"/> with the given <paramref name="key"/>.
+    /// </summary>
+    /// <param name="dic">this <see cref="IDictionary{TKey,TValue}"/></param>
+    /// <param name="key">the desired <see cref="KeyValuePair{TKey,TValue}.key"/></param>
+    /// <param name="fallbackSupplier">invoked with <paramref name="key"/> if it isn't found in the dictionary</param>
+    /// <typeparam name="TKey">the <see cref="KeyValuePair{TKey,TValue}.key"/> type</typeparam>
+    /// <typeparam name="TValue">the <see cref="KeyValuePair{TKey,TValue}.value"/> type</typeparam>
+    /// <returns><see cref="IDictionary{TKey,TValue}.this[TKey]"/>, if present; otherwise, the result of <paramref name="fallbackSupplier"/>(<paramref name="key"/>)</returns>
+    public static TValue GetValueOrSupply<TKey, TValue>(
+        this IDictionary<TKey, TValue> dic,
+        TKey                           key,
+        Func<TKey, TValue>             fallbackSupplier
+    ) {
+        return dic.TryGetValue(key, out var val) ? val : fallbackSupplier(key);
     }
 
     /// <summary>
@@ -52,9 +102,10 @@ public static class DictionaryExtensions {
     /// <typeparam name="TIn">the type of the desired <paramref name="key"/></typeparam>
     /// <typeparam name="TOut">the type of the <paramref name="valueFactory"/> output</typeparam>
     /// <returns>the existing <typeparamref name="TVal"/> or the result of <paramref name="valueFactory"/></returns>
-    public static TVal GetOrAddLazily<TKey, TVal, TIn, TOut>(this ConcurrentDictionary<TKey, Lazy<TVal>> concurrentDictionary, [NotNull] TIn key, Func<TIn, TOut> valueFactory)
+    public static TVal GetOrAddLazily<TKey, TVal, TIn, TOut>(this ConcurrentDictionary<TKey, Lazy<TVal>> concurrentDictionary, TIn key, Func<TIn, TOut> valueFactory)
         where TIn : TKey
-        where TOut : TVal {
+        where TOut : TVal
+        where TKey : notnull {
         Must.NotBeNull(key);
         return concurrentDictionary.GetOrAdd(key, k => new Lazy<TVal>(() => valueFactory((TIn)k!))).Value;
     }
