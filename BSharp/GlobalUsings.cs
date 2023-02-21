00f2cb6b3a6655df04d89ac52725143454de34bf
Brandon Cimino <bcimino@cccis.com>
2023-02-20 18:31:52 -0600

    fixed conditional imports

--- BSharp/GlobalUsings.cs
+++ BSharp/GlobalUsings.cs
@@ -2,3 +2,7 @@
 
 global using PureAttribute = System.Diagnostics.Contracts.PureAttribute;
 global using NotNullAttribute = System.Diagnostics.CodeAnalysis.NotNullAttribute;
+
+global using System;
+global using System.Numerics;
+global using System.Linq;
