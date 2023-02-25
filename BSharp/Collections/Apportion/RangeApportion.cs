68083c5c83b4c45088a1fba7914cd059e649bb2c
Brandon Cimino <bcimino@cccis.com>
2023-02-24 23:32:45 -0600

    `Splerp` -> `Terp`

--- BSharp/Collections/Apportion/RangeApportion.cs
+++ BSharp/Collections/Apportion/RangeApportion.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace FowlFever.BSharp.Collections.Apportion;
 
 /// <summary>
-/// Separates a set of <see cref="Indexes"/> into individual <see cref="Range"/>s that are proportional to the given <see cref="Weights"/>.
+/// Separates a set of <see cref="Indexes"/> into individual <see cref="Range"/>s that are proportional to the given <see cref="RangeApportion.Weights"/>.
 /// </summary>
 /// <param name="Source">the original <see cref="Indexes"/> (i.e. an abstract representation of the entries in an <see cref="ICollection{T}"/>)</param>
 /// <param name="Weights">the relative size of each of the resulting <see cref="ApportionBase{PORTION,SOURCE,SOURCE_ELEMENT}.Portions"/></param>
@@ -21,11 +19,11 @@ public record RangeApportion(Indexes Source, IEnumerable<double> Weights) : Appo
             }
             else {
                 var nextFraction = w / TotalWeight;
-                var splerp       = amountLeft.Splerp(nextFraction);
+                var splerp       = amountLeft.Terp(nextFraction);
                 var soFar        = Source - amountLeft;
 
-                yield return soFar..(soFar + splerp.A);
-                amountLeft = splerp.B;
+                yield return soFar..(soFar + splerp.taken);
+                amountLeft = splerp.leftovers;
             }
 
             i++;
