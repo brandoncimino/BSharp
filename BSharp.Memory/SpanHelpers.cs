0b616e6fc6274acc0a6474fd097c6a0d695744b4
Brandon Cimino <bcimino@cccis.com>
2023-02-25 20:14:08 -0600

    moved `GetListSpan` into `SpanHelpers`

--- BSharp.Memory/SpanHelpers.cs
+++ BSharp.Memory/SpanHelpers.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 using JetBrains.Annotations;
 
 namespace FowlFever.BSharp.Memory;
 
+/// <summary>
+/// Internal utilities to help <see cref="Span{T}"/> manipulation.
+/// </summary>
 internal static class SpanHelpers {
     public const StringSplitOptions TrimEntriesOption = (StringSplitOptions)2;
 
@@ -29,8 +36,10 @@ internal static class SpanHelpers {
     }
 
     internal static ReadOnlySpan<T> GenericTrim<T>(this ReadOnlySpan<T> span) {
-        return span.SkipWhile(static it => IsTrimmable(it))
-                   .SkipLastWhile(static it => IsTrimmable(it));
+        // ReSharper disable once ConvertToLocalFunction
+        Func<T, bool> trimmable = static it => IsTrimmable(it);
+        return span.Skip(span.CountWhile(trimmable))
+                   .Skip(span.CountWhile(trimmable, from: Spanq.From.End));
     }
 
     /// <exception cref="ArgumentOutOfRangeException">if <paramref name="index"/> &lt; 0 or ≥ <see cref="length"/></exception>
@@ -104,4 +113,40 @@ internal static class SpanHelpers {
             throw new ArgumentOutOfRangeException(_amountToAdd, amountToAdd, $"🙅‍♀️ {_caller}: {_count} {count} + {_amountToAdd} {amountToAdd} would exceed the {_maximum} limit of {maximum}!");
         }
     }
+
+    internal static ReadOnlySpan<T> AsReadOnly<T>(this Span<T> span) => span;
+
+    #region Lists
+
+    /// <summary>
+    /// The <see cref="FieldInfo"/> that stores the <see cref="Array"/> that backs a <see cref="List{T}"/>.
+    /// </summary>
+    private static readonly FieldInfo UnderlyingArrayField = typeof(List<>).GetField("_items", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new MissingFieldException(nameof(List<object>), "_items");
+
+    /// <param name="list">a <see cref="List{T}"/></param>
+    /// <typeparam name="T">the list element type</typeparam>
+    /// <returns>the <see cref="Array"/> that backs the given <see cref="List{T}"/></returns>
+    private static T[] GetUnderlyingArray<T>(List<T> list) {
+        var array = UnderlyingArrayField.GetValue(list);
+        Debug.Assert(array != null);
+        return (T[])array;
+    }
+
+    /// <summary>
+    /// Retrieves the <see cref="Span{T}"/> that backs the given <see cref="List{T}"/>.
+    /// </summary>
+    /// <param name="list">a <see cref="List{T}"/></param>
+    /// <typeparam name="T">the list element type</typeparam>
+    /// <returns>the <see cref="Span{T}"/> that backs the <see cref="List{T}"/></returns>
+    /// <remarks>
+    /// This delegates to the .NET 6+ <a href="https://learn.microsoft.com/en-us/dotnet/api/System.Runtime.InteropServices.CollectionsMarshal.AsSpan">CollectionsMarshal.AsSpan()</a> if it is available.
+    /// </remarks>
+    internal static Span<T> GetListSpan<T>(List<T> list) {
+#if NET6_0_OR_GREATER
+        return CollectionsMarshal.AsSpan(list);
+#endif
+        return GetUnderlyingArray(list);
+    }
+
+    #endregion
 }
