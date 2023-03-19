37c5887206bb5654821b933c54c4f26d5725d4be
Brandon Cimino <bcimino@cccis.com>
2023-03-18 20:24:43 -0500

    added `TryGetSpan` and `TryGetReadOnlySpan`

--- BSharp.Memory/Spanq.cs
+++ BSharp.Memory/Spanq.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
+
+using JetBrains.Annotations;
 
 namespace FowlFever.BSharp.Memory;
 
@@ -37,5 +41,61 @@ public static partial class Spanq {
 
     internal static string FormatString<T>(this Span<T> span) {
         return FormatString((ReadOnlySpan<T>)span);
+    }
+
+    /// <summary>
+    /// Attempts to get the underlying <see cref="ReadOnlySpan{T}"/> from an <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <param name="stuff">a sequence of stuff</param>
+    /// <param name="span"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static bool TryGetReadOnlySpan<T>([NoEnumeration] IEnumerable<T> stuff, out ReadOnlySpan<T> span) {
+        switch (stuff) {
+            case T[] arr:
+                span = arr;
+                return true;
+            case List<T> ls:
+                span = SpanHelpers.GetListSpan(ls);
+                return true;
+            case ImmutableArray<T> imm:
+                span = imm.AsSpan();
+                return true;
+            case ArraySegment<T> seg:
+                span = seg;
+                return true;
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            case IAsReadOnlySpan<T> asRo:
+                span = asRo.AsReadOnlySpan();
+                return true;
+            default:
+                span = default;
+                return false;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to get the underlying <see cref="Span{T}"/> from this <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <param name="stuff">a sequence of stuff</param>
+    /// <param name="span">the underlying <b><i>mutable</i></b> <see cref="Span{T}"/></param>
+    /// <typeparam name="T">the span element type</typeparam>
+    /// <returns>true if a span could be retrieved</returns>
+    internal static bool TryGetSpan<T>(IEnumerable<T> stuff, out Span<T> span) {
+        switch (stuff) {
+            case T[] arr:
+                span = arr;
+                return true;
+            case List<T> ls:
+                span = SpanHelpers.GetListSpan(ls);
+                return true;
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            case IAsSpan<T> asSpan:
+                span = asSpan.AsSpan();
+                return true;
+            default:
+                span = default;
+                return false;
+        }
     }
 }
