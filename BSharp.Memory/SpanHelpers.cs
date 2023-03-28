36336b2294b947d27749d1a9277f7bd32f056628
Brandon Cimino <bcimino@cccis.com>
2023-03-27 23:39:56 -0500

    removed the trim stuff

--- BSharp.Memory/SpanHelpers.cs
+++ BSharp.Memory/SpanHelpers.cs
@@ -15,33 +15,6 @@ namespace FowlFever.BSharp.Memory;
 internal static class SpanHelpers {
     public const StringSplitOptions TrimEntriesOption = (StringSplitOptions)2;
 
-    /// <summary>
-    /// Determines if <see cref="F:System.StringSplitOptions.TrimEntries"/> should apply.
-    /// </summary>
-    /// <remarks>
-    /// TODO: Add the ability to provide a <see cref="Func{T, T}"/> predicate to <see cref="SpanSpliterator{T}"/> that will override this default behavior
-    /// </remarks>
-    /// <param name="entry">a <typeparamref name="T"/> instance</param>
-    /// <returns><c>true</c> if we should trim the entry</returns>
-    internal static bool IsTrimmable<T>(T? entry) {
-        return entry switch {
-            char c => char.IsWhiteSpace(c),
-#if NET5_0_OR_GREATER
-            System.Text.Rune r => System.Text.Rune.IsWhiteSpace(r),
-#endif
-            string s => string.IsNullOrWhiteSpace(s),
-            null     => true,
-            _        => false
-        };
-    }
-
-    internal static ReadOnlySpan<T> GenericTrim<T>(this ReadOnlySpan<T> span) {
-        // ReSharper disable once ConvertToLocalFunction
-        Func<T, bool> trimmable = static it => IsTrimmable(it);
-        return span.Skip(span.CountWhile(trimmable))
-                   .Skip(span.CountWhile(trimmable, from: Spanq.From.End));
-    }
-
     /// <exception cref="ArgumentOutOfRangeException">if <paramref name="index"/> &lt; 0 or ≥ <see cref="length"/></exception>
     internal static int RequireIndex(
         this int                                     length,
