16e898311d30fd88bf73c0908878997c94dde1f1
Brandon Cimino <bcimino@cccis.com>
2023-03-28 23:57:19 -0500

    Simplified and cleaned up `Mathb.Signage` methods

--- BSharp/Mathb.Signage.cs
+++ BSharp/Mathb.Signage.cs
@@ -1,6 +1,4 @@
-using System;
-
-using FowlFever.BSharp.Exceptions;
+using System.Runtime.CompilerServices;
 
 namespace FowlFever.BSharp {
     public static partial class Mathb {
@@ -8,152 +6,133 @@ namespace FowlFever.BSharp {
 
         #region Math.Sign()
 
-        /**
-         * <seealso cref="Math.Sign(short)"/>
-         */
-        [Pure]
-        public static int Sign(this short value) {
-            return Math.Sign(value);
-        }
+        /// <inheritdoc cref="Math.Sign(short)"/>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Sign(this short value) => Math.Sign(value);
 
-        /**
-         * <seealso cref="Math.Sign(int)"/>
-         */
-        [Pure]
-        public static int Sign(this int integer) {
-            return Math.Sign(integer);
-        }
+        /// <inheritdoc cref="Math.Sign(int)"/>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Sign(this int integer) => Math.Sign(integer);
 
-        /**
-         * <seealso cref="Math.Sign(long)"/>
-         */
-        [Pure]
+        /// <inheritdoc cref="Math.Sign(long)"/>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Sign(this long value) => Math.Sign(value);
 
-        /**
-         * <seealso cref="Math.Sign(float)"/>
-         */
-        [Pure]
+        /// <inheritdoc cref="Math.Sign(float)"/>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Sign(this float value) => Math.Sign(value);
 
-        /**
-         * <seealso cref="Math.Sign(double)"/>
-         */
-        [Pure]
+        /// <inheritdoc cref="Math.Sign(double)"/>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Sign(this double value) => Math.Sign(value);
 
-        /**
-         * <seealso cref="Math.Sign(decimal)"/>
-         */
-        [Pure]
-        public static int Sign(this decimal value) {
-            return Math.Sign(value);
-        }
+        /// <inheritdoc cref="Math.Sign(decimal)"/>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Sign(this decimal value) => Math.Sign(value);
 
-        /// <returns><paramref name="value"/>.<see cref="TimeSpan.CompareTo(object)"/>(<see cref="TimeSpan.Zero"/>)</returns>
-        [Pure]
-        public static int Sign(this TimeSpan value) {
-            return value.CompareTo(TimeSpan.Zero);
-        }
+        /// <returns><paramref name="value"/>.<see cref="TimeSpan.CompareTo(TimeSpan)"/>(<see cref="TimeSpan.Zero"/>)</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Sign(this TimeSpan value) => value.CompareTo(TimeSpan.Zero);
 
         #endregion
 
         #region IsPositive
 
-        [Pure] public static bool IsPositive(this short    value) => value >= 0;
-        [Pure] public static bool IsPositive(this int      value) => value >= 0;
-        [Pure] public static bool IsPositive(this long     value) => value >= 0;
-        [Pure] public static bool IsPositive(this float    value) => value >= 0;
-        [Pure] public static bool IsPositive(this double   value) => value >= 0;
-        [Pure] public static bool IsPositive(this decimal  value) => value >= 0;
-        [Pure] public static bool IsPositive(this TimeSpan value) => value >= TimeSpan.Zero;
+        /// <returns><paramref name="value"/> &ge; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPositive(this short value) => value >= 0;
 
-        /// <summary>
-        /// Attempts to call the appropriate <see cref="IsPositive(short)"/> overload.
-        /// </summary>
-        /// <param name="value">a value supported by <see cref="IsPositive(short)"/>, etc.</param>
-        /// <returns>true if <paramref name="value"/> ≥ 0</returns>
-        /// <exception cref="ArgumentException">if <paramref name="value"/> isn't a type with an appropriate <see cref="IsPositive(short)"/> method</exception>
-        [Pure]
-        public static bool IsPositive<T>(T value) {
-            return value switch {
-                null       => false,
-                short s    => s.IsPositive(),
-                int i      => i.IsPositive(),
-                long l     => l.IsPositive(),
-                float f    => f.IsPositive(),
-                double d   => d.IsPositive(),
-                decimal d  => d.IsPositive(),
-                TimeSpan t => t.IsPositive(),
-                _          => throw Reject.UnhandledSwitchType(value, nameof(value), nameof(IsPositive)),
-            };
-        }
+        /// <returns><paramref name="value"/> &ge; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPositive(this int value) => value >= 0;
+
+        /// <returns><paramref name="value"/> &ge; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPositive(this long value) => value >= 0;
+
+        /// <returns><paramref name="value"/> &ge; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPositive(this float value) => value >= 0;
+
+        /// <returns><paramref name="value"/> &ge; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPositive(this double value) => value >= 0;
+
+        /// <returns><paramref name="value"/> &ge; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPositive(this decimal value) => value >= 0;
+
+        /// <returns><paramref name="value"/> &ge; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPositive(this TimeSpan value) => value >= TimeSpan.Zero;
+
+        /// <returns><paramref name="value"/> &ge; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPositive(this sbyte value) => value >= 0;
 
         #endregion
 
         #region IsStrictlyPositive
 
-        [Pure] public static bool IsStrictlyPositive(this short    value) => value > 0;
-        [Pure] public static bool IsStrictlyPositive(this int      value) => value > 0;
-        [Pure] public static bool IsStrictlyPositive(this long     value) => value > 0;
-        [Pure] public static bool IsStrictlyPositive(this float    value) => value > 0;
-        [Pure] public static bool IsStrictlyPositive(this double   value) => value > 0;
-        [Pure] public static bool IsStrictlyPositive(this decimal  value) => value > 0;
-        [Pure] public static bool IsStrictlyPositive(this TimeSpan value) => value > TimeSpan.Zero;
+        /// <returns><paramref name="value"/> &gt; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsStrictlyPositive(this short value) => value > 0;
 
-        /// <summary>
-        /// Attempts to call the appropriate <see cref="IsStrictlyPositive(short)"/> overload.
-        /// </summary>
-        /// <param name="value">a value supported by <see cref="IsStrictlyPositive(short)"/>, etc.</param>
-        /// <returns>true if <paramref name="value"/> > 0</returns>
-        /// <exception cref="ArgumentException">if <paramref name="value"/> isn't a type with an appropriate <see cref="IsStrictlyPositive(short)"/> method</exception>
-        [Pure]
-        public static bool IsStrictlyPositive<T>(T value) {
-            return value switch {
-                null       => false,
-                short s    => s.IsStrictlyPositive(),
-                int i      => i.IsStrictlyPositive(),
-                long l     => l.IsStrictlyPositive(),
-                float f    => f.IsStrictlyPositive(),
-                double d   => d.IsStrictlyPositive(),
-                decimal d  => d.IsStrictlyPositive(),
-                TimeSpan t => t.IsStrictlyPositive(),
-                _          => throw Reject.UnhandledSwitchType(value, nameof(value), nameof(IsStrictlyPositive)),
-            };
-        }
+        /// <returns><paramref name="value"/> &gt; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsStrictlyPositive(this int value) => value > 0;
+
+        /// <returns><paramref name="value"/> &gt; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsStrictlyPositive(this long value) => value > 0;
+
+        /// <returns><paramref name="value"/> &gt; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsStrictlyPositive(this float value) => value > 0;
+
+        /// <returns><paramref name="value"/> &gt; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsStrictlyPositive(this double value) => value > 0;
+
+        /// <returns><paramref name="value"/> &gt; 0</returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsStrictlyPositive(this decimal value) => value > 0;
+
+        /// <returns><paramref name="value"/> &gt; <see cref="TimeSpan.Zero"/></returns>
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsStrictlyPositive(this TimeSpan value) => value > TimeSpan.Zero;
 
         #endregion
 
         #region IsNegative
 
-        [Pure] public static bool IsNegative(this short    value) => value < 0;
-        [Pure] public static bool IsNegative(this int      value) => value < 0;
-        [Pure] public static bool IsNegative(this long     value) => value < 0;
-        [Pure] public static bool IsNegative(this float    value) => value < 0;
-        [Pure] public static bool IsNegative(this double   value) => value < 0;
-        [Pure] public static bool IsNegative(this decimal  value) => value < 0;
-        [Pure] public static bool IsNegative(this TimeSpan value) => value < TimeSpan.Zero;
+        /// <returns><param name="value"> &lt; 0</param></returns> 
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNegative(this short value) => value < 0;
 
-        /// <summary>
-        /// Attempts to call the appropriate <see cref="IsNegative(short)"/> overload.
-        /// </summary>
-        /// <param name="value">a value supported by <see cref="IsNegative(short)"/>, etc.</param>
-        /// <returns>true if <paramref name="value"/> <![CDATA[<]]> 0</returns>
-        /// <exception cref="ArgumentException">if <paramref name="value"/> isn't a type with an appropriate <see cref="IsNegative(short)"/> method</exception>
-        [Pure]
-        public static bool IsNegative<T>(T value) {
-            return value switch {
-                null       => false,
-                short s    => s.IsNegative(),
-                int i      => i.IsNegative(),
-                long l     => l.IsNegative(),
-                float f    => f.IsNegative(),
-                double d   => d.IsNegative(),
-                decimal d  => d.IsNegative(),
-                TimeSpan t => t.IsNegative(),
-                _          => throw Reject.UnhandledSwitchType(value, nameof(value), nameof(IsNegative)),
-            };
-        }
+        /// <returns><param name="value"> &lt; 0</param></returns> 
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNegative(this int value) => value < 0;
+
+        /// <returns><param name="value"> &lt; 0</param></returns> 
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNegative(this long value) => value < 0;
+
+        /// <returns><param name="value"> &lt; 0</param></returns> 
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNegative(this float value) => value < 0;
+
+        /// <returns><param name="value"> &lt; 0</param></returns> 
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNegative(this double value) => value < 0;
+
+        /// <returns><param name="value"> &lt; 0</param></returns> 
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNegative(this decimal value) => value < 0;
+
+        /// <returns><param name="value"> &lt; <see cref="TimeSpan.Zero"/></param></returns> 
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNegative(this TimeSpan value) => value < TimeSpan.Zero;
 
         #endregion
 
