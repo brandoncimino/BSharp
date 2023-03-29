4aaa558e0921919fbe962ccd8f9a922ad7cc7550
Brandon Cimino <bcimino@cccis.com>
2023-03-28 23:58:31 -0500

    reduced a lot of chaff

--- BSharp/Exceptions/Must.Numbers.cs
+++ BSharp/Exceptions/Must.Numbers.cs
@@ -34,57 +34,5 @@ public static partial class Must {
         throw Reject(actualValue, details, parameterName, rejectedBy, reason: "must be > 0");
     }
 
-    public static T BePositive<T>(
-        T?      actualValue,
-        string? details = default,
-        [CallerArgumentExpression("actualValue")]
-        string? _actualValue = default,
-        [CallerMemberName] string? _caller = default
-    ) where T : unmanaged {
-        return BePositive(NotBeNull(actualValue));
-    }
-
-    [NonNegativeValue]
-    public static T BeStrictlyPositive<T>(
-        [NotNull] T actualValue,
-        [CallerArgumentExpression("actualValue")]
-        string? parameterName = default,
-        [CallerMemberName] string? methodName = default
-    ) where T : unmanaged {
-        NotBeNull(actualValue);
-        return Be(actualValue, static arg => Mathb.IsStrictlyPositive(arg), parameterName, methodName, reason: "must be strictly positive (x > 0)");
-    }
-
-    public static T BeNegative<T>(
-        [NotNull] T actualValue,
-        [CallerArgumentExpression("actualValue")]
-        string? parameterName = default,
-        [CallerMemberName] string? methodName = default
-    ) {
-        NotBeNull(actualValue);
-        return Be(actualValue, static arg => Mathb.IsNegative(arg), parameterName, methodName, reason: "must be negative (x < 0)");
-    }
-
-    public static T BeGreaterThan<T>(
-        [NotNull] T firstValue,
-        [NotNull] T secondValue,
-        [CallerArgumentExpression("firstValue")]
-        string? firstName = default,
-        [CallerArgumentExpression("secondValue")]
-        string? secondName = default,
-        [CallerMemberName] string? methodName = default
-    )
-        where T : IComparable<T> {
-        return Compare(
-                firstValue,
-                ComparisonOperator.GreaterThan,
-                secondValue,
-                firstName,
-                secondName,
-                methodName
-            )
-            .first;
-    }
-
     #endregion
 }
