c8782018d32584fc266bb8faae9c5e6e14395d48
Brandon Cimino <bcimino@cccis.com>
2023-04-21 18:44:57 -0500

    removed `FindFileName`

--- Clerical/Clerk.FileName.cs
+++ Clerical/Clerk.FileName.cs
@@ -1,20 +1,7 @@
-using System.Diagnostics.Contracts;
-
 namespace FowlFever.Clerical;
 
 public static partial class Clerk {
     #region FileName
-
-    [Pure]
-    public static PathPart? FindFileName(ReadOnlySpan<char> path) {
-        var coreFileName = Path.GetFileName(path);
-
-        if (coreFileName.IsEmpty || PathPart.IsSpecialPathPart(path)) {
-            return default;
-        }
-
-        return PathPart.Of(coreFileName);
-    }
 
     /// <inheritdoc cref="Path.GetFileName(System.ReadOnlySpan{char})"/>
     /// <remarks>
