fc3c96495092fbaf7b73bdd6758fff0dd4a745a5
Brandon Cimino <bcimino@cccis.com>
2023-04-21 18:35:54 -0500

    overrode `Equals` & `GetHashCode`

--- Clerical/Substring.cs
+++ Clerical/Substring.cs
@@ -1,4 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+
+using CommunityToolkit.HighPerformance;
 
 namespace FowlFever.Clerical;
 
@@ -18,16 +21,20 @@ public readonly record struct Substring([field: MaybeNull] string Source, int St
     /// Retrieves a <see cref="char"/> from this substring <see cref="AsSpan"/>.
     /// </summary>
     /// <inheritdoc cref="ReadOnlySpan{T}.this"/>
+    [Pure]
     public char this[int index] => AsSpan()[index];
 
     /// <returns>the <see cref="System.MemoryExtensions.AsSpan(string?, int, int)"/> from the <see cref="Source"/> that represents this <see cref="Substring"/></returns>
+    [Pure]
     public ReadOnlySpan<char> AsSpan() => Source.AsSpan(Start, Length);
 
     public bool IsDefault => Source is null;
 
-    public static implicit operator Substring(string             str)       => new(str, 0, str.Length);
-    public static implicit operator ReadOnlySpan<char>(Substring substring) => substring.AsSpan();
+    [Pure] public static                   Substring AllOf(string       str)       => new(str, 0, str.Length);
+    [Pure] public static implicit operator Substring(string             str)       => AllOf(str);
+    [Pure] public static implicit operator ReadOnlySpan<char>(Substring substring) => substring.AsSpan();
 
+    [Pure]
     public override string ToString() {
         return AsSpan().ToString();
     }
@@ -56,11 +63,20 @@ public readonly record struct Substring([field: MaybeNull] string Source, int St
     /// <param name="source">the <see cref="string"/> that the <paramref name="span"/> came from</param>
     /// <returns>the created <see cref="Substring"/></returns>
     /// <exception cref="ArgumentException">if the <paramref name="span"/> doesn't <see cref="System.MemoryExtensions.Overlaps{T}(System.ReadOnlySpan{T},System.ReadOnlySpan{T})">overlap</see> with the <paramref name="source"/></exception>
+    [Pure]
     public static Substring CreateFromSpan(ReadOnlySpan<char> span, string source) {
         if (TryCreateFromSpan(span, source, out var result)) {
             return result;
         }
 
         throw new ArgumentException("The given span didn't overlap with the reference string!");
+    }
+
+    public bool Equals(Substring other) {
+        return AsSpan().SequenceEqual(other);
+    }
+
+    public override int GetHashCode() {
+        return AsSpan().GetDjb2HashCode();
     }
 }
