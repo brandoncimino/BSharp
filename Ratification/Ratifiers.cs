4b1ccf1f9db33d0574f19b850f8076c53e9f23a2
Brandon Cimino <bcimino@cccis.com>
2023-09-18 17:08:31 -0500

    Removed the ridiculous `Ratification` project

--- Ratification/Ratifiers.cs
+++
@@ -1,6 +0,0 @@
-namespace Ratified;
-
-public static class Ratifiers {
-    public static readonly IRatifier<string, IEnumerable<char>> ContainsAny = Ratifier<string>.Create<IEnumerable<char>>(static (str, chars) => chars.Any(str.Contains));
-    public static          IRatifier<string>                    MakeContainsAny(IEnumerable<char> chars) => ContainsAny.Bind(chars);
-}
