b743b73257df3bfb80a54af07933b162d52b5321
Brandon Cimino <bcimino@cccis.com>
2023-09-18 16:11:50 -0500

    made a `ToImmutableArray` overload that accepts a `TArg` parameter

--- BSharp/Collections/ImmutableExtensions.Memory.cs
+++ BSharp/Collections/ImmutableExtensions.Memory.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections.Immutable;
 
 using FowlFever.BSharp.Memory;
+using FowlFever.BSharp.Memory.Enumerators;
 
 using JetBrains.Annotations;
 
@@ -11,14 +11,20 @@ public static partial class ImmutableExtensions {
     #region Spanq
 
     [MustUseReturnValue]
-    public static ImmutableArray<TOut> ToImmutableArray<TSpan, TOut>(this SpanSpliterator<TSpan> spliterator, [RequireStaticDelegate] ReadOnlySpanFunc<TSpan, TOut> spanSelector) where TSpan : IEquatable<TSpan> {
+    public static ImmutableArray<TOut> ToImmutableArray<TSpan, TArg, TOut>(this SpanSpliterator<TSpan> spliterator, TArg arg, [RequireStaticDelegate] ReadOnlySpanFunc<TSpan, TArg, TOut> spanSelector) where TSpan : IEquatable<TSpan> {
         var builder = ImmutableArray.CreateBuilder<TOut>();
 
         foreach (var span in spliterator) {
-            builder.Add(spanSelector(span));
+            builder.Add(spanSelector(span, arg));
         }
 
         return builder.MoveToImmutableSafely();
+    }
+
+    [MustUseReturnValue]
+    public static ImmutableArray<TOut> ToImmutableArray<TSpan, TOut>(this SpanSpliterator<TSpan> spliterator, [RequireStaticDelegate] ReadOnlySpanFunc<TSpan, TOut> spanSelector) where TSpan : IEquatable<TSpan> {
+        //TODO: Benchmark this double-delegate version vs. duplicating the code from the other ToImmutableArray() overload
+        return spliterator.ToImmutableArray<TSpan, ReadOnlySpanFunc<TSpan, TOut>, TOut>(spanSelector, static (span, func) => func(span));
     }
 
     [MustUseReturnValue]
