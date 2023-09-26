cbc22a4ef1ac2e4fed2cff0de4f009ebdf8de29a
Brandon Cimino <bcimino@cccis.com>
2023-09-26 11:28:49 -0500

    Cleaned up `SpanDelegates`

--- BSharp.Memory/SpanDelegates.cs
+++ BSharp.Memory/SpanDelegates.cs
@@ -3,35 +3,67 @@ using System.Buffers;
 
 namespace FowlFever.BSharp.Memory;
 
-#region ReadOnlySpan
-
-public delegate TOut               ReadOnlySpanFunc<TIn, in TArg, out TOut>(ReadOnlySpan<TIn> span, TArg arg);
-public delegate TOut               ReadOnlySpanFunc<TIn, out TOut>(ReadOnlySpan<TIn>          span);
-public delegate TOut               BiRoSpanFunc<A, B, in TArg, out TOut>(ReadOnlySpan<A>      a, ReadOnlySpan<B> b, TArg arg);
-public delegate TOut               BiRoSpanFunc<A, B, out TOut>(ReadOnlySpan<A>               a, ReadOnlySpan<B> b);
-public delegate ReadOnlySpan<TOut> ToRoSpanFunc<in TIn, TOut>(TIn                             input);
-public delegate ReadOnlySpan<TOut> RoSpanTransformer<TIn, TOut>(ReadOnlySpan<TIn>             span);
-public delegate ReadOnlySpan<TOut> RoSpanMixer<TIn, in TArg, TOut>(ReadOnlySpan<TIn>          span, TArg arg);
+#region {Ro}SpanFunc
 
 /// <summary>
-/// An <see cref="Action{T}"/> that acts on a <see cref="ReadOnlySpan{T}"/>.
+/// A <see cref="Func{T1,T2,TResult}"/> that accepts a <see cref="ReadOnlySpan{T}"/> argument.
 /// </summary>
 /// <remarks>
-/// Equivalent to the built-in <see cref="ReadOnlySpanAction{T,TArg}"/>, but without the <c>TArg</c> "state" parameter.
+/// Analogous to <see cref="ReadOnlySpanAction{T,TArg}"/>.
 /// </remarks>
-/// <typeparam name="T">the <see cref="ReadOnlySpan{T}"/> type</typeparam>
-public delegate void ReadOnlySpanAction<T>(ReadOnlySpan<T> span);
+/// <seealso cref="SpanFunc{TIn,TArg,TOut}"/>
+public delegate TOut ReadOnlySpanFunc<TIn, in TArg, out TOut>(ReadOnlySpan<TIn> span, TArg arg);
+
+/// <summary>
+/// A <see cref="Func{T1, TResult}"/> that accepts a <see cref="ReadOnlySpan{T}"/> argument.
+/// </summary>
+/// <seealso cref="SpanFunc{TIn,TOut}"/>
+public delegate TOut ReadOnlySpanFunc<TIn, out TOut>(ReadOnlySpan<TIn> span);
+
+/// <summary>
+/// A <see cref="Func{T1, T2, TResult}"/> that accepts a <see cref="Span{T}"/> argument.
+/// </summary>
+/// <remarks>
+/// Analogous to <see cref="SpanAction{T,TArg}"/>.
+/// </remarks>
+/// <seealso cref="ReadOnlySpanFunc{TIn,TArg,TOut}"/>
+public delegate TOut SpanFunc<TIn, in TArg, out TOut>(Span<TIn> span, TArg arg);
+
+/// <summary>
+/// A <see cref="Func{T1, TResult}"/> that accepts a <see cref="Span{T}"/> argument.
+/// </summary>
+/// <seealso cref="ReadOnlySpanFunc{TIn, TOut}"/>
+public delegate TOut SpanFunc<TIn, out TOut>(Span<TIn> span);
 
 #endregion
 
-#region Span
+#region {Ro}SpanTransformer
 
-public delegate TOut       SpanFunc<TIn, in TArg, out TOut>(Span<TIn>    span, TArg arg);
-public delegate TOut       SpanFunc<TIn, out TOut>(Span<TIn>             span);
-public delegate TOut       BiSpanFunc<A, B, in TArg, out TOut>(Span<A>   a, Span<B> b, TArg arg);
-public delegate TOut       BiSpanFunc<A, B, out TOut>(Span<A>            a, Span<B> b);
-public delegate Span<TOut> ToSpanFunc<in TIn, TOut>(TIn                  input);
+/// <summary>
+/// A <see cref="Func{T1, T2, TResult}"/> that operates on and returns <see cref="ReadOnlySpan{T}"/>s.
+/// </summary>
+/// <seealso cref="SpanTransformer{TIn,TArg,TOut}"/>
+public delegate ReadOnlySpan<TOut> RoSpanTransformer<TIn, in TArg, TOut>(ReadOnlySpan<TIn> span, TArg arg);
+
+/// <summary>
+/// A <see cref="Func{T, TResult}"/> that operates on and returns <see cref="ReadOnlySpan{T}"/>s.
+/// </summary>
+/// <seealso cref="SpanTransformer{TIn,TOut}"/>
+public delegate ReadOnlySpan<TOut> RoSpanTransformer<TIn, TOut>(ReadOnlySpan<TIn> span);
+
+/// <summary>
+/// A <see cref="Func{T1, T2, TResult}"/> that operates on and returns <see cref="Span{T}"/>s.
+/// </summary>
+/// <seealso cref="RoSpanTransformer{T1, T2, TOut}"/>
 public delegate Span<TOut> SpanTransformer<TIn, in TArg, TOut>(Span<TIn> span, TArg arg);
+
+/// <summary>
+/// A <see cref="Func{T, TResult}"/> that operates on and returns <see cref="Span{T}"/>s.
+/// </summary>
+/// <seealso cref="RoSpanTransformer{TIn,TOut}"/>
+public delegate Span<TOut> SpanTransformer<TIn, TOut>(Span<TIn> span);
+
+#endregion
 
 /// <summary>
 /// An <see cref="Action{T}"/> that acts on a <see cref="Span{T}"/>.
@@ -41,5 +73,3 @@ public delegate Span<TOut> SpanTransformer<TIn, in TArg, TOut>(Span<TIn> span, T
 /// </remarks>
 /// <typeparam name="T">the <see cref="Span{T}"/> type</typeparam>
 public delegate void SpanAction<T>(Span<T> span);
-
-#endregion
