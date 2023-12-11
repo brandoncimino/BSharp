0f71afd91fefa0bd5b9cdaaca8a667606aa81c3f
Brandon Cimino <bcimino@cccis.com>
2023-12-10 20:42:16 -0600

    cleanup

--- BSharp/Collections/CollectionUtils.Dictionaries.cs
+++ BSharp/Collections/CollectionUtils.Dictionaries.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 
 using JetBrains.Annotations;
 
@@ -13,37 +11,6 @@ public static partial class CollectionUtils {
     #region Dictionaries After Dark
 
     //TODO: move to a `DictionaryExtensions` class
-
-    /// <summary>
-    /// Shorthand to go from an <see cref="IEnumerable{T}"/> of <see cref="KeyValuePair{TKey,TValue}"/>s back to an <see cref="IDictionary{TKey,TValue}"/> via <see cref="Enumerable.ToDictionary{TSource,TKey,TValue}(System.Collections.Generic.IEnumerable{TSource},System.Func{TSource,TKey},System.Func{TSource,TValue})"/>
-    /// </summary>
-    /// <param name="source">a collection of <see cref="KeyValuePair{TKey,TValue}"/>s</param>
-    /// <typeparam name="TKey">the type of <see cref="IDictionary{TKey,TValue}.Keys"/></typeparam>
-    /// <typeparam name="TVal">the type of <see cref="IDictionary{TKey,TValue}.Values"/></typeparam>
-    /// <returns>a new <see cref="IDictionary{TKey,TValue}"/></returns>
-    public static IDictionary<TKey, TVal> ToDictionary<TKey, TVal>([InstantHandle] this IEnumerable<KeyValuePair<TKey, TVal>> source)
-        where TKey : notnull {
-        return new Dictionary<TKey, TVal>(source);
-    }
-
-    /// <remarks>
-    /// Similar to <see cref="Enumerable.ToDictionary{TSource,TKey}(System.Collections.Generic.IEnumerable{TSource},System.Func{TSource,TKey})"/>, but produces
-    /// a sequence of <see cref="KeyValuePair{T,T}"/>s so you can branch of into things like:
-    /// <ul>
-    /// <li><see cref="ImmutableDictionary.ToImmutableDictionary{TKey,TValue}(System.Collections.Generic.IEnumerable{System.Collections.Generic.KeyValuePair{TKey,TValue}})"/></li>
-    /// <li><see cref="ToConcurrentDictionary{TKey,TVal}"/></li>
-    /// </ul>
-    /// </remarks>
-    /// <param name="source">this <see cref="IEnumerable{T}"/></param>
-    /// <param name="keySelector">generates the <see cref="KeyValuePair{TKey,TValue}.Key"/> for a <typeparamref name="T"/></param>
-    /// <param name="valSelector">generates the <see cref="KeyValuePair{TKey,TValue}.Value"/> for a <typeparamref name="T"/></param>
-    /// <typeparam name="T">the type of entries in the <paramref name="source"/></typeparam>
-    /// <typeparam name="TKey">the <see cref="KeyValuePair{TKey,TValue}.Key"/> type</typeparam>
-    /// <typeparam name="TVal">the <see cref="KeyValuePair{TKey,TValue}.Value"/> type</typeparam>
-    /// <returns>a sequence of <see cref="KeyValuePair{TKey,TValue}"/>s</returns>
-    public static IEnumerable<KeyValuePair<TKey, TVal>> ToKeyValuePairs<T, TKey, TVal>(this IEnumerable<T> source, Func<T, TKey> keySelector, Func<T, TVal> valSelector) {
-        return source.Select(it => KeyValuePair.Create(keySelector(it), valSelector(it)));
-    }
 
     /// <summary>
     /// Selects a <see cref="KeyValuePair{TKey,TValue}.Key"/> from each <typeparamref name="T"/> element, combining them into <see cref="KeyValuePair{TKey,TValue}"/>s.
