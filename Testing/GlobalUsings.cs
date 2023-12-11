f598a1b148513e6a91e1fb20531532538b16bbe8
Brandon Cimino <bcimino@cccis.com>
2023-12-10 22:12:02 -0600

    Explicit import to avoid conflicts with .net 8

--- Testing/GlobalUsings.cs
+++ Testing/GlobalUsings.cs
@@ -1,3 +1,4 @@
 global using Is = NUnit.Framework.Is;
 global using Throws = FowlFever.Testing.NUnitExtensionPoints.Throws;
 global using PureAttribute = System.Diagnostics.Contracts.PureAttribute;
+global using ExperimentalAttribute = FowlFever.BSharp.Attributes.ExperimentalAttribute;
