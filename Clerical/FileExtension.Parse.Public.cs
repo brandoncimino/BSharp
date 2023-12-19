f83429acef70738735e52c49fef57db3b19132f2
Brandon Cimino <bcimino@cccis.com>
2023-12-19 00:58:54 -0600

    Meant to have committed this earlier

--- Clerical/FileExtension.Parse.Public.cs
+++ Clerical/FileExtension.Parse.Public.cs
@@ -1,11 +1,10 @@
 namespace FowlFever.Clerical;
 
 // This file contains the public "Parse" methods available for `FileExtension`s.
-public readonly partial struct FileExtension
-#if NET7_0_OR_GREATER
-    : ISpanParsable<FileExtension>
+#if !NET7_0_OR_GREATER
+[SuppressMessage("ReSharper", "InheritdocInvalidUsage", Justification = "Versions prior to .NET 7 don't understand static abstract members in interfaces")]
 #endif
-{
+public readonly partial struct FileExtension : IClericalParsable<FileExtension> {
     #region Explicit implementations of .NET 7 interfaces
 
     // These are explicit implementations so that we don't clutter up our method signatures with the useless `IFormatProvider` parameters.
@@ -15,44 +14,19 @@ public readonly partial struct FileExtension
     [Pure] static FileExtension IParsable<FileExtension>.    Parse(string                s, IFormatProvider? provider)                           => Parse(s);
     [Pure] static bool ISpanParsable<FileExtension>.         TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, out FileExtension result) => TryParse(s, out result);
     [Pure] static bool IParsable<FileExtension>.             TryParse(string?            s, IFormatProvider? provider, out FileExtension result) => TryParse(s, out result);
-
 #endif
 
     #endregion
 
     #region Public "Parse" methods
 
-    /// <inheritdoc cref="Parser.Parse_Forgiving"/>
-    [Pure]
-    public static FileExtension Parse(ReadOnlySpan<char> s) => Parser.Parse_Forgiving(s);
+    [Pure] public static FileExtension Parse(ReadOnlySpan<char> s, ClericalStyles styles = default) => Parser.Parse_Internal(s, styles);
 
-    /// <inheritdoc cref="Parser.Parse_Strict"/>
-    [Pure]
-    public static FileExtension ParseExact(ReadOnlySpan<char> s) => Parser.Parse_Strict(s);
+    [Pure] public static FileExtension Parse(string s, ClericalStyles styles = default) => Parser.Parse_Internal(s, styles);
 
-    /// <inheritdoc cref="Parser.Parse_Forgiving"/>
-    [Pure]
-    public static FileExtension Parse(string s) => Parser.Parse_Forgiving(s);
+    [Pure] public static bool TryParse(ReadOnlySpan<char> s, out FileExtension result, ClericalStyles styles = default) => Parser.TryParse_Internal(s, styles, out result) is null;
 
-    /// <inheritdoc cref="Parser.Parse_Strict"/>
-    [Pure]
-    public static FileExtension ParseExact(string s) => Parser.Parse_Strict(s);
-
-    /// <inheritdoc cref="Parser.Parse_Forgiving"/>
-    [Pure]
-    public static bool TryParse(ReadOnlySpan<char> s, out FileExtension result) => Parser.TryParse_Internal(s, false, false, out result);
-
-    /// <inheritdoc cref="Parser.Parse_Strict"/>
-    [Pure]
-    public static bool TryParseExact(ReadOnlySpan<char> s, out FileExtension result) => Parser.TryParse_Internal(s, true, false, out result);
-
-    /// <inheritdoc cref="Parser.Parse_Forgiving"/>
-    [Pure]
-    public static bool TryParse(string? s, out FileExtension result) => Parser.TryParse_Internal(s, false, false, out result);
-
-    /// <inheritdoc cref="Parser.Parse_Strict"/>
-    [Pure]
-    public static bool TryParseExact(string? s, out FileExtension result) => Parser.TryParse_Internal((SpanOrSegment)s, true, false, out result);
+    [Pure] public static bool TryParse(string? s, out FileExtension result, ClericalStyles styles = default) => Parser.TryParse_Internal(s, styles, out result) is null;
 
     #endregion
 }
